63d76e0f37669e555e788326dd8eb030eebd7563
Alexander Ivanov <alehander42@gmail.com>
2018-02-13 12:42:04 +0200

    Improve headers build

--- nimbus.nimble
+++ nimbus.nimble
@@ -10,7 +10,7 @@ skipDirs      = @["tests"]
 requires "nim >= 0.17.0",
          "https://github.com/status-im/nim-keccak-tiny.git >= 0.1.0",
          "https://github.com/status-im/nim-rlp.git >= 1.0.0",
-         "https://github.com/status-im/nim-ttmath >= 0.1.0"
+         "https://github.com/status-im/nim-ttmath >= 0.2.0"
 
 
 
--- tests/build_test.sh
+++ tests/build_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nim --passC:"-I." cpp tests/code_stream_test.nim
-nim --passC:"-I." cpp tests/gas_meter_test.nim
-nim --passC:"-I." cpp tests/memory_test.nim
-nim --passC:"-I." cpp tests/stack_test.nim
+nim cpp tests/code_stream_test.nim
+nim cpp tests/gas_meter_test.nim
+nim cpp tests/memory_test.nim
+nim cpp tests/stack_test.nim
