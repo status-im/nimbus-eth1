66882be592d21a3f59ae77f98bd8726a1d3ed813
mratsim <mamy_github@numforge.co>
2018-06-14 17:32:12 +0200

    memory: use a template again and avoid (?) a cstring-> string conversion

--- nimbus/vm/memory.nim
+++ nimbus/vm/memory.nim
@@ -56,9 +56,9 @@ proc write*(memory: var Memory, startPosition: Natural, value: openarray[byte]) 
   for z, b in value:
     memory.bytes[z + startPosition] = b
 
-proc write*(memory: var Memory, startPosition: Natural, size: Natural, value: cstring) {.inline.}=
-  memory.write(startPosition, ($value).toBytes)
+template write*(memory: var Memory, startPosition: Natural, size: Natural, value: cstring) =
+  memory.write(startPosition, value.toBytes)
   # TODO ~ O(n^3):
-  #  - there is a string allocation with $
+  #  - there is a string allocation with $ (?)
   #  - then a conversion to seq (= new allocation) with toBytes
   #  - then writing to memory
