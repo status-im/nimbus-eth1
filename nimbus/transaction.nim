c10da69d6e8aca341e70292d67ff883087373062
coffeepots <coffeepotsgh@gmail.com>
2018-08-31 15:15:31 +0100

    Update toSignature to use range instead of UInt256

--- nimbus/transaction.nim
+++ nimbus/transaction.nim
@@ -45,7 +45,7 @@ proc toSignature*(transaction: Transaction): Signature =
   bytes[0..31] = transaction.R.toByteArrayBE()
   bytes[32..63] = transaction.S.toByteArrayBE()
   # TODO: V will become a byte or range soon.
-  bytes[64] = cast[uint64](transaction.V.data.lo).uint8
+  bytes[64] = transaction.V
   initSignature(bytes)
 
 proc getSender*(transaction: Transaction, output: var EthAddress): bool =
