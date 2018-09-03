350bf7e6728c5a3a5900e87e960c5588f2b7ac99
coffeepots <coffeepotsgh@gmail.com>
2018-09-03 19:24:55 +0100

    Add hasCodeOrNonce

--- nimbus/db/state_db.nim
+++ nimbus/db/state_db.nim
@@ -138,3 +138,6 @@ proc getCode*(db: AccountStateDB, address: EthAddress): ByteRange =
 proc dumpAccount*(db: AccountStateDB, addressS: string): string =
   let address = addressS.parseAddress
   return fmt"{addressS}: Storage: {db.getStorage(address, 0.u256)}; getAccount: {db.getAccount address}"
+
+proc hasCodeOrNonce*(account: AccountStateDB, address: EthAddress): bool {.inline.} =
+  account.getNonce(address) != 0 or account.getCodeHash(address) != EMPTY_SHA3
