c3c84782a55a385252b918b7a750ae12815db1d3
coffeepots <coffeepotsgh@gmail.com>
2018-09-04 12:27:07 +0100

    Make generateAddress a func

--- nimbus/utils/addresses.nim
+++ nimbus/utils/addresses.nim
@@ -1,4 +1,4 @@
 import nimcrypto, eth_common, rlp
 
-proc generateAddress*(address: EthAddress, nonce: AccountNonce): EthAddress =
+func generateAddress*(address: EthAddress, nonce: AccountNonce): EthAddress =
   result[0..19] = keccak256.digest(rlp.encodeList(address, nonce).toOpenArray).data[12..31]
