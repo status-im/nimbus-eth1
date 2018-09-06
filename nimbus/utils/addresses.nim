7e02eedea058acbc3ef63001d7957ff46dbab1a6
coffeepots <coffeepotsgh@gmail.com>
2018-09-06 13:48:06 +0100

    Use toOpenArray in generateAddress

--- nimbus/utils/addresses.nim
+++ nimbus/utils/addresses.nim
@@ -1,4 +1,4 @@
 import nimcrypto, eth_common, rlp
 
 func generateAddress*(address: EthAddress, nonce: AccountNonce): EthAddress =
-  result[0..19] = keccak256.digest(rlp.encodeList(address, nonce).toOpenArray).data[12..31]
+  result[0..19] = keccak256.digest(rlp.encodeList(address, nonce).toOpenArray).data.toOpenArray(12, 31)
