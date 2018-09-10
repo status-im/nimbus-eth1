3d64c66b7a327b0cf25b1f09a5069e3543b8e992
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2018-09-10 11:44:07 +0300

    More state restoring

--- nimbus/db/state_db.nim
+++ nimbus/db/state_db.nim
@@ -64,6 +64,12 @@ proc setBalance*(db: var AccountStateDB, address: EthAddress, balance: UInt256) 
 proc increaseBalance*(db: var AccountStateDB, address: EthAddress, delta: UInt256) =
   db.setBalance(address, db.getBalance(address) + delta)
 
+proc addBalance*(db: var AccountStateDB, address: EthAddress, delta: UInt256) =
+  db.setBalance(address, db.getBalance(address) + delta)
+
+proc subBalance*(db: var AccountStateDB, address: EthAddress, delta: UInt256) =
+  db.setBalance(address, db.getBalance(address) - delta)
+
 template createTrieKeyFromSlot(slot: UInt256): ByteRange =
   # XXX: This is too expensive. Similar to `createRangeFromAddress`
   # Converts a number to hex big-endian representation including
