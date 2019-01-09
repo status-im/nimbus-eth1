a2a11ed7aa7247cf1521c9466535a29256f14bef
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2019-01-09 18:10:26 +0100

    document env var

--- env.sh
+++ env.sh
@@ -3,11 +3,13 @@
 rel_path="$(dirname $0)"
 abs_path="$(cd $rel_path; pwd)"
 
-#- make it an absolute path, so we can call this script from other dirs
+# make it an absolute path, so we can call this script from other dirs
 export PATH="${abs_path}/vendor/Nim/bin:$PATH"
 
-#- Nimble needs this to be an absolute path
+# Nimble needs this to be an absolute path
 export NIMBLE_DIR="${abs_path}/vendor/.nimble"
+
+# used by nim-beacon-chain/tests/simulation/start.sh
 export BUILD_OUTPUTS_DIR="${abs_path}/build"
 
 exec "$@"
