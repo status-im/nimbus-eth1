307163b8e262cfbd89870459f81a7bd380476bc7
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2019-02-26 23:13:31 +0100

    build p2pd and run some client tests
    (with `make V=0 test-libp2p-daemon`)

--- env.sh
+++ env.sh
@@ -3,8 +3,11 @@
 rel_path="$(dirname $0)"
 abs_path="$(cd $rel_path; pwd)"
 
+# used by libp2p/go-libp2p-daemon
+export GOPATH="${abs_path}/vendor/go"
+
 # make it an absolute path, so we can call this script from other dirs
-export PATH="${abs_path}/vendor/Nim/bin:$PATH"
+export PATH="${abs_path}/vendor/Nim/bin:${GOPATH}/bin:${PATH}"
 
 # Nimble needs this to be an absolute path
 export NIMBLE_DIR="${abs_path}/vendor/.nimble"
