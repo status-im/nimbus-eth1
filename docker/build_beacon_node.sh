d62eb2710a89bb8f4fe0aecf8df0da2d2d38e392
Zahary Karadjov <zahary@gmail.com>
2019-03-22 18:29:31 +0200

    WIP push all containers to a single repo / use docker tags

--- docker/build_beacon_node.sh
+++ docker/build_beacon_node.sh
@@ -2,10 +2,13 @@
 
 set -euv
 
+DOCKERHUB_REPO_NAME=statusteam/nimbus_beacon_node
+
 buildAndPush() {
   NETWORK=$1
   BACKEND=$2
-  CONTAINER_NAME=statusteam/beacon_node_${NETWORK}_${BACKEND}
+
+  CONTAINER_NAME=${DOCKERHUB_REPO_NAME}:${NETWORK}_${BACKEND}
 
   docker build -t $CONTAINER_NAME beacon_node \
     --build-arg network=$NETWORK \
