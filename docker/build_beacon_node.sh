2b26f5f2f47565efeebfcf0a70968541bd9b7f81
Zahary Karadjov <zahary@gmail.com>
2019-05-14 16:55:03 +0300

    Deploy only to the correct testnet depending on the current branch

--- docker/build_beacon_node.sh
+++ docker/build_beacon_node.sh
@@ -6,6 +6,12 @@ cd $(dirname "$0")
 
 export GIT_REVISION=$(git rev-parse HEAD)
 
+NETWORK=testnet0
+
+if [[ $(git rev-parse --abbrev-ref HEAD) == "devel" ]]; then
+  NETWORK=testnet1
+fi
+
 buildAndPush() {
   export NETWORK=$1
   export NETWORK_BACKEND=$2
@@ -13,8 +19,5 @@ buildAndPush() {
   (cd beacon_node && make push)
 }
 
-buildAndPush testnet0 rlpx
-buildAndPush testnet1 rlpx
-
+buildAndPush $NETWORK rlpx
 #buildAndPush testnet0 libp2p
-#buildAndPush testnet1 libp2p
