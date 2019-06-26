9296248013c2536dac6de00d6676b073ed8ee4a9
Oskar Thoren <ot@oskarthoren.com>
2019-06-26 12:43:36 +0800

    Compile secp

--- nimbus/build_status_api.sh
+++ nimbus/build_status_api.sh
@@ -4,7 +4,7 @@
 #rm -rf nimcache/
 #rm libnimbus_api.a
 
-../env.sh nim c --opt:speed --lineTrace:off -d:noCompileSecp --verbosity:2 status_api
+../env.sh nim c --opt:speed --lineTrace:off --verbosity:2 status_api
 
 gcc status_api.c ./libnimbus_api.a -lm -o xx
 
