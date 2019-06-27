4a649b8f58622fb37bb2dd0f7db8a69a3dc0db34
Oskar Thoren <ot@oskarthoren.com>
2019-06-27 19:14:03 +0800

    Build with debug info

--- nimbus/build_status_api.sh
+++ nimbus/build_status_api.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-#echo "[Cleaning up caches...]"
-#rm -rf nimcache/
-#rm libnimbus_api.a
+echo "[Cleaning up caches...]"
+# rm -rf nimcache/
+rm libnimbus_api.so
 
-../env.sh nim c --opt:speed --lineTrace:off --verbosity:2 status_api
+# ../env.sh nim c  --opt:speed --lineTrace:off --verbosity:2 status_api
+# gcc status_api.c ./libnimbus_api.so -lm -o xx
 
-gcc status_api.c ./libnimbus_api.so -lm -o xx
+# debug info
+../env.sh nim c --debuginfo --opt:speed --lineTrace:off --verbosity:2 status_api
+gcc status_api.c ./libnimbus_api.so -lm -g -o xx
 
 ./xx
-
