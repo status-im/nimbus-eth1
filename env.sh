032a07ff9be7f81318f9561b8276fef175ac9923
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2019-08-24 21:12:43 +0200

    bump submodules

--- env.sh
+++ env.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-source vendor/nimbus-build-system/scripts/env.sh
+# We use ${BASH_SOURCE[0]} instead of $0 to allow sourcing this file
+# and we fall back to a Zsh-specific special var to also support Zsh.
+REL_PATH="$(dirname ${BASH_SOURCE[0]:-${(%):-%x}})"
+ABS_PATH="$(cd ${REL_PATH}; pwd)"
+source ${ABS_PATH}/vendor/nimbus-build-system/scripts/env.sh
 
