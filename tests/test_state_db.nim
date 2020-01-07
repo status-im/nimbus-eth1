727b477fcab3ec878627be2ee5e8475e14e55a78
andri lim <jangko128@gmail.com>
2020-01-07 19:49:42 +0700

    [skip ci] add accounts cache test

--- tests/test_state_db.nim
+++ tests/test_state_db.nim
@@ -9,6 +9,8 @@ import  unittest2, eth/trie/[hexary, db],
         ../nimbus/db/state_db, stew/byteutils, eth/common,
         stew/ranges
 
+include ../nimbus/db/accounts_cache
+
 proc stateDBMain*() =
   suite "Account State DB":
     setup:
@@ -45,3 +47,68 @@ proc stateDBMain*() =
       stateDB.setCode(address, code.toRange)
       check stateDB.isDeadAccount(address) == true
       check stateDB.accountExists(address) == true
+
+    test "clone storage":
+      var x = RefAccount(
+        overlayStorage: initTable[UInt256, UInt256](),
+        originalStorage: newTable[UInt256, UInt256]()
+      )
+
+      x.overlayStorage[10.u256] = 11.u256
+      x.overlayStorage[11.u256] = 12.u256
+
+      x.originalStorage[10.u256] = 11.u256
+      x.originalStorage[11.u256] = 12.u256
+
+      var y = x.clone(cloneStorage = true)
+      y.overlayStorage[12.u256] = 13.u256
+      y.originalStorage[12.u256] = 13.u256
+
+      check 12.u256 notin x.overlayStorage
+      check 12.u256 in y.overlayStorage
+
+      check x.overlayStorage.len == 2
+      check y.overlayStorage.len == 3
+
+      check 12.u256 in x.originalStorage
+      check 12.u256 in y.originalStorage
+
+      check x.originalStorage.len == 3
+      check y.originalStorage.len == 3
+
+    test "accounts cache":
+      func initAddr(z: int): EthAddress =
+        result[^1] = z.byte
+
+      var ac = init(AccountsCache, memDB, emptyRlpHash, true)
+      var addr1 = initAddr(1)
+
+      check ac.isDeadAccount(addr1) == true
+      check ac.accountExists(addr1) == false
+      check ac.hasCodeOrNonce(addr1) == false
+
+      ac.setBalance(addr1, 1000.u256)
+      check ac.getBalance(addr1) == 1000.u256
+      ac.subBalance(addr1, 100.u256)
+      check ac.getBalance(addr1) == 900.u256
+      ac.addBalance(addr1, 200.u256)
+      check ac.getBalance(addr1) == 1100.u256
+
+      ac.setNonce(addr1, 1)
+      check ac.getNonce(addr1) == 1
+      ac.incNonce(addr1)
+      check ac.getNonce(addr1) == 2
+
+      var code = hexToSeqByte("0x0f572e5295c57f15886f9b263e2f6d2d6c7b5ec6").toRange
+      ac.setCode(addr1, code)
+      check ac.getCode(addr1) == code
+
+      ac.setStorage(addr1, 1.u256, 10.u256)
+      check ac.getStorage(addr1, 1.u256) == 10.u256
+      check ac.getCommittedStorage(addr1, 1.u256) == 0.u256
+
+      check ac.hasCodeOrNonce(addr1) == true
+      check ac.getCodeSize(addr1) == code.len
+      
+when isMainModule:
+  stateDBMain()
