55fb1294ed5f63a3b3e8b829f379a1eed6e1e315
kdeme <kim.demey@gmail.com>
2020-02-14 11:36:37 +0100

    Check all subscribed topics in quicksim + bump vendor/nim-eth

--- waku/quicksim.nim
+++ waku/quicksim.nim
@@ -11,16 +11,18 @@ template sourceDir: string = currentSourcePath.rsplit(DirSep, 1)[0]
 const sigWakuPath = &"{sourceDir}{DirSep}rpc{DirSep}wakucallsigs.nim"
 createRpcSigs(RpcHttpClient, sigWakuPath)
 
+const topicAmount = 100
+
 let
   trafficNode = newRpcHttpClient()
-  lightWakuNode = newRpcHttpClient()
   lightNode = newRpcHttpClient()
+  lightNode2 = newRpcHttpClient()
 
-waitFor lightWakuNode.connect("localhost", Port(8545))
-waitFor lightNode.connect("localhost", Port(8546))
+waitFor lightNode.connect("localhost", Port(8545))
+waitFor lightNode2.connect("localhost", Port(8546))
 waitFor trafficNode.connect("localhost", Port(8548))
 
-proc generateTopics(amount = 100): seq[waku_protocol.Topic] =
+proc generateTopics(amount = topicAmount): seq[waku_protocol.Topic] =
   var topic: waku_protocol.Topic
   for i in 0..<amount:
     if randomBytes(topic) != 4:
@@ -30,15 +32,15 @@ proc generateTopics(amount = 100): seq[waku_protocol.Topic] =
 let
   symKey = "0x0000000000000000000000000000000000000000000000000000000000000001"
   topics = generateTopics()
-  symKeyID = waitFor lightWakuNode.waku_addSymKey(symKey)
+  symKeyID = waitFor lightNode.waku_addSymKey(symKey)
   options = WhisperFilterOptions(symKeyID: some(symKeyID),
                                  topics: some(topics))
-  filterID = waitFor lightWakuNode.waku_newMessageFilter(options)
+  filterID = waitFor lightNode.waku_newMessageFilter(options)
 
-  symKeyID2 = waitFor lightNode.waku_addSymKey(symKey)
+  symKeyID2 = waitFor lightNode2.waku_addSymKey(symKey)
   options2 = WhisperFilterOptions(symKeyID: some(symKeyID2),
-                                 topics: some(topics))
-  filterID2 = waitFor lightNode.waku_newMessageFilter(options2)
+                                  topics: some(topics))
+  filterID2 = waitFor lightNode2.waku_newMessageFilter(options2)
 
   symkeyID3 = waitFor trafficNode.waku_addSymKey(symKey)
 
@@ -48,15 +50,27 @@ var message = WhisperPostMessage(symKeyID: some(symkeyID3),
                                 payload: "0x45879632".HexDataStr,
                                 powTime: 1.0,
                                 powTarget: 0.002)
-discard waitFor trafficNode.waku_post(message)
 
-var messages: seq[WhisperFilterMessage]
+info "Posting envelopes on all subscribed topics"
+for i in 0..<topicAmount:
+  message.topic = some(topics[i])
+  discard waitFor trafficNode.waku_post(message)
 
-# Check if the subscription for the topic works
-while messages.len == 0:
-  messages = waitFor lightWakuNode.waku_getFilterMessages(filterID)
-  waitFor sleepAsync(1000.milliseconds)
-info "Received test message", payload = messages[0].payload
+# Check if the subscription for the topics works
+
+waitFor sleepAsync(1000.milliseconds) # This is a bit brittle
+
+let
+  messages = waitFor lightNode.waku_getFilterMessages(filterID)
+  messages2 = waitFor lightNode2.waku_getFilterMessages(filterID2)
+
+if messages.len != topicAmount or messages2.len != topicAmount:
+  error "Light node did not receive envelopes on all subscribed topics",
+    lightnode1=messages.len, lightnode2=messages2.len
+  quit 1
+
+info "Received envelopes on all subscribed topics"
 
 # Generate test traffic on node
 discard waitFor trafficNode.wakusim_generateRandomTraffic(10_000)
+info "Started random traffic generation"
