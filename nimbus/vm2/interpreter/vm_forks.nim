b7bf84a71fbbcbe8ee133a78d39f05c284a4d5e1
Jordan Hrycaj <jordan@status.im>
2021-04-08 16:13:27 +0100

    added compiler flag sentinels to vm2 headers
    why:
      making sure that deep links into vm2 sources are configured properly. it
      is intended that only the vm_*.nim interface headers are allowed to
      source files in vm2. the sentinels just protect from coding errors.

--- nimbus/vm2/interpreter/vm_forks.nim
+++ nimbus/vm2/interpreter/vm_forks.nim
@@ -5,6 +5,11 @@
 #  * MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
 # at your option. This file may not be copied, modified, or distributed except according to those terms.
 
+when not defined(vm2_enabled):
+  {.fatal: "Flags \"vm2_enabled\" must be defined"}
+when defined(evmc_enabled):
+  {.fatal: "Flags \"evmc_enabled\" and \"vm2_enabled\" are mutually exclusive"}
+
 import stint, eth/common/eth_types
 
 type
