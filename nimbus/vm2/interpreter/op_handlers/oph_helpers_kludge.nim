e6eee3f3a6d7b73a2e2f66fcecfd01bad0eeb885
Jordan Hrycaj <jordan@status.im>
2021-04-22 19:12:16 +0100

    prepared v2state_transaction.nim so it can be compiled locally
    details:
      use the -d:kludge:1 flag for syntax check

--- nimbus/vm2/interpreter/op_handlers/oph_helpers_kludge.nim
+++ nimbus/vm2/interpreter/op_handlers/oph_helpers_kludge.nim
@@ -14,6 +14,7 @@
 
 import
   ../../../errors,
+  ../forks_list,
   ../op_codes,
   ./oph_defs_kludge,
   eth/common/eth_types,
@@ -31,6 +32,7 @@ const
   MaxCallDepth*     = 42
 
 # function stubs from state.nim
+proc `status=`*(v: BaseVMState; status: bool) = discard
 template mutateStateDB*(vmState: BaseVMState, body: untyped) =
   block:
     var db {.inject.} = vmState.accountDb
@@ -42,22 +44,28 @@ proc getBalance*[T](c: Computation, address: T): Uint256 = result
 proc accountExists*(c: Computation, address: EthAddress): bool = result
 
 # function stubs from computation.nim (to satisfy compiler logic)
+proc execCallOrCreate*(cParam: Computation) = discard
+proc refundSelfDestruct*(c: Computation) = discard
 func shouldBurnGas*(c: Computation): bool = result
+proc getGasRefund*(c: Computation): GasInt = result
 proc newComputation*[A,B](v:A, m:B, salt = 0.u256): Computation = new result
 proc isSuccess*(c: Computation): bool = result
+proc isOriginComputation*(c: Computation): bool = result
 proc merge*(c, child: Computation) = discard
 template chainTo*(c, d: Computation, e: untyped) =
   c.child = d; c.continuation = proc() = e
 
-# function stubs from accounts_cache.nim:
+# function stubs from accounts_cache.nim (some also match state_db.nim):
 func inAccessList*[A,B](ac: A; address: B): bool = false
 proc accessList*[A,B](ac: var A, address: B) = discard
+proc incNonce*[A,B](ac: var A, address: B) = discard
+proc addBalance*[A,B](ac: var A, address: B, delta: UInt256) = discard
 
 # function stubs from gas_meter.nim
 proc consumeGas*(gasMeter: var GasMeter;amount: GasInt;reason: string) = discard
 proc returnGas*(gasMeter: var GasMeter; amount: GasInt) = discard
 
-# stubs from gas_costs.nim
+# function stubs from gas_costs.nim
 type
   GasResult* =
      tuple[gasCost, gasRefund: GasInt]
@@ -72,8 +80,19 @@ type
       c_gasBalance*, c_currentMemSize*, c_memOffset*, c_memLength*: int64
     else:
       discard
+
 proc c_handler*(x: int; y: Uint256, z: GasParams): GasResult = result
 proc m_handler*(x: int; curMemSize, memOffset, memLen: int64): int = result
+proc forkToSchedule*(fork: Fork): GasCosts = result
+
+# function stubs from config.nim
+proc toFork*[T](c: T; number: BlockNumber): Fork = result
+
+# function stubs from transaction.nim
+proc intrinsicGas*(tx: Transaction, fork: Fork): GasInt = result
+
+# function stubs from message.nim
+proc isCreate*(message: Message): bool = result
 
 # ------------------------------------------------------------------------------
 # Kludge END
