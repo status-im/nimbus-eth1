status-im/nimbus-eth1

commit 2a7ccceb3ef6015232e9612584f467b67b3e28b0

EVM: Make continuation exceptions behave as they did before
The account database code is not supposed to raise exceptions in the EVM, and
the behaviour is not well defined if it does.  It isn't compliant with EVMC
spec either.  But that will be dealt with properly when the account state-cache
is dealt with, as there is some work to be done on it.

Meanwhile, if it raises in code under `chainTo` and then `(continuation)()`,
the behaviour was changed slightly by the stack-shrink patches.

Before those patches, an exception after the recursion-point was converted to
`c.setError` "Opcode Dispatch Error" in `executeOpcodes.  After, it would
propagate out, a different behaviour.  (It still correctly walked the chain of
`c.dispose()` calls to clean up.)

It's easy to restore the original behaviour just by moving the continuation
call, so let's do that.

Signed-off-by: Jamie Lokier <jamie@shareable.org>

nimbus/vm/interpreter_dispatch.nim
@@ -386,13 +386,13 @@ proc executeOpcodes(c: Computation) =
   let fork = c.fork
 
   block:
-    if not c.continuation.isNil:
-      (c.continuation)()
-      c.continuation = nil
-    elif c.execPrecompiles(fork):
+    if c.continuation.isNil and c.execPrecompiles(fork):
       break
 
     try:
+      if not c.continuation.isNil:
+        (c.continuation)()
+        c.continuation = nil
       c.selectVM(fork)
     except CatchableError as e:
       c.setError(&"Opcode Dispatch Error msg={e.msg}, depth={c.msg.depth}", true)

commit 537cac1bf5748c9280e3bbb0e6538c51174c0a31

EVM: Move where `continuation` is cleared to fix a potential stall
This fixes a bug spotted by @mjfh that was introduced by commit 2a7ccceb:

    try:
      if not c.continuation.isNil:
        (c.continuation)()
        c.continuation = nil
      c.selectVM(fork)
    except CatchableError as e:
      ...

The call to `(c.continuation)()` was moved by 2a7ccceb inside the `try` so
that, like all the Op functions do already, if the continuation raises, the
interpreter's general catch turns the exception into a an error status result.

But if the continuation raises an exception, `continuation` is not cleared in
the next line, and at the next resumption the continuation is called again.
It may loop doing this.

This doesn't currently happen because the continuations don't really raise, but
it's still a correctness issue.

This fix also allows a continuation to spawn a second continuation, if it
encounters a second suspension point.  This also doesn't happen currently,
but the pattern will become useful with async EVM.

Signed-off-by: Jamie Lokier <jamie@shareable.org>

nimbus/vm/computation.nim
@@ -307,6 +307,7 @@ proc afterExec(c: Computation) {.noinline.} =
 template chainTo*(c: Computation, toChild: typeof(c.child), after: untyped) =
   c.child = toChild
   c.continuation = proc() =
+    c.continuation = nil
     after
 
 when vm_use_recursion:

nimbus/vm/interpreter_dispatch.nim
@@ -395,7 +395,6 @@ proc executeOpcodes(c: Computation) =
     try:
       if not c.continuation.isNil:
         (c.continuation)()
-        c.continuation = nil
       c.selectVM(fork)
     except CatchableError as e:
       c.setError(&"Opcode Dispatch Error msg={e.msg}, depth={c.msg.depth}", true)