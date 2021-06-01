05bc174bef64bc90384fb5e71cb9f09a2a8c3aff
Jamie Lokier <jamie@shareable.org>
2021-06-01 11:02:50 +0100

    Forks: Use a common fork list outside the EVMs
    Many places outside the EVM use `Fork` and the fork list, and in general we
    want progressively fewer dependencies on EVM internal types and files.
    
    This may prove to be a temporary location, especially when we implement
    issue #640.  But it's a fine temporary location if so.
    
    Signed-off-by: Jamie Lokier <jamie@shareable.org>

---
+++ nimbus/forks.nim
@@ -0,0 +1,19 @@
+# Nimbus - Enumerate Eth1 forks
+#
+# Copyright (c) 2018 Status Research & Development GmbH
+# Licensed under either of
+#  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+#  * MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+# at your option. This file may not be copied, modified, or distributed except according to those terms.
+
+type
+  Fork* = enum
+    FkFrontier       = "frontier"
+    FkHomestead      = "homestead"
+    FkTangerine      = "tangerine whistle"
+    FkSpurious       = "spurious dragon"
+    FkByzantium      = "byzantium"
+    FkConstantinople = "constantinople"
+    FkPetersburg     = "petersburg"
+    FkIstanbul       = "istanbul"
+    FkBerlin         = "berlin"
