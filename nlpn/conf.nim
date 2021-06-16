aef7a25174da4442cbc925af9aef3aed3904c1d1
Kim De Mey <kim.demey@gmail.com>
2021-06-16 21:18:45 +0200

    Add nlpn (#714)
    * Add start of nlpn client
    
    * Fix some error handling in nlpn
    
    * Clean-up GOARCH from nlpn.yml and add vendor to include paths

---
+++ nlpn/conf.nim
@@ -0,0 +1,130 @@
+# Nimbus
+# Copyright (c) 2021 Status Research & Development GmbH
+# Licensed and distributed under either of
+#   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
+#   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
+# at your option. This file may not be copied, modified, or distributed except according to those terms.
+
+{.push raises: [Defect].}
+
+import
+  confutils, confutils/std/net, chronicles,
+  eth/keys, eth/net/nat, eth/p2p/discoveryv5/[enr, node]
+
+const
+  DefaultListenAddress* = (static ValidIpAddress.init("0.0.0.0"))
+  DefaultAdminListenAddress* = (static ValidIpAddress.init("127.0.0.1"))
+
+type
+  PortalCmd* = enum
+    noCommand
+
+  PortalConf* = object
+    logLevel* {.
+      defaultValue: LogLevel.DEBUG
+      desc: "Sets the log level"
+      name: "log-level" .}: LogLevel
+
+    udpPort* {.
+      defaultValue: 9009
+      desc: "UDP listening port"
+      name: "udp-port" .}: uint16
+
+    listenAddress* {.
+      defaultValue: DefaultListenAddress
+      desc: "Listening address for the Discovery v5 traffic"
+      name: "listen-address" }: ValidIpAddress
+
+    bootnodes* {.
+      desc: "ENR URI of node to bootstrap discovery with. Argument may be repeated"
+      name: "bootnode" .}: seq[Record]
+
+    nat* {.
+      desc: "Specify method to use for determining public address. " &
+            "Must be one of: any, none, upnp, pmp, extip:<IP>"
+      defaultValue: NatConfig(hasExtIp: false, nat: NatAny)
+      name: "nat" .}: NatConfig
+
+    enrAutoUpdate* {.
+      defaultValue: false
+      desc: "Discovery can automatically update its ENR with the IP address " &
+            "and UDP port as seen by other nodes it communicates with. " &
+            "This option allows to enable/disable this functionality"
+      name: "enr-auto-update" .}: bool
+
+    nodeKey* {.
+      desc: "P2P node private key as hex",
+      defaultValue: PrivateKey.random(keys.newRng()[])
+      name: "nodekey" .}: PrivateKey
+
+    metricsEnabled* {.
+      defaultValue: false
+      desc: "Enable the metrics server"
+      name: "metrics" .}: bool
+
+    metricsAddress* {.
+      defaultValue: DefaultAdminListenAddress
+      desc: "Listening address of the metrics server"
+      name: "metrics-address" .}: ValidIpAddress
+
+    metricsPort* {.
+      defaultValue: 8008
+      desc: "Listening HTTP port of the metrics server"
+      name: "metrics-port" .}: Port
+
+    rpcEnabled* {.
+      desc: "Enable the JSON-RPC server"
+      defaultValue: false
+      name: "rpc" }: bool
+
+    rpcPort* {.
+      desc: "HTTP port for the JSON-RPC service"
+      defaultValue: 8545
+      name: "rpc-port" }: Port
+
+    rpcAddress* {.
+      desc: "Listening address of the RPC server"
+      defaultValue: DefaultAdminListenAddress
+      name: "rpc-address" }: ValidIpAddress
+
+    case cmd* {.
+      command
+      defaultValue: noCommand .}: PortalCmd
+    of noCommand:
+      discard
+
+proc parseCmdArg*(T: type enr.Record, p: TaintedString): T
+    {.raises: [Defect, ConfigurationError].} =
+  if not fromURI(result, p):
+    raise newException(ConfigurationError, "Invalid ENR")
+
+proc completeCmdArg*(T: type enr.Record, val: TaintedString): seq[string] =
+  return @[]
+
+proc parseCmdArg*(T: type Node, p: TaintedString): T
+    {.raises: [Defect, ConfigurationError].} =
+  var record: enr.Record
+  if not fromURI(record, p):
+    raise newException(ConfigurationError, "Invalid ENR")
+
+  let n = newNode(record)
+  if n.isErr:
+    raise newException(ConfigurationError, $n.error)
+
+  if n[].address.isNone():
+    raise newException(ConfigurationError, "ENR without address")
+
+  n[]
+
+proc completeCmdArg*(T: type Node, val: TaintedString): seq[string] =
+  return @[]
+
+proc parseCmdArg*(T: type PrivateKey, p: TaintedString): T
+    {.raises: [Defect, ConfigurationError].} =
+  try:
+    result = PrivateKey.fromHex(string(p)).tryGet()
+  except CatchableError:
+    raise newException(ConfigurationError, "Invalid private key")
+
+proc completeCmdArg*(T: type PrivateKey, val: TaintedString): seq[string] =
+  return @[]
---
+++ nlpn/nlpn.nim
@@ -0,0 +1,66 @@
+# Nimbus
+# Copyright (c) 2021 Status Research & Development GmbH
+# Licensed and distributed under either of
+#   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
+#   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
+# at your option. This file may not be copied, modified, or distributed except according to those terms.
+
+{.push raises: [Defect].}
+
+import
+  confutils, confutils/std/net, chronicles, chronicles/topics_registry,
+  chronos, metrics, metrics/chronos_httpserver,
+  eth/keys, eth/net/nat,
+  eth/p2p/discoveryv5/protocol as discv5_protocol,
+  eth/p2p/portal/protocol as portal_protocol,
+  ./conf
+
+proc run(config: PortalConf) {.raises: [CatchableError, Defect].} =
+  let
+    rng = newRng()
+    bindIp = config.listenAddress
+    udpPort = Port(config.udpPort)
+    # TODO: allow for no TCP port mapping!
+    (extIp, _, extUdpPort) =
+      try: setupAddress(config.nat,
+        config.listenAddress, udpPort, udpPort, "dcli")
+      except CatchableError as exc: raise exc
+      # TODO: Ideally we don't have the Exception here
+      except Exception as exc: raiseAssert exc.msg
+
+  let d = newProtocol(config.nodeKey,
+          extIp, none(Port), extUdpPort,
+          bootstrapRecords = config.bootnodes,
+          bindIp = bindIp, bindPort = udpPort,
+          enrAutoUpdate = config.enrAutoUpdate,
+          rng = rng)
+
+  d.open()
+
+  let portal = PortalProtocol.new(d)
+
+  if config.metricsEnabled:
+    let
+      address = config.metricsAddress
+      port = config.metricsPort
+    notice "Starting metrics HTTP server",
+      url = "http://" & $address & ":" & $port & "/metrics"
+    try:
+      chronos_httpserver.startMetricsHttpServer($address, port)
+    except CatchableError as exc: raise exc
+    # TODO: Ideally we don't have the Exception here
+    except Exception as exc: raiseAssert exc.msg
+
+  d.start()
+
+  runForever()
+
+when isMainModule:
+  {.pop.}
+  let config = PortalConf.load()
+  {.push raises: [Defect].}
+
+  setLogLevel(config.logLevel)
+
+  case config.cmd
+  of noCommand: run(config)
