f0637cfbfccddeaa392e63a950cb9d0fd78de27b
KonradStaniec <konrad.staniec@gmail.com>
2021-08-27 18:04:55 +0200

    Add rpc for local node info (#810)
    * Add rpc for local node info
    
    * Group let statement

--- fluffy/tests/test_content_network.nim
+++ fluffy/tests/test_content_network.nim
@@ -9,6 +9,7 @@ import
   std/os,
   testutils/unittests,
   eth/[keys, trie/db, trie/hexary, ssz/ssz_serialization],
+  eth/p2p/discoveryv5/protocol as discv5_protocol,
   ../../nimbus/[genesis, chain_config, db/db_chain],
   ../network/state/portal_protocol, ../network/state/content,
   ./test_helpers
@@ -73,3 +74,5 @@ procSuite "Content Network":
 
       let hash = hexary.keccak(foundContent.get().payload.asSeq())
       check hash.data == key
+    await node1.closeWait()
+    await node2.closeWait()
