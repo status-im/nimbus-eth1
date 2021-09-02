df3e7bb368cdab0b7ea0c023c6db380b98787ff1
KonradStaniec <konrad.staniec@gmail.com>
2021-09-02 14:35:25 +0200

    Add content lookup function (#807)
    * Add content lookup function
    
    * Pr comments
    
    Refactor contentLookup function
    Add additional logginf for protocol breaches
    in find content responses

--- fluffy/tests/test_content_network.nim
+++ fluffy/tests/test_content_network.nim
@@ -9,7 +9,7 @@ import
   std/os,
   testutils/unittests,
   eth/[keys, trie/db, trie/hexary, ssz/ssz_serialization],
-  eth/p2p/discoveryv5/protocol as discv5_protocol,
+  eth/p2p/discoveryv5/protocol as discv5_protocol, eth/p2p/discoveryv5/routing_table,
   ../../nimbus/[genesis, chain_config, db/db_chain],
   ../network/state/portal_protocol, ../network/state/content,
   ./test_helpers
@@ -74,5 +74,58 @@ procSuite "Content Network":
 
       let hash = hexary.keccak(foundContent.get().payload.asSeq())
       check hash.data == key
+
     await node1.closeWait()
     await node2.closeWait()
+
+  asyncTest "Find content in the network via content lookup":
+    let
+      node1 = initDiscoveryNode(
+        rng, PrivateKey.random(rng[]), localAddress(20302))
+      node2 = initDiscoveryNode(
+        rng, PrivateKey.random(rng[]), localAddress(20303))
+      node3 = initDiscoveryNode(
+        rng, PrivateKey.random(rng[]), localAddress(20304))
+
+
+      proto1 = PortalProtocol.new(node1)
+      proto2 = PortalProtocol.new(node2)
+      proto3 = PortalProtocol.new(node3)
+
+    let trie =
+      genesisToTrie("fluffy" / "tests" / "custom_genesis" / "chainid7.json")
+
+    proto3.contentStorage = ContentStorage(trie: trie)
+
+    # Node1 knows about Node2, and Node2 knows about Node3 which hold all content
+    check proto1.addNode(proto2.baseProtocol.localNode) == Added
+    check proto2.addNode(proto3.baseProtocol.localNode) == Added
+
+    check (await proto2.ping(proto3.baseProtocol.localNode)).isOk()
+
+    var keys: seq[seq[byte]]
+    for k, v in trie.replicate:
+      keys.add(k)
+
+    # Get first key
+    var nodeHash: NodeHash
+    let firstKey = keys[0]
+    copyMem(nodeHash.data.addr, unsafeAddr firstKey[0], sizeof(nodeHash.data))
+
+    let contentKey = ContentKey(
+      networkId: 0'u16,
+      contentType: content.ContentType.Account,
+      nodeHash: nodeHash)
+
+    let foundContent = await proto1.contentLookup(contentKey)
+
+    check:
+      foundContent.isSome()
+
+    let hash = hexary.keccak(foundContent.get().asSeq())
+
+    check hash.data == firstKey
+
+    await node1.closeWait()
+    await node2.closeWait()
+    await node3.closeWait()
