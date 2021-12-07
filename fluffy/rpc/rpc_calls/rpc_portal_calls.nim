92c2cf76eb12cc247247ee748b6064228324b122
kdeme <kim.demey@gmail.com>
2021-12-07 15:43:19 +0100

    Add resolve call for Portal networks
    And:
    - Refactor some code by adding a findNodeVerified call
    - add the portal network lookup json-rpc call that uses resolve
    - add usage of this lookup in the portal testnet tests
    - Additional comments

--- fluffy/rpc/rpc_calls/rpc_portal_calls.nim
+++ fluffy/rpc/rpc_calls/rpc_portal_calls.nim
@@ -1,9 +1,11 @@
 ## Portal State Network json-rpc calls
 proc portal_state_nodeInfo(): NodeInfo
 proc portal_state_routingTableInfo(): RoutingTableInfo
+proc portal_state_lookupEnr(nodeId: NodeId): Record
 proc portal_state_recursiveFindNodes(): seq[Record]
 
 ## Portal History Network json-rpc calls
 proc portal_history_nodeInfo(): NodeInfo
 proc portal_history_routingTableInfo(): RoutingTableInfo
+proc portal_history_lookupEnr(nodeId: NodeId): Record
 proc portal_history_recursiveFindNodes(): seq[Record]
