38593f2ef222fb49a1abc3cbdecd1ed0a1b29bc5
Jordan Hrycaj <jordan@status.im>
2021-12-10 15:41:16 +0000

    Simplify packer loop
    why:
      Fixed the db/accounts-cache transaction framework. Also, the persist()
      call is not needed (caused crashes earlier because of incorrect usage.)
    
    also:
      rebased to current master

--- nimbus/utils/tx_pool/tx_tasks/tx_packer.nim
+++ nimbus/utils/tx_pool/tx_tasks/tx_packer.nim
@@ -97,14 +97,6 @@ template safeExecutor(info: string; code: untyped) =
     let e = getCurrentException()
     raise newException(TxPackerError, info & "(): " & $e.name & " -- " & e.msg)
 
-proc gasBurned(pst: TxPackerStateRef): GasInt =
-  ## To be used instead of `vmState.cumulativeGasUsed` which is ignored as the
-  ## same value is available as `vmState.receipts[inx-1].cumulativeGasUsed`.
-  ## This makes it handy for transparently picking up after a rollback.
-  let inx = pst.xp.txDB.byStatus.eq(txItemPacked).nItems
-  if 0 < inx:
-    result = pst.vmState.receipts[inx-1].cumulativeGasUsed
-
 # ------------------------------------------------------------------------------
 # Private functions
 # ------------------------------------------------------------------------------
@@ -149,14 +141,15 @@ proc runTxCommit(pst: TxPackerStateRef; item: TxItemRef; gasBurned: GasInt)
   if vmState.generateWitness:
     vmState.stateDB.collectWitnessData()
 
-  # Commit accounts
-  vmState.stateDB.persist(clearCache = false)
+  # # Commit accounts => not needed
+  # vmState.stateDB.persist(clearCache = false)
 
   # Update receipts sequence
   if vmState.receipts.len <= inx:
     vmState.receipts.setLen(inx + receiptsExtensionSize)
 
-  vmState.cumulativeGasUsed = pst.gasBurned + gasBurned
+  # vmState.cumulativeGasUsed = pst.gasBurned + gasBurned
+  vmState.cumulativeGasUsed += gasBurned
   vmState.receipts[inx] = vmState.makeReceipt(item.tx.txType)
 
   # Update txRoot
@@ -170,130 +163,38 @@ proc runTxCommit(pst: TxPackerStateRef; item: TxItemRef; gasBurned: GasInt)
 # Private functions: packer loop
 # ------------------------------------------------------------------------------
 
-#[
-proc collectAccount(pst: TxPackerStateRef; nonceList: TxStatusNonceRef): bool
-    {.gcsafe,raises: [Defect,CatchableError].}  =
-  ## Make sure that the full account list goes into the block. Otherwise
-  ## proceed with per-item transaction/step-wise mode, below.
-  let dbTx = pst.xp.chain.db.db.beginTransaction
-
-  # Keep a list of items added. It will be used either for rollback,
-  # or for the account-wise update of the txRoot.
-  var
-    allFuel = 0.GasInt
-    topOfList = 0
-    itemList = newSeq[TxItemRef](nonceList.len)
-
-  # Collect items and pack
-  for item in nonceList.incNonce:
-    let
-      accTx = pst.vmState.stateDB.beginSavepoint
-      totalGas = pst.gasBurned
-      gasUsed = pst.runTx(item)
-
-    if not pst.xp.classifyPacked(totalGas, gasUsed):
-      # Roll back databases
-      pst.vmState.stateDB.rollback(accTx)
-      dbTx.rollback()
-
-      # Undo collecting items for this account
-      for n in 0 ..< topOfList:
-        discard pst.xp.txDB.reassign(itemList[n],txItemStaged)
-      say "collectAccount",
-        " item[", topOfList, "] too big ", item.pp,
-        " size=", totalGas, "+", gasUsed,
-        " => rollback"
-      return false
-
-    # Commit account state DB
-    pst.vmState.stateDB.commit(accTx)
-
-    # Finish book-keeping and move item to `packed` bucket
-    pst.runTxCommit(item, gasUsed)
-    say "collectAccount", " item", item.pp, " => accepted"
-
-    # Locally accumulate total gas
-    allFuel += gasUsed
-
-    # Collect item for post-processing and/or rollback
-    itemList[topOfList] = item
-    topOfList.inc
-
-  # Accept account items
-  dbTx.commit
-
-  say "collectAccount",
-    " ", itemList[0].sender.pp,
-    " baseFee=", pst.xp.chain.head.baseFee,
-    " len=", itemList.len, " => OK"
-  true
-#]#
-
-proc collectItem(pst: TxPackerStateRef; item: TxItemRef): bool
-    {.gcsafe,raises: [Defect,CatchableError].}  =
-  ## Collect single item in its own transaction frame.
-  ##
-  ## if the return code is `false`, the descriptor variable `pst.stop` is
-  ## reset to `false` as a suggestion to continue packing the next account,
-  ## and `true` for termination.
-  let
-    xp = pst.xp
-    vmState = pst.vmState
-
-  let dbTx = xp.chain.db.db.beginTransaction
-  defer: dbTx.dispose
-
-  let accTx = vmState.stateDB.beginSavepoint
-  defer: vmState.stateDB.dispose(accTx)
-
-  let
-    totalGas = pst.gasBurned
-    gasUsed = pst.runTx(item)
-  if not xp.classifyPacked(totalGas, gasUsed):
-    pst.stop = not xp.classifyPackedNext(totalGas, gasUsed)
-    # otherwise rollback, and stop
-    return false
-
-  # Commit account state DB
-  vmState.stateDB.commit(accTx)
-
-  # Finish book-keeping and move item to `packed` bucket
-  pst.runTxCommit(item, gasUsed)
-
-  # Accept single item
-  dbTx.commit
-  true
-
-
 proc packerLoop(pst: TxPackerStateRef)
     {.gcsafe,raises: [Defect,CatchableError].}  =
   ## Greedily compact items as long as the accumulated `gasLimit` values
   ## are below the block size
+  let
+    xp = pst.xp
+    vmState = pst.vmState
 
   # Flush `packed` bucket
-  pst.xp.bucketFlushPacked
+  xp.bucketFlushPacked
 
   # Select items and move them to the `packed` bucket
-  for (account,nonceList) in pst.xp.txDB.decAccount(txItemStaged):
-
-    #[
-    # For the given account, try to execute all items of this account
-    # within a single transaction frame.
-    #
-    # say "packerVmExec account=", account.pp
-    if pst.collectAccount(nonceList):
-      continue
-    #]#
-
-    # Otherwise execute items individually, each one within its own
-    # transaction frame.
+  for (_,nonceList) in pst.xp.txDB.decAccount(txItemStaged):
     for item in nonceList.incNonce:
       say "packerVmExec item=", item.pp
-      if not pst.collectItem(item):
-        if pst.stop:
-          return
-        # stop inner loop and continue with next account
-        break
+
+      let
+        accTx = vmState.stateDB.beginSavepoint
+        totalGas = vmState.cumulativeGasUsed
+        gasUsed = pst.runTx(item)
+
+      if not xp.classifyPacked(totalGas, gasUsed):
+        vmState.stateDB.rollback(accTx)
+        if xp.classifyPackedNext(totalGas, gasUsed):
+          break # continue with next account
+        return  # stop
+
+      # Commit account state DB
+      vmState.stateDB.commit(accTx)
+
+      # Finish book-keeping and move item to `packed` bucket
+      pst.runTxCommit(item, gasUsed)
 
 # ------------------------------------------------------------------------------
 # Public functions
