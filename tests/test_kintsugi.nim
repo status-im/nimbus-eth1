93a52e5f7e9d52852e2d9979de500dac2f2cdd52
Jordan Hrycaj <jordan@status.im>
2022-01-21 18:20:55 +0000

    different tests for OS variants

--- tests/test_kintsugi.nim
+++ tests/test_kintsugi.nim
@@ -16,6 +16,8 @@ import
   unittest2
 
 const
+  isLinux32bit = defined(linux) and int.sizeof == 4
+
   baseDir = [".", "tests", ".." / "tests", $DirSep] # path containg repo
   repoDir = ["status", "replay"]                    # alternative repo paths
   jFile = "nimbus_kintsugi.json"
@@ -35,12 +37,12 @@ proc findFilePath(file: string): string =
 proc flushDbDir(s: string) =
   let dataDir = s / "nimbus"
   if (dataDir / "data").dirExists:
-    dataDir.removeDir
+    # Typically under Windows: there might be stale file locks.
+    try: dataDir.removeDir except: discard
 
 # ------------------------------------------------------------------------------
 # Private functions
 # ------------------------------------------------------------------------------
-
 
 # ------------------------------------------------------------------------------
 # Test Runner
@@ -50,15 +52,28 @@ proc runner(noisy = true; file = jFile) =
   let
     fileInfo = file.splitFile.name.split(".")[0]
     filePath = file.findFilePath
-    tmpDir = filePath.splitFile.dir / "tmp"
-  var
-    mdb, ddb: BaseChainDB
-  defer:
-    tmpDir.flushDbDir
-    discard
+
+  # There is a crash problem with the persistent BaseChainDB initialisation
+  # and clean up on the Github/CI Linux/i386 engines running Ubuntu 18.04.06.
+  # It will result in spurious segfaults for some reason.
+  #
+  # This could not be reproduced on a virtual Qemu machine running
+  # Debian/bullseye i386, see also
+  # https://github.com/status-im/nimbus-eth2/issues/3121, some observations
+  # similar to this one.
+  when isLinux32bit:
+    let tmpDir = "*notused*"
+  else:
+    let tmpDir = filePath.splitFile.dir / "tmp"
+    defer: tmpDir.flushDbDir
 
   suite &"Kintsugi test scenario":
-    var params: NetworkParams
+    var
+      params: NetworkParams
+      mdb: BaseChainDB
+
+    when not isLinux32bit:
+      var ddb: BaseChainDB
 
     test &"Load params from {fileInfo}":
       check filePath.loadNetworkParams(params)
@@ -70,17 +85,42 @@ proc runner(noisy = true; file = jFile) =
         params = params)
 
     test &"Construct persistent BaseChainDB on {tmpDir}":
-      ddb = newBaseChainDB(
-        tmpDir.newChainDb.trieDB,
-        id = params.config.chainID.NetworkId,
-        params = params)
+      when isLinux32bit:
+        # Crazy enough, on the Github/CI Linux/i386 engines running
+        # Ubuntu 18.04.06, some of the VM variants crash already if
+        # the constructor below is present without even applying it
+        # (e.g. as `ddb.initializeEmptyDb`.)
+        skip()
+      else:
+        # Before allocating the database, the data directory needs to be
+        # cleared. There might be left overs from a previous crash or
+        # because there were file locks under Windows which prevented a
+        # previous clean up.
+        tmpDir.flushDbDir
 
-    test &"Initialise in-memory Gensis":
+        # The effect of this constructor is roughly equivalent to the command
+        # line invocation of nimbus as
+        #
+        #    nimbus \
+        #       --data-dir:$tmpDir \
+        #       --custom-network:$filePath \
+        #       --prune-mode:full ...
+        #
+        # as described in https://github.com/status-im/nimbus-eth1/issues/932.
+        ddb = newBaseChainDB(
+          tmpDir.newChainDb.trieDB,
+          id = params.config.chainID.NetworkId,
+          params = params)
+
+    test "Initialise in-memory Genesis":
       mdb.initializeEmptyDb
 
-    test &"Initialise persistent Gensis, expect AssertionError":
-      expect AssertionError:
-        ddb.initializeEmptyDb
+    test "Initialise persistent Genesis, expect AssertionError":
+      when isLinux32bit:
+        skip()
+      else:
+        expect AssertionError:
+          ddb.initializeEmptyDb
 
 # ------------------------------------------------------------------------------
 # Main function(s)
