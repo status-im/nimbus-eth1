757bfabb4820c6569bc5103f51a525312b723f6e
Jordan Hrycaj <jordan@status.im>
2022-06-06 09:09:52 +0100

    Non-adjacent intervals set management
    why:
      extracted from snap as separate helper utility
    
    also:
      should go to `stew`, later on

--- tests/all_tests.nim
+++ tests/all_tests.nim
@@ -14,6 +14,7 @@ cliBuilder:
           ./test_accounts_cache,
           ./test_custom_network,
           ./test_jwt_auth,
+          ./test_interval_set,
           ./test_gas_meter,
           ./test_memory,
           ./test_stack,
