2c0a8fa26d8a49bfac3a5102a3daf7ffdbf29682
jangko <jangko128@gmail.com>
2022-07-28 09:19:57 +0700

    update to supported macOS in Github CI

--- hive_integration/nodocker/build_sims.sh
+++ hive_integration/nodocker/build_sims.sh
@@ -7,12 +7,17 @@ USE_SYSTEM_NIM=1
 ENV_SCRIPT="vendor/nimbus-build-system/scripts/env.sh"
 
 # nimbus_db_backend:none -> we only use memory db in simulators
-NIM_FLAGS="c -r -d:release -d:disable_libbacktrace -d:nimbus_db_backend:none"
+NIM_FLAGS="c -d:release -d:disable_libbacktrace -d:nimbus_db_backend:none"
 
 ${ENV_SCRIPT} nim ${NIM_FLAGS} ${SIM_DIR}/engine/engine_sim
 ${ENV_SCRIPT} nim ${NIM_FLAGS} ${SIM_DIR}/consensus/consensus_sim
 ${ENV_SCRIPT} nim ${NIM_FLAGS} ${SIM_DIR}/graphql/graphql_sim
 ${ENV_SCRIPT} nim ${NIM_FLAGS} ${SIM_DIR}/rpc/rpc_sim
+
+${SIM_DIR}/engine/engine_sim
+${SIM_DIR}/consensus/consensus_sim
+${SIM_DIR}/graphql/graphql_sim
+${SIM_DIR}/rpc/rpc_sim
 
 echo "## ${1}" > simulators.md
 cat engine.md consensus.md graphql.md rpc.md >> simulators.md
