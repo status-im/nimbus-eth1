cb2100aabc44c521182dd7f278ee0302d5be3930
Adam Spitz <adamspitz@status.im>
2023-01-21 14:43:09 +0100

    Updated to the latest nim-eth, nim-rocksdb, nim-web3.

--- nimbus/db/kvstore_rocksdb.nim
+++ nimbus/db/kvstore_rocksdb.nim
@@ -26,8 +26,11 @@ proc put*(db: RocksStoreRef, key, value: openArray[byte]): KvResult[void] =
 proc contains*(db: RocksStoreRef, key: openArray[byte]): KvResult[bool] =
   db.store.contains(key)
 
-proc del*(db: RocksStoreRef, key: openArray[byte]): KvResult[void] =
+proc del*(db: RocksStoreRef, key: openArray[byte]): KvResult[bool] =
   db.store.del(key)
+
+proc clear*(db: RocksStoreRef): KvResult[bool] =
+  db.store.clear()
 
 proc close*(db: RocksStoreRef) =
   db.store.close
