c20397ae85bbf4a33db2d82724e17062453302c7
Jordan Hrycaj <jordan@status.im>
2023-02-27 15:58:54 +0000

    using asan
    prepare:
      Please comment out `{.passc: "-DUSE_ASM_X86_64"}` in file
      `nim-secp256k1/secp256k1/abi.nim`
    
      Then run `make nimbus` before using this test suite.

--- gabbleblotchit.mk
+++ gabbleblotchit.mk
@@ -13,15 +13,16 @@ BUILD_ARGS += -o:build/$(TARGET) nimbus/$(TARGET).nim
 RELEASE_FLAGS = -d:release
 
 DEBUG_FLAGS = --debugger:native --debuginfo:on
-DEBUG_FLAGS += --opt:none -d:useGcAssert -d:memProfiler
+DEBUG_FLAGS += --opt:none # -d:useGcAssert -d:memProfiler
+DEBUG_FLAGS += --passC:-fsanitize=address --passC:-fno-omit-frame-pointer
+DEBUG_FLAGS += --passL:-fsanitize=address
 
 BOEHM_FLAGS = $(DEBUG_FLAGS) --mm:boehm -d:boehm_enabled
-NOBOEHM_FLAGS = $(DEBUG_FLAGS)
 
-OTHER_FLAGS =
-ifeq ($(SWAP_SYNC_REFS),1)
-OTHER_FLAGS += -d:swap_sync_refs
-endif
+NOBOEHM_FLAGS = $(DEBUG_FLAGS) -d:default_enabled
+
+NIM_ENV = ./env.sh
+RUN_ENV = ASAN_OPTIONS=detect_leaks=0
 
 # ----------------
 .PHONY: default help
@@ -34,17 +35,16 @@ help::
 	echo
 	echo "Usage: $(MAKE) -f $(MAKEFILE) <target> <option>"
 	echo
-	echo "<target>: help             -- this help page"
+	echo "<target>: help      -- this help page"
 	echo
-	echo "          release          -- build release version"
-	echo "          boehm            -- build test version with safe gc"
-	echo "          noboehm          -- build test version with unsafe default gc"
-	echo "          run              -- run previously built version"
+	echo "          release   -- build release version"
+	echo "          boehm     -- build test version with safe gc"
+	echo "          noboehm   -- build test version with unsafe default gc"
+	echo "          run       -- run previously built version"
 	echo
-	echo "          brun             -- same as double targets boehm and run"
-	echo "          nobrun           -- same as double targets noboehm and run"
+	echo "          brun      -- same as double targets boehm and run"
+	echo "          nobrun    -- same as double targets noboehm and run"
 	echo
-	echo "<option>: SWAP_SYNC_REFS=1 -- swap descriptors in NimbusNode object"
 
 # ----------------
 .PHONY: release boehm brun noboehm nobrun run
@@ -55,11 +55,11 @@ release:
 
 boehm brun::
 	@echo "*** Compiling for safe boehm gc"
-	./env.sh nim c $(OTHER_FLAGS) $(BOEHM_FLAGS) $(BUILD_ARGS)
+	$(NIM_ENV) nim c $(OTHER_FLAGS) $(BOEHM_FLAGS) $(BUILD_ARGS)
 
 noboehm nobrun::
 	@echo "*** Compiling for probably crashing gc"
-	./env.sh nim c $(OTHER_FLAGS) $(NOBOEHM_FLAGS) $(BUILD_ARGS)
+	$(NIM_ENV) nim c $(OTHER_FLAGS) $(NOBOEHM_FLAGS) $(BUILD_ARGS)
 
 brun nobrun run::
 	@test -x ./build/$(TARGET) || {\
@@ -67,7 +67,7 @@ brun nobrun run::
 	  false; }
 
 brun nobrun run::
-	./build/$(TARGET) --sync-mode=full --discovery=none
+	$(RUN_ENV) ./build/$(TARGET) --sync-mode=full --discovery=none
 
 # ----------------
 .PHONY: clean distclean clobber
--- nimbus/gabbleblotchit.nim
+++ nimbus/gabbleblotchit.nim
@@ -10,87 +10,20 @@
 import
   ../nimbus/vm_compile_info
 
-when defined(swap_sync_refs):
-  {.warning: "Swapped positions of snapSyncRef and fullSyncRef".}
-
 import
-  std/[os, net],
-  chronicles,
-  chronos,
-  eth/[keys, p2p/bootnodes],
-  eth/p2p as eth_p2p,
-  stew/shims/net as stewNet,
+  std/os,
   ./common,
   ./db/select_backend,
-  ./core/chain,
-  ./sync/[full, protocol, snap]
+  ./core/chain
 
 type
-  NimbusState = enum
-    Starting, Running, Stopping
-
   NimbusNode = ref object
-    ethNode: EthereumNode
-    state: NimbusState
-    ctx: EthContext
-    chainRef: ChainRef
     dbBackend: ChainDB
-    when defined(swap_sync_refs):
-      fullSyncRef: FullSyncRef
-      snapSyncRef: SnapSyncRef
-    else:
-      snapSyncRef: SnapSyncRef # << reverse these two and the systems ..
-      fullSyncRef: FullSyncRef # << .. survives without crashing
 
-const
-  CONFIG_MAX_PEERS = 25
-  CONFIG_AGENT_STRING = "Wen dowego afoot tomann abode stomen forest cooryin"
-  CONFIG_LOG_LEVEL = LogLevel.INFO
 let
   CONFIG_DATA_DIR = getHomeDir() / ".cache" / "nimbus"
 
-proc basicServices(nimbus: NimbusNode,
-                   com: CommonRef) =
-  # chainRef: some name to avoid module-name/filed/function misunderstandings
-  nimbus.chainRef = newChain(com)
-
-proc setupP2P(nimbus: NimbusNode) =
-  ## Creating P2P Server
-  let kpres = nimbus.ctx.getNetKeys("random", CONFIG_DATA_DIR)
-  if kpres.isErr:
-    fatal "Get network keys error", msg = kpres.error
-    quit(QuitFailure)
-
-  let keypair = kpres.get()
-  var address = Address(
-    ip: ValidIpAddress.init("0.0.0.0"),
-    tcpPort: Port(30303),
-    udpPort: Port(30303)
-  )
-
-  var bootstrapNodes: seq[ENode]
-  for item in MainnetBootnodes:
-    bootstrapNodes.add ENode.fromString(item).tryGet()
-
-  nimbus.ethNode = newEthereumNode(
-    keypair, address, MainNet, CONFIG_AGENT_STRING,
-    addAllCapabilities = false, minPeers = CONFIG_MAX_PEERS,
-    bootstrapNodes = bootstrapNodes,
-    bindUdpPort = Port(30303), bindTcpPort = Port(30303),
-    bindIp =  ValidIpAddress.init("0.0.0.0"),
-    rng = nimbus.ctx.rng)
-
-  # Early-initialise "--snap-sync" before starting any network connections.
-  block:
-    block:
-      nimbus.fullSyncRef = FullSyncRef.init(
-        nimbus.ethNode, nimbus.chainRef, nimbus.ctx.rng, CONFIG_MAX_PEERS,
-        true)
-
 proc start(nimbus: NimbusNode) =
-  ## logging
-  setLogLevel(CONFIG_LOG_LEVEL)
-
   createDir(CONFIG_DATA_DIR)
   nimbus.dbBackend = newChainDB(CONFIG_DATA_DIR)
   let trieDB = trieDB nimbus.dbBackend
@@ -100,65 +33,19 @@ proc start(nimbus: NimbusNode) =
     MainNet.networkParams
     )
 
-  com.initializeEmptyDb()
-
-  block:
-    basicServices(nimbus, com)
-    setupP2P(nimbus)
-
-    block:
-      block:
-        nimbus.fullSyncRef.start
-
-    if nimbus.state == Starting:
-      # it might have been set to "Stopping" with Ctrl+C
-      nimbus.state = Running
-
-proc stop*(nimbus: NimbusNode) {.async, gcsafe.} =
-  trace "Graceful shutdown"
-  if nimbus.fullSyncRef.isNil.not:
-    nimbus.fullSyncRef.stop()
-
-proc process*(nimbus: NimbusNode) =
-  # Main event loop
-  while nimbus.state == Running:
-    try:
-      poll()
-    except CatchableError as e:
-      debug "Exception in poll()", exc = e.name, err = e.msg
-      discard e # silence warning when chronicles not activated
-
-  # Stop loop
-  waitFor nimbus.stop()
 
 when isMainModule:
-  var nimbus = NimbusNode(state: Starting, ctx: newEthContext())
-
-  ## Ctrl+C handling
-  proc controlCHandler() {.noconv.} =
-    when defined(windows):
-      # workaround for https://github.com/nim-lang/Nim/issues/4057
-      setupForeignThreadGc()
-    nimbus.state = Stopping
-    echo "\nCtrl+C pressed. Waiting for a graceful shutdown."
-  setControlCHook(controlCHandler)
-
-  ## Show logs on stdout until we get the user's logging choice
-  discard defaultChroniclesStream.output.open(stdout)
+  var nimbus = NimbusNode()
 
   # Print some information about the test
-  echo "*** gabbleblotchit: Ignoring command line arguments"
   var info = ""
-  when defined(useGcAssert):
-    when defined(boehm_enabled):
-      info = "Boehm gc debugging"
-    else:
-      info = "Gc debugging"
+  when defined(boehm_enabled):
+    info = "Boehm gc debugging"
   else:
-    info = "Release mode"
-  when defined(swap_sync_refs):
-    info &= ", swapped sync descriptors"
+    when defined(default_enabled):
+      info = "Gc debugging"
+    else:
+      info = "Release mode"
   echo "*** gabbleblotchit: ", info
 
   nimbus.start()
-  nimbus.process()
