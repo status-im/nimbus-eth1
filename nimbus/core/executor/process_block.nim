36b91fc8f3aca1012ad5bf4e51099a95eef7979c
Adam Spitz <adamspitz@status.im>
2023-03-16 14:28:35 -0400

    Gwei conversion should use u256 because u64 can overflow.

--- nimbus/core/executor/process_block.nim
+++ nimbus/core/executor/process_block.nim
@@ -32,7 +32,7 @@ import
 # ------------------------------------------------------------------------------
 
 func gwei(n: uint64): UInt256 =
-  (n * (10'u64 ^ 9'u64)).u256
+  n.u256 * (10 ^ 9).u256
 
 proc procBlkPreamble(vmState: BaseVMState;
                      header: BlockHeader; body: BlockBody): bool
