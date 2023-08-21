80b26fe1c88b2f767c29923109868a0e7167227b
Jordan Hrycaj <jordan@status.im>
2023-08-21 16:10:08 +0100

    Simplify RocksDB sub-tables iterator

--- nimbus/db/aristo/aristo_init/aristo_rocksdb/rdb_walk.nim
+++ nimbus/db/aristo/aristo_init/aristo_rocksdb/rdb_walk.nim
@@ -17,30 +17,34 @@ import
   std/sequtils,
   eth/common,
   rocksdb,
-  ../../aristo_desc,
   ../aristo_init_common,
   ./rdb_desc
 
 # ------------------------------------------------------------------------------
-# Private functions
+# Private helpers
 # ------------------------------------------------------------------------------
 
 func keyPfx(kData: cstring, kLen: csize_t): int =
-  if not kData.isNil and kLen == 1 + sizeof(VertexID):
+  if not kData.isNil and kLen == 1 + sizeof(uint64):
     kData.toOpenArrayByte(0,0)[0].int
   else:
     -1
 
 func keyXid(kData: cstring, kLen: csize_t): uint64 =
-  if not kData.isNil and kLen == 1 + sizeof(VertexID):
+  if not kData.isNil and kLen == 1 + sizeof(uint64):
     return uint64.fromBytesBE kData.toOpenArrayByte(1,int(kLen)-1).toSeq
-
-func to(xid: uint64; T: type Blob): T =
-  xid.toBytesBE.toSeq
 
 func valBlob(vData: cstring, vLen: csize_t): Blob =
   if not vData.isNil and 0 < vLen:
     return vData.toOpenArrayByte(0,int(vLen)-1).toSeq
+
+
+# ------------------------------------------------------------------------------
+# Private debugging helpers
+# ------------------------------------------------------------------------------
+
+proc pp(kd: cstring, kl: csize_t): string =
+  if kd.isNil: "n/a" else: $kd.keyXid(kl)
 
 # ------------------------------------------------------------------------------
 # Public iterators
@@ -82,80 +86,60 @@ iterator walk*(
     count.inc
     # End while
 
+
 iterator walk*(
     rdb: RdbInst;
     pfx: StorageType;
       ): tuple[n: int, xid: uint64, data: Blob] =
-  ## Walk over key-value pairs of the table referted to by the argument `pfx`.
+  ## Walk over key-value pairs of the table referted to by the argument `pfx`
+  ## whic must be different from `Oops` and `AdmPfx`.
   ##
   ## Non-decodable entries are stepped over while the counter `n` of the
   ## yield record is still incremented.
-  let rit = rdb.store.db.rocksdb_create_iterator(rdb.store.readOptions)
-  defer: rit.rocksdb_iter_destroy()
-
-  rit.rocksdb_iter_seek_to_first()
-  var
-    count = 0
-    kLen: csize_t
-    kData: cstring
-
+  ##
   block walkBody:
-    # Skip over admin records (if any) and advance to the key sub-table
+    if pfx in {Oops, AdmPfx}:
+      # Unsupported
+      break walkBody
+
+    let rit = rdb.store.db.rocksdb_create_iterator(rdb.store.readOptions)
+    defer: rit.rocksdb_iter_destroy()
+
+    var
+      count = 0
+      kLen: csize_t
+      kData: cstring
+
+    # Seek for `VertexID(1)` and subsequent entries if that fails. There should
+    # always be a `VertexID(1)` entry unless the sub-table is empty. There is
+    # no such control for the filter table in which case there is a blind guess
+    # (in case `rocksdb_iter_seek()` does not search `ge` for some reason.)
+    let keyOne = 1u64.toRdbKey pfx
+
+    # It is not clear what happens when the `key` does not exist. The guess
+    # is that the interation will proceed at the next key position.
+    #
+    # Comment from GO port at
+    #    //github.com/DanielMorsing/rocksdb/blob/master/iterator.go:
+    #
+    # Seek moves the iterator the position of the key given or, if the key
+    # doesn't exist, the next key that does exist in the database. If the key
+    # doesn't exist, and there is no next key, the Iterator becomes invalid.
+    #
+    kData = cast[cstring](unsafeAddr keyOne[0])
+    kLen = sizeof(keyOne).csize_t
+    rit.rocksdb_iter_seek(kData, kLen)
     if rit.rocksdb_iter_valid() == 0:
       break walkBody
-    kData = rit.rocksdb_iter_key(addr kLen)
-
-    case pfx:
-    of Oops, AdmPfx:
-      discard
-    of VtxPfx, KeyPfx:
-      # Skip over admin records until vertex sub-table reached
-      while kData.keyPfx(kLen) < VtxPfx.ord:
-
-        # Update Iterator and fetch next item
-        rit.rocksdb_iter_next()
-        if rit.rocksdb_iter_valid() == 0:
-          break walkBody
-        kData = rit.rocksdb_iter_key(addr kLen)
-        # End while
-
-    case pfx:
-    of Oops, AdmPfx, VtxPfx:
-      discard
-    of KeyPfx:
-      # Reposition search head to key sub-table
-      while kData.keyPfx(kLen) < KeyPfx.ord:
-
-        # Move search head to the first Merkle hash entry by seeking the same
-        # vertex ID on the key table. This might skip over stale keys smaller
-        # than the current one.
-        let key = @[KeyPfx.ord.byte] & kData.keyXid(kLen).to(Blob)
-        rit.rocksdb_iter_seek(cast[cstring](unsafeAddr key[0]), csize_t(kLen))
-
-        # It is not clear what happens when the `key` does not exist. The guess
-        # is that nothing would happen and the interation will proceed at the
-        # next vertex position.
-        kData = rit.rocksdb_iter_key(addr kLen)
-        if KeyPfx.ord <= kData.keyPfx(kLen):
-          # OK, reached Merkle hash table
-          break
-
-        # Update Iterator
-        rit.rocksdb_iter_next()
-        if rit.rocksdb_iter_valid() == 0:
-          break walkBody
-        kData = rit.rocksdb_iter_key(addr kLen)
-        # End while
 
     # Fetch sub-table data
     while true:
-      let kPfx = kData.keyPfx(kLen)
-      if pfx.ord < kPfx:
+      kData = rit.rocksdb_iter_key(addr kLen)
+      if pfx.ord != kData.keyPfx kLen:
         break walkBody # done
 
       let xid = kData.keyXid(kLen)
-      if 0 < xid or pfx == AdmPfx:
-
+      if 0 < xid:
         # Fetch value data
         var vLen: csize_t
         let vData = rit.rocksdb_iter_value(addr vLen)
@@ -168,7 +152,7 @@ iterator walk*(
       rit.rocksdb_iter_next()
       if rit.rocksdb_iter_valid() == 0:
         break walkBody
-      kData = rit.rocksdb_iter_key(addr kLen)
+
       count.inc
       # End while
 
