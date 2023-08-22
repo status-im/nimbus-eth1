8462ceb49a55cce7dde9a1b1993b020243898879
Jordan Hrycaj <jordan@status.im>
2023-08-22 16:35:35 +0100

    Split `aristo_filter` source into sub-files
    why:
      Make space for filter management API

---
+++ nimbus/db/aristo/aristo_filter/aristo_filter_desc.nim
@@ -0,0 +1,19 @@
+# nimbus-eth1
+# Copyright (c) 2021 Status Research & Development GmbH
+# Licensed under either of
+#  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
+#    http://www.apache.org/licenses/LICENSE-2.0)
+#  * MIT license ([LICENSE-MIT](LICENSE-MIT) or
+#    http://opensource.org/licenses/MIT)
+# at your option. This file may not be copied, modified, or distributed
+# except according to those terms.
+
+import
+  ../aristo_desc
+
+type
+  StateRootPair* = object
+    be*: HashKey
+    fg*: HashKey
+
+# End
