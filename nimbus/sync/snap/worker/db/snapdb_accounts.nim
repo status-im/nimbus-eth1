203e0011048673ad832444f44bf6e6363eedf59a
jangko <jangko128@gmail.com>
2024-01-30 13:46:16 +0700

    Fix copyright year

--- nimbus/sync/snap/worker/db/snapdb_accounts.nim
+++ nimbus/sync/snap/worker/db/snapdb_accounts.nim
@@ -1,5 +1,5 @@
 # nimbus-eth1
-# Copyright (c) 2021 Status Research & Development GmbH
+# Copyright (c) 2021-2024 Status Research & Development GmbH
 # Licensed under either of
 #  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
 #    http://www.apache.org/licenses/LICENSE-2.0)
