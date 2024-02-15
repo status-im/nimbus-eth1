a109e75b0c50e9a1b037e294dd946bda56ff13ca
kdeme <kim.demey@gmail.com>
2024-02-15 17:04:25 +0100

    Update copyright years for altered files

--- fluffy/rpc/rpc_calls/rpc_portal_debug_calls.nim
+++ fluffy/rpc/rpc_calls/rpc_portal_debug_calls.nim
@@ -1,3 +1,10 @@
+# fluffy
+# Copyright (c) 2021-2024 Status Research & Development GmbH
+# Licensed and distributed under either of
+#   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
+#   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
+# at your option. This file may not be copied, modified, or distributed except according to those terms.
+
 ## Portal History Network json-rpc debug & testing calls
 proc portal_history_storeContent(dataFile: string): bool
 proc portal_history_propagate(dataFile: string): bool
