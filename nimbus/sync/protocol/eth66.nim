5ae3802d6da7a6e88fd47a4485ce830008631d31
jangko <jangko128@gmail.com>
2024-02-19 17:11:08 +0700

    Fix copyright year

--- nimbus/sync/protocol/eth66.nim
+++ nimbus/sync/protocol/eth66.nim
@@ -1,6 +1,6 @@
 # Nimbus - Ethereum Wire Protocol
 #
-# Copyright (c) 2018-2021 Status Research & Development GmbH
+# Copyright (c) 2018-2024 Status Research & Development GmbH
 # Licensed under either of
 #  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
 #    http://www.apache.org/licenses/LICENSE-2.0)
