c99fdeb17ea130e9a45ef50bd39c734133a084b2
kdeme <kim.demey@gmail.com>
2024-03-21 15:09:29 +0100

    Replace status-im/portal-spec-tests with ethereum fork version
    - The fluffy test vector repo got forked (well, copied rather) to
    become the official one under ethereum github org, so we change
    to that repo now and archive ours.
    - Our repo also stored accumulator / historical_roots, replace
    that with a new repo which is only for network configs.
    - Several changes needed to be made due to test vectors that got
    updated + some of them got changed to / are yaml format instead of
    json.

--- fluffy/tests/portal_spec_tests/mainnet/test_history_content.nim
+++ fluffy/tests/portal_spec_tests/mainnet/test_history_content.nim
@@ -16,7 +16,8 @@ import
   ../../../network_metadata,
   ../../../eth_data/[history_data_json_store, history_data_ssz_e2s],
   ../../../network/history/[history_content, history_network, accumulator],
-  ../../test_history_util
+  ../../test_history_util,
+  ../../test_yaml_utils
 
 suite "History Content Encodings":
   test "HeaderWithProof Building and Encoding":
@@ -79,83 +80,73 @@ suite "History Content Encodings":
 
   test "HeaderWithProof Encoding/Decoding and Verification":
     const dataFile =
-      "./vendor/portal-spec-tests/tests/mainnet/history/headers_with_proof/14764013.json"
+      "./vendor/portal-spec-tests/tests/mainnet/history/headers_with_proof/14764013.yaml"
 
-    let accumulator = loadAccumulator()
+    let
+      content = YamlPortalContent.loadFromYaml(dataFile).valueOr:
+        raiseAssert "Invalid data file: " & error
+      accumulator = loadAccumulator()
+      contentKeyEncoded = content.content_key.hexToSeqByte()
+      contentValueEncoded = content.content_value.hexToSeqByte()
 
-    let res = readJsonType(dataFile, JsonPortalContentTable)
+    # Decode content
+    let
+      contentKey = decodeSsz(contentKeyEncoded, ContentKey)
+      contentValue = decodeSsz(contentValueEncoded, BlockHeaderWithProof)
+
+    check:
+      contentKey.isOk()
+      contentValue.isOk()
+
+    let blockHeaderWithProof = contentValue.get()
+
+    let res = decodeRlp(blockHeaderWithProof.header.asSeq(), BlockHeader)
     check res.isOk()
-    let content = res.get()
+    let header = res.get()
 
-    for k, v in content:
-      # TODO: strange assignment failure when using try/except ValueError
-      # for the hexToSeqByte() here.
-      let
-        contentKeyEncoded = v.content_key.hexToSeqByte()
-        contentValueEncoded = v.content_value.hexToSeqByte()
+    check accumulator.verifyHeader(header, blockHeaderWithProof.proof).isOk()
 
-      # Decode content
-      let
-        contentKey = decodeSsz(contentKeyEncoded, ContentKey)
-        contentValue = decodeSsz(contentValueEncoded, BlockHeaderWithProof)
-
-      check:
-        contentKey.isOk()
-        contentValue.isOk()
-
-      let blockHeaderWithProof = contentValue.get()
-
-      let res = decodeRlp(blockHeaderWithProof.header.asSeq(), BlockHeader)
-      check res.isOk()
-      let header = res.get()
-
-      check accumulator.verifyHeader(header, blockHeaderWithProof.proof).isOk()
-
-      # Encode content
-      check:
-        SSZ.encode(blockHeaderWithProof) == contentValueEncoded
-        encode(contentKey.get()).asSeq() == contentKeyEncoded
+    # Encode content
+    check:
+      SSZ.encode(blockHeaderWithProof) == contentValueEncoded
+      encode(contentKey.get()).asSeq() == contentKeyEncoded
 
   test "PortalBlockBody (Legacy) Encoding/Decoding and Verification":
     const
-      dataFile = "./vendor/portal-spec-tests/tests/mainnet/history/bodies/14764013.json"
-      headersWithProofFile =
-        "./vendor/portal-spec-tests/tests/mainnet/history/headers_with_proof/14764013.json"
+      dataFile = "./vendor/portal-spec-tests/tests/mainnet/history/bodies/14764013.yaml"
+      headersDataFile =
+        "./vendor/portal-spec-tests/tests/mainnet/history/headers_with_proof/14764013.yaml"
 
-    let res = readJsonType(dataFile, JsonPortalContentTable)
-    check res.isOk()
-    let content = res.get()
+    let
+      content = YamlPortalContent.loadFromYaml(dataFile).valueOr:
+        raiseAssert "Invalid data file: " & error
+      headerContent = YamlPortalContent.loadFromYaml(headersDataFile).valueOr:
+        raiseAssert "Invalid data file: " & error
 
-    let headersRes = readJsonType(headersWithProofFile, JsonPortalContentTable)
-    check headersRes.isOk()
-    let headers = headersRes.get()
+      contentKeyEncoded = content.content_key.hexToSeqByte()
+      contentValueEncoded = content.content_value.hexToSeqByte()
 
-    for k, v in content:
-      let
-        contentKeyEncoded = v.content_key.hexToSeqByte()
-        contentValueEncoded = v.content_value.hexToSeqByte()
+    # Get the header for validation of body
+    let
+      headerEncoded = headerContent.content_value.hexToSeqByte()
+      headerWithProofRes = decodeSsz(headerEncoded, BlockHeaderWithProof)
+    check headerWithProofRes.isOk()
+    let headerRes = decodeRlp(headerWithProofRes.get().header.asSeq(), BlockHeader)
+    check headerRes.isOk()
+    let header = headerRes.get()
 
-      # Get the header for validation of body
-      let
-        headerEncoded = headers[k].content_value.hexToSeqByte()
-        headerWithProofRes = decodeSsz(headerEncoded, BlockHeaderWithProof)
-      check headerWithProofRes.isOk()
-      let headerRes = decodeRlp(headerWithProofRes.get().header.asSeq(), BlockHeader)
-      check headerRes.isOk()
-      let header = headerRes.get()
+    # Decode content key
+    let contentKey = decodeSsz(contentKeyEncoded, ContentKey)
+    check contentKey.isOk()
 
-      # Decode content key
-      let contentKey = decodeSsz(contentKeyEncoded, ContentKey)
-      check contentKey.isOk()
+    # Decode (SSZ + RLP decode step) and validate block body
+    let contentValue = validateBlockBodyBytes(contentValueEncoded, header)
+    check contentValue.isOk()
 
-      # Decode (SSZ + RLP decode step) and validate block body
-      let contentValue = validateBlockBodyBytes(contentValueEncoded, header)
-      check contentValue.isOk()
-
-      # Encode content and content key
-      check:
-        encode(contentValue.get()) == contentValueEncoded
-        encode(contentKey.get()).asSeq() == contentKeyEncoded
+    # Encode content and content key
+    check:
+      encode(contentValue.get()) == contentValueEncoded
+      encode(contentKey.get()).asSeq() == contentKeyEncoded
 
   test "PortalBlockBody (Shanghai) Encoding/Decoding":
     # TODO: We don't have the header (without proof) ready here so cannot do
@@ -188,41 +179,37 @@ suite "History Content Encodings":
   test "Receipts Encoding/Decoding and Verification":
     const
       dataFile =
-        "./vendor/portal-spec-tests/tests/mainnet/history/receipts/14764013.json"
-      headersWithProofFile =
-        "./vendor/portal-spec-tests/tests/mainnet/history/headers_with_proof/14764013.json"
+        "./vendor/portal-spec-tests/tests/mainnet/history/receipts/14764013.yaml"
+      headersDataFile =
+        "./vendor/portal-spec-tests/tests/mainnet/history/headers_with_proof/14764013.yaml"
 
-    let res = readJsonType(dataFile, JsonPortalContentTable)
-    check res.isOk()
-    let content = res.get()
+    let
+      content = YamlPortalContent.loadFromYaml(dataFile).valueOr:
+        raiseAssert "Invalid data file: " & error
+      headerContent = YamlPortalContent.loadFromYaml(headersDataFile).valueOr:
+        raiseAssert "Invalid data file: " & error
 
-    let headersRes = readJsonType(headersWithProofFile, JsonPortalContentTable)
-    check headersRes.isOk()
-    let headers = headersRes.get()
+      contentKeyEncoded = content.content_key.hexToSeqByte()
+      contentValueEncoded = content.content_value.hexToSeqByte()
 
-    for k, v in content:
-      let
-        contentKeyEncoded = v.content_key.hexToSeqByte()
-        contentValueEncoded = v.content_value.hexToSeqByte()
+    # Get the header for validation of receipts
+    let
+      headerEncoded = headerContent.content_value.hexToSeqByte()
+      headerWithProofRes = decodeSsz(headerEncoded, BlockHeaderWithProof)
+    check headerWithProofRes.isOk()
+    let headerRes = decodeRlp(headerWithProofRes.get().header.asSeq(), BlockHeader)
+    check headerRes.isOk()
+    let header = headerRes.get()
 
-      # Get the header for validation of receipts
-      let
-        headerEncoded = headers[k].content_value.hexToSeqByte()
-        headerWithProofRes = decodeSsz(headerEncoded, BlockHeaderWithProof)
-      check headerWithProofRes.isOk()
-      let headerRes = decodeRlp(headerWithProofRes.get().header.asSeq(), BlockHeader)
-      check headerRes.isOk()
-      let header = headerRes.get()
+    # Decode content key
+    let contentKey = decodeSsz(contentKeyEncoded, ContentKey)
+    check contentKey.isOk()
 
-      # Decode content key
-      let contentKey = decodeSsz(contentKeyEncoded, ContentKey)
-      check contentKey.isOk()
+    # Decode (SSZ + RLP decode step) and validate receipts
+    let contentValue = validateReceiptsBytes(contentValueEncoded, header.receiptRoot)
+    check contentValue.isOk()
 
-      # Decode (SSZ + RLP decode step) and validate receipts
-      let contentValue = validateReceiptsBytes(contentValueEncoded, header.receiptRoot)
-      check contentValue.isOk()
-
-      # Encode content
-      check:
-        encode(contentValue.get()) == contentValueEncoded
-        encode(contentKey.get()).asSeq() == contentKeyEncoded
+    # Encode content
+    check:
+      encode(contentValue.get()) == contentValueEncoded
+      encode(contentKey.get()).asSeq() == contentKeyEncoded
