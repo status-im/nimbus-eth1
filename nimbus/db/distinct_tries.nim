9ac8061bc93b1eba34ce5df8fa1ee371309d32f5
advaita-saha <adv11sphs@gmail.com>
2024-04-11 13:30:43 +0530

    comment out delete functions

--- nimbus/db/distinct_tries.nim
+++ nimbus/db/distinct_tries.nim
@@ -106,8 +106,9 @@ proc maybeGetAccountBytes*(trie: AccountsTrie, address: EthAddress): Option[Blob
 proc putAccountBytes*(trie: var AccountsTrie, address: EthAddress, value: openArray[byte]) =
   CoreDbPhkRef(trie).put(address, value)
 
-proc delAccountBytes*(trie: var AccountsTrie, address: EthAddress) =
-  CoreDbPhkRef(trie).del(address)
+# INVALID FOR VERKLE ( as of April 2024 )
+# proc delAccountBytes*(trie: var AccountsTrie, address: EthAddress) =
+#   CoreDbPhkRef(trie).del(address)
 
 # ------------------------------------------------------------------------------
 # Public functions: storage trie
@@ -141,8 +142,9 @@ proc maybeGetSlotBytes*(trie: StorageTrie, slotAsKey: openArray[byte]): Option[B
 proc putSlotBytes*(trie: var StorageTrie, slotAsKey: openArray[byte], value: openArray[byte]) =
   CoreDbPhkRef(trie).put(slotAsKey, value)
 
-proc delSlotBytes*(trie: var StorageTrie, slotAsKey: openArray[byte]) =
-  CoreDbPhkRef(trie).del(slotAsKey)
+# INVALID FOR VERKLE ( as of April 2024 )
+# proc delSlotBytes*(trie: var StorageTrie, slotAsKey: openArray[byte]) =
+#   CoreDbPhkRef(trie).del(slotAsKey)
 
 proc storageTrieForAccount*(trie: AccountsTrie, account: Account, isPruning = true): StorageTrie =
   # TODO: implement `prefix-db` to solve issue #228 permanently.
