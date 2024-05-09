c2f34a77aab6ffc3c48e22df8bd6fd4c39ecec74
web3-developer <51288821+web3-developer@users.noreply.github.com>
2024-05-09 15:38:50 +0800

    Implement high level offer validation steps.

--- fluffy/network/state/state_validation.nim
+++ fluffy/network/state/state_validation.nim
@@ -7,34 +7,54 @@
 
 import eth/[common, trie], eth/trie/[hexary, db, trie_defs], ./state_content
 
+proc isValidTrieNode(expectedHash: KeccakHash, node: TrieNode): bool {.inline.} =
+  expectedHash == keccakHash(node.asSeq())
+
+proc isValidBytecode(expectedHash: KeccakHash, code: Bytecode): bool {.inline.} =
+  expectedHash == keccakHash(code.asSeq())
+
 proc validateFetchedAccountTrieNode*(
     trustedAccountTrieNodeKey: AccountTrieNodeKey,
     accountTrieNode: AccountTrieNodeRetrieval,
 ): bool =
   let expectedHash = trustedAccountTrieNodeKey.nodeHash
-  let actualHash = keccakHash(accountTrieNode.node.asSeq())
-
-  expectedHash == actualHash
+  isValidTrieNode(expectedHash, accountTrieNode.node)
 
 proc validateFetchedContractTrieNode*(
     trustedContractTrieNodeKey: ContractTrieNodeKey,
     contractTrieNode: ContractTrieNodeRetrieval,
 ): bool =
   let expectedHash = trustedContractTrieNodeKey.nodeHash
-  let actualHash = keccakHash(contractTrieNode.node.asSeq())
-
-  expectedHash == actualHash
+  isValidTrieNode(expectedHash, contractTrieNode.node)
 
 proc validateFetchedContractCode*(
     trustedContractCodeKey: ContractCodeKey, contractCode: ContractCodeRetrieval
 ): bool =
   let expectedHash = trustedContractCodeKey.codeHash
-  let actualHash = keccakHash(contractCode.code.asSeq())
+  isValidBytecode(expectedHash, contractCode.code)
 
-  expectedHash == actualHash
+# Needs to handle partial path or full address path
+proc isValidTrieProof(
+    expectedRootHash: KeccakHash, path: Nibbles, proof: TrieProof
+): bool =
+  if proof.len() == 0:
+    return false
 
-# TODO: implement generic validation path walking proof verification
+  if not isValidTrieNode(expectedRootHash, proof[0]):
+    return false
 
+  # TODO: walk down the rest of the trie and check that each node is in the path of the proof
+
+  # NOTE: this does not check the last node
+
+  false
+
+proc decodeAccount(leafNode: TrieNode): Account =
+  # TODO: implement this
+  #rlpFromBytes
+  Account()
+
+# Precondition: AccountTrieNodeOffer.blockHash is already checked to be part of the canonical chain
 proc validateOfferedAccountTrieNode*(
     trustedStateRoot: KeccakHash,
     accountTrieNodeKey: AccountTrieNodeKey,
@@ -47,10 +67,10 @@ proc validateOfferedAccountTrieNode*(
   #   proof*: TrieProof
   #   blockHash*: BlockHash
 
-  # verify account proof
+  isValidTrieProof(trustedStateRoot, accountTrieNodeKey.path, accountTrieNode.proof) and
+    isValidTrieNode(accountTrieNodeKey.nodeHash, accountTrieNode.proof[^1])
 
-  false
-
+# Precondition: ContractTrieNodeOffer.blockHash is already checked to be part of the canonical chain
 proc validateOfferedContractTrieNode*(
     trustedStateRoot: KeccakHash,
     contractTrieNodeKey: ContractTrieNodeKey,
@@ -65,12 +85,19 @@ proc validateOfferedContractTrieNode*(
   #   accountProof*: TrieProof
   #   blockHash*: BlockHash
 
-  # verify account proof
-  # get storage root from account leaf
-  # verify storage proof
+  let
+    addressHash = keccakHash(contractTrieNodeKey.address).data
+    accountPath = Nibbles(@addressHash)
+  if not isValidTrieProof(trustedStateRoot, accountPath, contractTrieNode.accountProof):
+    return false
 
-  false
+  let account = decodeAccount(contractTrieNode.accountProof[^1])
 
+  isValidTrieProof(
+    account.storageRoot, contractTrieNodeKey.path, contractTrieNode.storageProof
+  ) and isValidTrieNode(contractTrieNodeKey.nodeHash, contractTrieNode.storageProof[^1])
+
+# Precondition: ContractCodeOffer.blockHash is already checked to be part of the canonical chain
 proc validateOfferedContractCode*(
     trustedStateRoot: KeccakHash,
     contractCodeKey: ContractCodeKey,
@@ -84,8 +111,12 @@ proc validateOfferedContractCode*(
   #   accountProof*: TrieProof
   #   blockHash*: BlockHash
 
-  # verify account proof
-  # get codehash from account leaf
-  # verify codehash
+  let
+    addressHash = keccakHash(contractCodeKey.address).data
+    accountPath = Nibbles(@addressHash)
+  if not isValidTrieProof(trustedStateRoot, accountPath, contractCode.accountProof):
+    return false
 
-  false
+  let account: Account = decodeAccount(contractCode.accountProof[^1])
+
+  isValidBytecode(account.codeHash, contractCode.code)
