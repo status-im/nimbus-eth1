fe6fc4ca66eaabca7754a70269c4018f70c0eefc
web3-developer <51288821+web3-developer@users.noreply.github.com>
2024-05-28 16:36:55 +0800

    Completed state network get content tests.

--- fluffy/tests/state_network_tests/test_state_network_getcontent.nim
+++ fluffy/tests/state_network_tests/test_state_network_getcontent.nim
@@ -18,7 +18,12 @@ import
   ./state_test_helpers
 
 procSuite "State Network - Get Content":
+  const STATE_NODE1_PORT = 20402
+  const STATE_NODE2_PORT = 20403
+
   let rng = newRng()
+
+  # Single state instance tests
 
   asyncTest "Single state instance - Get existing account trie node":
     const file = testVectorDir / "account_trie_node.yaml"
@@ -26,7 +31,7 @@ procSuite "State Network - Get Content":
     let
       testCase = YamlTrieNodeKVs.loadFromYaml(file).valueOr:
         raiseAssert "Cannot read test vector: " & error
-      stateNode1 = newStateNode(rng, 20302)
+      stateNode1 = newStateNode(rng, STATE_NODE1_PORT)
 
     stateNode1.start()
 
@@ -57,7 +62,7 @@ procSuite "State Network - Get Content":
     let
       testCase = YamlTrieNodeKVs.loadFromYaml(file).valueOr:
         raiseAssert "Cannot read test vector: " & error
-      stateNode1 = newStateNode(rng, 20302)
+      stateNode1 = newStateNode(rng, STATE_NODE1_PORT)
 
     stateNode1.start()
 
@@ -79,7 +84,7 @@ procSuite "State Network - Get Content":
     let
       testCase = YamlTrieNodeKVs.loadFromYaml(file).valueOr:
         raiseAssert "Cannot read test vector: " & error
-      stateNode1 = newStateNode(rng, 20302)
+      stateNode1 = newStateNode(rng, STATE_NODE1_PORT)
 
     stateNode1.start()
 
@@ -111,7 +116,7 @@ procSuite "State Network - Get Content":
     let
       testCase = YamlTrieNodeKVs.loadFromYaml(file).valueOr:
         raiseAssert "Cannot read test vector: " & error
-      stateNode1 = newStateNode(rng, 20302)
+      stateNode1 = newStateNode(rng, STATE_NODE1_PORT)
 
     stateNode1.start()
 
@@ -134,7 +139,7 @@ procSuite "State Network - Get Content":
     let
       testCase = YamlContractBytecodeKVs.loadFromYaml(file).valueOr:
         raiseAssert "Cannot read test vector: " & error
-      stateNode1 = newStateNode(rng, 20302)
+      stateNode1 = newStateNode(rng, STATE_NODE1_PORT)
 
     stateNode1.start()
 
@@ -165,7 +170,7 @@ procSuite "State Network - Get Content":
     let
       testCase = YamlContractBytecodeKVs.loadFromYaml(file).valueOr:
         raiseAssert "Cannot read test vector: " & error
-      stateNode1 = newStateNode(rng, 20302)
+      stateNode1 = newStateNode(rng, STATE_NODE1_PORT)
 
     stateNode1.start()
 
@@ -180,3 +185,166 @@ procSuite "State Network - Get Content":
         res.isNone()
 
     await stateNode1.stop()
+
+  # Two state instances tests
+
+  asyncTest "Two state instances - Get existing account trie node":
+    const file = testVectorDir / "account_trie_node.yaml"
+
+    let
+      testCase = YamlTrieNodeKVs.loadFromYaml(file).valueOr:
+        raiseAssert "Cannot read test vector: " & error
+      stateNode1 = newStateNode(rng, STATE_NODE1_PORT)
+      stateNode2 = newStateNode(rng, STATE_NODE2_PORT)
+
+    stateNode1.start()
+    stateNode2.start()
+
+    check:
+      stateNode1.portalProtocol().addNode(stateNode2.localNode()) == Added
+      stateNode2.portalProtocol().addNode(stateNode1.localNode()) == Added
+
+      (await stateNode1.portalProtocol().ping(stateNode2.localNode())).isOk()
+      (await stateNode2.portalProtocol().ping(stateNode1.localNode())).isOk()
+
+    for testData in testCase:
+      let
+        contentKeyBytes = testData.content_key.hexToSeqByte().ByteList
+        contentKey = ContentKey.decode(contentKeyBytes).get()
+        contentId = toContentId(contentKeyBytes)
+        contentValueBytes = testData.content_value_retrieval.hexToSeqByte()
+        expectedContentValue = AccountTrieNodeRetrieval.decode(contentValueBytes).get()
+
+      # only store the content in the first state instance
+      stateNode1.portalProtocol().storeContent(
+        contentKeyBytes, contentId, contentValueBytes
+      )
+      check:
+        stateNode1.containsId(contentId)
+        not stateNode2.containsId(contentId)
+
+      let
+        res1 = await stateNode1.stateNetwork.getAccountTrieNode(
+          contentKey.accountTrieNodeKey
+        )
+        res2 = await stateNode2.stateNetwork.getAccountTrieNode(
+          contentKey.accountTrieNodeKey
+        )
+      check:
+        stateNode1.containsId(contentId)
+        stateNode2.containsId(contentId)
+        res1.isOk()
+        res1.get() == expectedContentValue
+        res1.get().node == expectedContentValue.node
+        res2.isOk()
+        res2.get() == expectedContentValue
+        res2.get().node == expectedContentValue.node
+
+    await stateNode1.stop()
+    await stateNode2.stop()
+
+  asyncTest "Two state instances - Get existing contract trie node":
+    const file = testVectorDir / "contract_storage_trie_node.yaml"
+
+    let
+      testCase = YamlTrieNodeKVs.loadFromYaml(file).valueOr:
+        raiseAssert "Cannot read test vector: " & error
+      stateNode1 = newStateNode(rng, STATE_NODE1_PORT)
+      stateNode2 = newStateNode(rng, STATE_NODE2_PORT)
+
+    stateNode1.start()
+    stateNode2.start()
+
+    check:
+      stateNode1.portalProtocol().addNode(stateNode2.localNode()) == Added
+      stateNode2.portalProtocol().addNode(stateNode1.localNode()) == Added
+
+      (await stateNode1.portalProtocol().ping(stateNode2.localNode())).isOk()
+      (await stateNode2.portalProtocol().ping(stateNode1.localNode())).isOk()
+
+    for testData in testCase:
+      let
+        contentKeyBytes = testData.content_key.hexToSeqByte().ByteList
+        contentKey = ContentKey.decode(contentKeyBytes).get()
+        contentId = toContentId(contentKeyBytes)
+        contentValueBytes = testData.content_value_retrieval.hexToSeqByte()
+        expectedContentValue = ContractTrieNodeRetrieval.decode(contentValueBytes).get()
+
+      # only store the content in the first state instance
+      stateNode1.portalProtocol().storeContent(
+        contentKeyBytes, contentId, contentValueBytes
+      )
+      check:
+        stateNode1.containsId(contentId)
+        not stateNode2.containsId(contentId)
+
+      let
+        res1 = await stateNode1.stateNetwork.getContractTrieNode(
+          contentKey.contractTrieNodeKey
+        )
+        res2 = await stateNode2.stateNetwork.getContractTrieNode(
+          contentKey.contractTrieNodeKey
+        )
+      check:
+        stateNode1.containsId(contentId)
+        stateNode2.containsId(contentId)
+        res1.isOk()
+        res1.get() == expectedContentValue
+        res1.get().node == expectedContentValue.node
+        res2.isOk()
+        res2.get() == expectedContentValue
+        res2.get().node == expectedContentValue.node
+
+    await stateNode1.stop()
+    await stateNode2.stop()
+
+  asyncTest "Two state instances - Get existing contract bytecode":
+    const file = testVectorDir / "contract_bytecode.yaml"
+
+    let
+      testCase = YamlContractBytecodeKVs.loadFromYaml(file).valueOr:
+        raiseAssert "Cannot read test vector: " & error
+      stateNode1 = newStateNode(rng, STATE_NODE1_PORT)
+      stateNode2 = newStateNode(rng, STATE_NODE2_PORT)
+
+    stateNode1.start()
+    stateNode2.start()
+
+    check:
+      stateNode1.portalProtocol().addNode(stateNode2.localNode()) == Added
+      stateNode2.portalProtocol().addNode(stateNode1.localNode()) == Added
+
+      (await stateNode1.portalProtocol().ping(stateNode2.localNode())).isOk()
+      (await stateNode2.portalProtocol().ping(stateNode1.localNode())).isOk()
+
+    for testData in testCase:
+      let
+        contentKeyBytes = testData.content_key.hexToSeqByte().ByteList
+        contentKey = ContentKey.decode(contentKeyBytes).get()
+        contentId = toContentId(contentKeyBytes)
+        contentValueBytes = testData.content_value_retrieval.hexToSeqByte()
+        expectedContentValue = ContractCodeRetrieval.decode(contentValueBytes).get()
+
+      # only store the content in the first state instance
+      stateNode1.portalProtocol().storeContent(
+        contentKeyBytes, contentId, contentValueBytes
+      )
+      check:
+        stateNode1.containsId(contentId)
+        not stateNode2.containsId(contentId)
+
+      let
+        res1 = await stateNode1.stateNetwork.getContractCode(contentKey.contractCodeKey)
+        res2 = await stateNode2.stateNetwork.getContractCode(contentKey.contractCodeKey)
+      check:
+        stateNode1.containsId(contentId)
+        stateNode2.containsId(contentId)
+        res1.isOk()
+        res1.get() == expectedContentValue
+        res1.get().code == expectedContentValue.code
+        res2.isOk()
+        res2.get() == expectedContentValue
+        res2.get().code == expectedContentValue.code
+
+    await stateNode1.stop()
+    await stateNode2.stop()
