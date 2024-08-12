8723a792251f51ccf8106684225073479a8f6019
Jacek Sieka <jacek@status.im>
2024-08-12 14:49:32 +0200

    add era dir to make_states

--- scripts/make_states.sh
+++ scripts/make_states.sh
@@ -6,9 +6,9 @@ set -e
 
 trap "exit" INT
 
-if [ -z "$3" ]
+if [ -z "$4" ]
   then
-    echo "Syntax: make_states.sh datadir era1dir statsdir [startdir]"
+    echo "Syntax: make_states.sh datadir era1dir eradir statsdir [startdir]"
     exit 1;
 fi
 
@@ -20,15 +20,16 @@ REV=$(git rev-parse --short=8 HEAD)
 DATA_DIR="$1/${DATE}-${REV}"
 
 mkdir -p "$DATA_DIR"
-[ "$4" ] && cp -ar "$4"/* "$DATA_DIR"
+[ "$5" ] && cp -ar "$5"/* "$DATA_DIR"
 
 while true;
 do
   "$SCRIPT_DIR/../build/nimbus" import \
     --data-dir:"${DATA_DIR}" \
     --era1-dir:"$2" \
+    --era-dir:"$3" \
     --debug-csv-stats:"$3/stats-${DATE}-${REV}.csv" \
-    --max-blocks:100000
+    --max-blocks:1000000
   cp -ar "$1/${DATE}-${REV}" "$1/${DATE}-${REV}"-$(printf "%04d" $counter)
   counter=$((counter+1))
 done
