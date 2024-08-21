6aaa11dc5f9cdcf0598f3e062b07fbfb7482fcce
advaita-saha <adv11sphs@gmail.com>
2024-08-21 17:06:25 +0530

    forward sync

--- nimbus/external_sync.nim
+++ nimbus/external_sync.nim
@@ -22,29 +22,10 @@ import
 
 var running {.volatile.} = true
 
-# Load the parent root of CL block from the CL header
-template getRootFromHeader(client: RestClientRef, blockIdent: BlockIdent): Eth2Digest =
-  let clHeader =
-    try:
-      awaitWithTimeout(client.getBlockHeader(blockIdent), 30.seconds):
-        error "Failed to get CL head"
-        quit(QuitFailure)
-    except CatchableError as exc:
-      error "Error getting CL head", error = exc.msg
-      quit(QuitFailure)
-
-  # Fetching the parent root from the CL header
-  if clHeader.isSome():
-    let beaconHeader = clHeader.get()
-    beaconHeader.data.header.message.parent_root
-  else:
-    error "CL header is not available"
-    quit(QuitFailure)
-
 # Load the EL block, from CL ( either head or CL root )
 template getBlockFromBeaconChain(
     client: RestClientRef, blockIdent: BlockIdent, clConfig: RuntimeConfig
-): (EthBlock, Eth2Digest) =
+): (EthBlock, bool) =
   let clBlock =
     try:
       awaitWithTimeout(client.getBlockV2(blockIdent, clConfig), 30.seconds):
@@ -55,21 +36,16 @@ template getBlockFromBeaconChain(
       quit(QuitFailure)
 
   # Constructing the EL block from the CL block
-  var prev_hash: Eth2Digest
   var eth1block: EthBlock
   if clBlock.isSome():
     let data = clBlock.get()[]
     eth1Block = data.asTrusted().getEthBlock().valueOr:
         error "Failed to get EL block from CL head"
         quit(QuitFailure)
-
-    withBlck(data):
-      prev_hash = forkyBlck.toBeaconBlockHeader().parent_root
-
-    (eth1Block, prev_hash)
+        
+    (eth1Block, true)
   else:
-    error "CL head is not available"
-    quit(QuitFailure)
+    (eth1Block, false)
 
 proc loadBlocksFromBeaconChain(conf: NimbusConf) {.async.} =
   let coreDB = AristoDbRocks.newCoreDbRef(string conf.dataDir, conf.dbOptions())
@@ -92,109 +68,93 @@ proc loadBlocksFromBeaconChain(conf: NimbusConf) {.async.} =
 
   let
     chain = com.newChain()
-    networkName =
-      if conf.networkId == MainNet:
-        "mainnet"
-      elif conf.networkId == SepoliaNet:
-        "sepolia"
-      elif conf.networkId == HoleskyNet:
-        "holesky"
-      else:
-        error "Unsupported network", network = conf.networkId
-        quit(QuitFailure)
-    clConfig = getMetadataForNetwork(networkName).cfg
 
   var
-    currentBlockNumber = com.db.getSavedStateBlockNumber()
+    currentBlockNumber = com.db.getSavedStateBlockNumber() + 1
     flags =
       boolFlag({PersistBlockFlag.NoValidation}, true) +
       boolFlag({PersistBlockFlag.NoFullValidation}, true) +
       boolFlag(NoPersistBodies, true) +
       boolFlag({PersistBlockFlag.NoPersistReceipts}, true)
     blocks: seq[EthBlock]
-    hashChain: seq[Eth2Digest]
     curBlck: EthBlock
-    prevHash: Eth2Digest
+    lastEra1Block: uint64
+    firstSlotAfterMerge: uint64
+    clConfig: RuntimeConfig
     client = RestClientRef.new(conf.beaconApi).valueOr:
       error "Cannot connect to server"
       quit(QuitFailure)
 
-  # Constructs a sequence of hashes from the head of chain to the current block
-  template backTraceChainFromHead() =
-    # Difference Log
-    notice "Current block number", currentBlockNumber
-    let head_number = curBlck.header.number
-    notice "Block number from CL head", head_number = head_number
-    var diff = head_number - currentBlockNumber
-    notice "Number of blocks to be covered", diff = diff
+  if conf.networkId == MainNet:
+    clConfig = getMetadataForNetwork("mainnet").cfg
+    lastEra1Block = 15537393'u64 # Mainnet
+    firstSlotAfterMerge = 4700013'u64
+  elif conf.networkId == SepoliaNet:
+    clConfig = getMetadataForNetwork("sepolia").cfg
+    lastEra1Block = 1450408'u64 # Sepolia
+    firstSlotAfterMerge = 115193'u64
+  elif conf.networkId == HoleskyNet:
+    clConfig = getMetadataForNetwork("holesky").cfg
+    lastEra1Block = 0'u64
+    firstSlotAfterMerge = 0'u64
+  else:
+    error "Unsupported network", network = conf.networkId
+    quit(QuitFailure)
 
-    # Check if CL head is behind the EL head
-    if currentBlockNumber > head_number:
-      error "CL head is behind the EL head"
+  template process() =
+    let statusRes = chain.persistBlocks(blocks, flags)
+    if statusRes.isErr():
+      error "Failed to persist blocks", error = statusRes.error
       quit(QuitFailure)
+    info "Persisted blocks", blocks = blocks.len
+    blocks.setLen(0)
 
-    if diff != 1:
-      let time1 = Moment.now()
-      while running and diff > 2:
-        prevHash = client.getRootFromHeader(BlockIdent.init(prevHash))
-        hashChain.add(prevHash)
-        diff = diff - 1
+  var (headBlck, _) = client.getBlockFromBeaconChain(BlockIdent.init(BlockIdentType.Head), clConfig)
 
-        # Very crude approcimations, TODO: Improve
-        let
-          time2 = Moment.now()
-          diff1 = (time2 - time1).nanoseconds().float / 1000000000
-          speed = hashChain.len.float / diff1
-          remaining = diff.float / speed
-        notice "Remaining blocks to be covered", diff = diff
-        notice "Speed of fetching blocks", speed = speed
-        notice "Time remaining", remaining = remaining
+  if headBlck.header.number <= currentBlockNumber:
+    notice "CL head is behind of EL head, or in sync", head = headBlck.header.number
+    quit(QuitSuccess)
 
-      let (clBlck, _) = client.getBlockFromBeaconChain(
-        BlockIdent.init(hashChain[hashChain.len - 1]), clConfig
-      )
-      notice "Blocks loaded from CL", chainLen = hashChain.len
-      notice "Last block loaded from CL", blockNumber = clBlck.header.number
-      notice "Current block number", currentBlockNumber
+  var importedSlot = (currentBlockNumber - lastEra1Block) + firstSlotAfterMerge
+  notice "Finding slot number corresponding to block", importedSlot
 
-  # Download the blocks from CL and load into the EL
-  template sync() =
-    while running and hashChain.len > 0:
-      if hashChain.len <= 1:
-        blocks.add(curBlck)
-        hashChain.setLen(0)
-      else:
-        const maxBlocks = 2000 # Load 2000 block at a time
-        for i in 0 ..< min(hashChain.len, maxBlocks):
-          let (eth1blck, _) =
-            client.getBlockFromBeaconChain(BlockIdent.init(hashChain.pop()), clConfig)
-          blocks.add(eth1blck)
-        
-        if hashChain.len == 0:
-          blocks.add(curBlck)
+  var clNum = 0'u64
+  while running and clNum < currentBlockNumber:
+    let (blk, stat) = client.getBlockFromBeaconChain(
+      BlockIdent.init(Slot(importedSlot)), clConfig
+    )
+    if not stat:
+      importedSlot += 1
+      continue
 
+    clNum = blk.header.number
+    # decreasing the lower bound with each iteration
+    importedSlot += currentBlockNumber - clNum
+
+  notice "Found the slot to start with", slot = importedSlot
+
+  while running and currentBlockNumber < headBlck.header.number:
+    var isAvailable = false
+    (curBlck, isAvailable) = client.getBlockFromBeaconChain(
+      BlockIdent.init(Slot(importedSlot)), clConfig
+    )
+    if not isAvailable:
+      importedSlot += 1
+      continue
+
+    blocks.add(curBlck)
+    importedSlot += 1
+    currentBlockNumber = curBlck.header.number
+
+    if blocks.lenu64 mod 1000 == 0 or currentBlockNumber == headBlck.header.number:
       notice "Blocks Downloaded from CL", numberOfBlocks = blocks.len
+      process()
+      currentBlockNumber = com.db.getSavedStateBlockNumber()
+      (headBlck, _) = client.getBlockFromBeaconChain(BlockIdent.init(BlockIdentType.Head), clConfig)
 
-      let statusRes = chain.persistBlocks(blocks, flags)
-      if statusRes.isErr():
-        error "Failed to persist blocks", error = statusRes.error
-        quit(QuitFailure)
-      info "Persisted blocks"
-      blocks.setLen(0)
-
-  # First time loading of the head state
-  (curBlck, prevHash) =
-    client.getBlockFromBeaconChain(BlockIdent.init(BlockIdentType.Head), clConfig)
-  notice "Got block from CL head", prevhash
-
-  # Keep construsting chain until the current block number is reached
-  while running and currentBlockNumber < curBlck.header.number:
-    hashChain.add(prevHash) # adding the head
-    backTraceChainFromHead()
-    sync()
-    (curBlck, prevHash) =
-      client.getBlockFromBeaconChain(BlockIdent.init(BlockIdentType.Head), clConfig)
-    currentBlockNumber = com.db.getSavedStateBlockNumber()
+  if blocks.len > 0:
+    process()
+  
 
 when isMainModule:
   ## Ctrl+C handling
