ca7aa6615b0c17f3d3b184f325403a4efd421865
web3-developer <51288821+web3-developer@users.noreply.github.com>
2024-09-25 10:44:24 +0800

    Move web3 rpc handler into eth handler. Create debug rpc handler and move existing debug endpoints.

--- fluffy/rpc/rpc_eth_api.nim
+++ fluffy/rpc/rpc_eth_api.nim
@@ -16,25 +16,20 @@ import
   web3/primitives as web3types,
   eth/common/eth_types,
   beacon_chain/spec/forks,
-  ../common/common_utils,
   ../network/history/[history_network, history_content],
   ../network/state/[state_network, state_content, state_endpoints],
-  ../network/beacon/beacon_light_client
+  ../network/beacon/beacon_light_client,
+  ../version
 
 from ../../nimbus/transaction import getSender, ValidationError
 from ../../nimbus/rpc/filters import headerBloomFilter, deriveLogs, filterLogs
 from ../../nimbus/beacon/web3_eth_conv import w3Addr, w3Hash, ethHash
 
-# Subset of Ethereum execution JSON-RPC API:
-# https://ethereum.github.io/execution-apis/api-documentation/
-#
-# Currently supported subset:
-# - eth_chainId
-# - eth_getBlockByHash
-# - eth_getBlockByNumber
-# - eth_getBlockTransactionCountByHash
-# - eth_getLogs - Partially: only requests by block hash
-#
+export rpcserver
+
+# See the list of Ethereum execution JSON-RPC APIs which will be supported by
+# Portal Network clients such as Fluffy:
+# https://github.com/ethereum/portal-network-specs?tab=readme-ov-file#the-json-rpc-api
 
 # Some similar code as from nimbus `rpc_utils`, but avoiding that import as it
 # brings in  a lot more. Should restructure `rpc_utils` a bit before using that.
@@ -127,7 +122,8 @@ proc installEthApiHandlers*(
     beaconLightClient: Opt[LightClient],
     stateNetwork: Opt[StateNetwork],
 ) =
-  # Supported API through the Portal Network
+  rpcServer.rpc("web3_clientVersion") do() -> string:
+    return clientVersion
 
   rpcServer.rpc("eth_chainId") do() -> Quantity:
     # The Portal Network can only support MainNet at the moment, so always return
@@ -376,133 +372,6 @@ proc installEthApiHandlers*(
       blockNumber = quantityTag.number.uint64
       proofs = (await sn.getProofs(blockNumber, data.EthAddress, slots)).valueOr:
         raise newException(ValueError, "Unable to get proofs")
-
-    var storageProof = newSeqOfCap[StorageProof](slots.len)
-    for i, slot in slots:
-      let (slotKey, slotValue) = proofs.slots[i]
-      storageProof.add(
-        StorageProof(
-          key: slotKey,
-          value: slotValue,
-          proof: seq[RlpEncodedBytes](proofs.slotProofs[i]),
-        )
-      )
-
-    return ProofResponse(
-      address: data,
-      accountProof: seq[RlpEncodedBytes](proofs.accountProof),
-      balance: proofs.account.balance,
-      nonce: web3types.Quantity(proofs.account.nonce),
-      codeHash: web3types.Hash256(proofs.account.codeHash.data),
-      storageHash: web3types.Hash256(proofs.account.storageRoot.data),
-      storageProof: storageProof,
-    )
-
-  # TODO: Should we move these debug methods into a separate debug rpcServer?
-
-  rpcServer.rpc("debug_getBalanceByStateRoot") do(
-    data: web3Types.Address, stateRoot: web3types.Hash256
-  ) -> UInt256:
-    ## Returns the balance of the account of given address.
-    ##
-    ## data: address to check for balance.
-    ## stateRoot: the state root used to search the state trie.
-    ## Returns integer of the current balance in wei.
-
-    let sn = stateNetwork.valueOr:
-      raise newException(ValueError, "State sub-network not enabled")
-
-    let balance = (
-      await sn.getBalanceByStateRoot(
-        KeccakHash.fromBytes(stateRoot.bytes()), data.EthAddress
-      )
-    ).valueOr:
-      raise newException(ValueError, "Unable to get balance")
-
-    return balance
-
-  rpcServer.rpc("debug_getTransactionCountByStateRoot") do(
-    data: web3Types.Address, stateRoot: web3types.Hash256
-  ) -> Quantity:
-    ## Returns the number of transactions sent from an address.
-    ##
-    ## data: address.
-    ## stateRoot: the state root used to search the state trie.
-    ## Returns integer of the number of transactions send from this address.
-
-    let sn = stateNetwork.valueOr:
-      raise newException(ValueError, "State sub-network not enabled")
-
-    let nonce = (
-      await sn.getTransactionCountByStateRoot(
-        KeccakHash.fromBytes(stateRoot.bytes()), data.EthAddress
-      )
-    ).valueOr:
-      raise newException(ValueError, "Unable to get transaction count")
-    return nonce.Quantity
-
-  rpcServer.rpc("debug_getStorageAtByStateRoot") do(
-    data: web3Types.Address, slot: UInt256, stateRoot: web3types.Hash256
-  ) -> FixedBytes[32]:
-    ## Returns the value from a storage position at a given address.
-    ##
-    ## data: address of the storage.
-    ## slot: integer of the position in the storage.
-    ## stateRoot: the state root used to search the state trie.
-    ## Returns: the value at this storage position.
-
-    let sn = stateNetwork.valueOr:
-      raise newException(ValueError, "State sub-network not enabled")
-
-    let slotValue = (
-      await sn.getStorageAtByStateRoot(
-        KeccakHash.fromBytes(stateRoot.bytes()), data.EthAddress, slot
-      )
-    ).valueOr:
-      raise newException(ValueError, "Unable to get storage slot")
-    return FixedBytes[32](slotValue.toBytesBE())
-
-  rpcServer.rpc("debug_getCodeByStateRoot") do(
-    data: web3Types.Address, stateRoot: web3types.Hash256
-  ) -> seq[byte]:
-    ## Returns code at a given address.
-    ##
-    ## data: address
-    ## stateRoot: the state root used to search the state trie.
-    ## Returns the code from the given address.
-
-    let sn = stateNetwork.valueOr:
-      raise newException(ValueError, "State sub-network not enabled")
-
-    let bytecode = (
-      await sn.getCodeByStateRoot(
-        KeccakHash.fromBytes(stateRoot.bytes()), data.EthAddress
-      )
-    ).valueOr:
-      raise newException(ValueError, "Unable to get code")
-
-    return bytecode.asSeq()
-
-  rpcServer.rpc("debug_getProofByStateRoot") do(
-    data: web3Types.Address, slots: seq[UInt256], stateRoot: web3types.Hash256
-  ) -> ProofResponse:
-    ## Returns information about an account and storage slots along with account
-    ## and storage proofs which prove the existence of the values in the state.
-    ##
-    ## data: address of the account.
-    ## slots: integers of the positions in the storage to return.
-    ## stateRoot: the state root used to search the state trie.
-    ## Returns: the proof response containing the account, account proof and storage proof
-
-    let sn = stateNetwork.valueOr:
-      raise newException(ValueError, "State sub-network not enabled")
-
-    let proofs = (
-      await sn.getProofsByStateRoot(
-        KeccakHash.fromBytes(stateRoot.bytes()), data.EthAddress, slots
-      )
-    ).valueOr:
-      raise newException(ValueError, "Unable to get proofs")
 
     var storageProof = newSeqOfCap[StorageProof](slots.len)
     for i, slot in slots:
