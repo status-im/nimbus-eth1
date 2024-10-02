2e9792a63fd175b67bef204c7ac942cf90c7d165
Jacek Sieka <jacek@status.im>
2024-10-02 16:46:28 +0200

    bump

--- fluffy/tools/beacon_lc_bridge/beacon_lc_bridge.nim
+++ fluffy/tools/beacon_lc_bridge/beacon_lc_bridge.nim
@@ -168,7 +168,7 @@ proc asPortalBlockData*(
       gasUsed: distinctBase(payload.gasUsed),
       timestamp: payload.timestamp.EthTime,
       extraData: bytes payload.extraData,
-      mixHash: payload.prevRandao.asEthHash,
+      mixHash: payload.prevRandao,
       nonce: default(BlockNonce),
       baseFeePerGas: Opt.some(payload.baseFeePerGas),
       withdrawalsRoot: withdrawalsRoot,
