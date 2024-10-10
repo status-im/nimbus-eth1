637a064aeb96c423435a87e51379ecb0877416db
chirag-parmar <chiragparmar12209@gmail.com>
2024-10-10 18:23:34 +0530

    revert changes on nimbus_verified_proxy

--- nimbus_verified_proxy/rpc/rpc_utils.nim
+++ nimbus_verified_proxy/rpc/rpc_utils.nim
@@ -9,25 +9,32 @@
 
 import
   std/typetraits,
-  eth/common/[hashes, headers, blocks, addresses, base],
+  eth/common/eth_types as etypes,
   eth/rlp,
-  web3/[engine_api_types, eth_api_types],
+  nimcrypto/hash,
+  stint,
+  web3,
+  web3/engine_api_types,
   ../../nimbus/db/core_db
 
+type
+  FixedBytes[N: static int] = primitives.FixedBytes[N]
+  Address = primitives.Address
+
 type ExecutionData* = object
-  parentHash*: Hash32
+  parentHash*: BlockHash
   feeRecipient*: Address
-  stateRoot*: Hash32
-  receiptsRoot*: Hash32
-  logsBloom*: Bytes256
-  prevRandao*: Bytes32
+  stateRoot*: BlockHash
+  receiptsRoot*: BlockHash
+  logsBloom*: FixedBytes[256]
+  prevRandao*: FixedBytes[32]
   blockNumber*: Quantity
   gasLimit*: Quantity
   gasUsed*: Quantity
   timestamp*: Quantity
   extraData*: DynamicBytes[0, 32]
   baseFeePerGas*: UInt256
-  blockHash*: Hash32
+  blockHash*: BlockHash
   transactions*: seq[TypedTransaction]
   withdrawals*: seq[WithdrawalV1]
 
@@ -70,9 +77,15 @@ proc asExecutionData*(payload: SomeExecutionPayload): ExecutionData =
       withdrawals: payload.withdrawals,
     )
 
+func toFixedBytes(d: MDigest[256]): FixedBytes[32] =
+  FixedBytes[32](d.data)
+
+template asEthHash(hash: BlockHash): etypes.Hash256 =
+  etypes.Hash256(distinctBase(hash))
+
 proc calculateTransactionData(
     items: openArray[TypedTransaction]
-): (Hash32, seq[TxOrHash], uint64) =
+): (etypes.Hash256, seq[TxOrHash], uint64) =
   ## returns tuple composed of
   ## - root of transactions trie
   ## - list of transactions hashes
@@ -84,28 +97,27 @@ proc calculateTransactionData(
     let tx = distinctBase(t)
     txSize = txSize + uint64(len(tx))
     tr.merge(rlp.encode(uint64 i), tx).expect "merge data"
-    txHashes.add(txOrHash keccak256(tx))
+    txHashes.add(txOrHash keccakHash(tx))
   let rootHash = tr.state(updateOk = true).expect "hash"
   (rootHash, txHashes, txSize)
 
-# This can change to type BlockHeader from eth_api_types?
-func blockHeaderSize(payload: ExecutionData, txRoot: Hash32): uint64 =
-  let bh = Header(
-    parentHash: payload.parentHash,
-    ommersHash: EMPTY_UNCLE_HASH,
-    coinbase: payload.feeRecipient,
-    stateRoot: payload.stateRoot,
+func blockHeaderSize(payload: ExecutionData, txRoot: etypes.Hash256): uint64 =
+  let bh = etypes.BlockHeader(
+    parentHash: payload.parentHash.asEthHash,
+    ommersHash: etypes.EMPTY_UNCLE_HASH,
+    coinbase: etypes.EthAddress payload.feeRecipient,
+    stateRoot: payload.stateRoot.asEthHash,
     transactionsRoot: txRoot,
-    receiptsRoot: payload.receiptsRoot,
-    logsBloom: payload.logsBloom,
-    difficulty: default(DifficultyInt),
-    number: distinctBase(payload.blockNumber),
+    receiptsRoot: payload.receiptsRoot.asEthHash,
+    logsBloom: distinctBase(payload.logsBloom).to(Bloom),
+    difficulty: default(etypes.DifficultyInt),
+    number: payload.blockNumber.distinctBase,
     gasLimit: distinctBase(payload.gasLimit),
     gasUsed: distinctBase(payload.gasUsed),
-    timestamp: EthTime(payload.timestamp),
-    extraData: bytes(payload.extraData),
+    timestamp: payload.timestamp.EthTime,
+    extraData: bytes payload.extraData,
     mixHash: payload.prevRandao,
-    nonce: default(Bytes8),
+    nonce: default(etypes.BlockNonce),
     baseFeePerGas: Opt.some payload.baseFeePerGas,
   )
   return uint64(len(rlp.encode(bh)))
@@ -118,10 +130,10 @@ proc asBlockObject*(p: ExecutionData): BlockObject {.raises: [ValueError].} =
   let headerSize = blockHeaderSize(p, txRoot)
   let blockSize = txSize + headerSize
   BlockObject(
-    number: Quantity(p.blockNumber),
+    number: web3.BlockNumber p.blockNumber,
     hash: p.blockHash,
     parentHash: p.parentHash,
-    sha3Uncles: EMPTY_UNCLE_HASH,
+    sha3Uncles: etypes.EMPTY_UNCLE_HASH,
     logsBloom: p.logsBloom,
     transactionsRoot: txRoot,
     stateRoot: p.stateRoot,
@@ -132,7 +144,7 @@ proc asBlockObject*(p: ExecutionData): BlockObject {.raises: [ValueError].} =
     gasLimit: p.gasLimit,
     gasUsed: p.gasUsed,
     timestamp: p.timestamp,
-    nonce: Opt.some(default(Bytes8)),
+    nonce: Opt.some(default(FixedBytes[8])),
     size: Quantity(blockSize),
     # TODO: It does not matter what we put here after merge blocks.
     # Other projects like `helios` return `0`, data providers like alchemy return
