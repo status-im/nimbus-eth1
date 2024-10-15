deadba2baa2e6eccde54b194353d7a88dcd7b7e1
chirag-parmar <chiragparmar12209@gmail.com>
2024-10-15 17:31:32 +0530

    update copyright year

--- tests/test_block_fixture.nim
+++ tests/test_block_fixture.nim
@@ -1,4 +1,4 @@
-# Copyright (c) 2022-2023 Status Research & Development GmbH
+# Copyright (c) 2023-2024 Status Research & Development GmbH
 # Licensed under either of
 #  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE))
 #  * MIT license ([LICENSE-MIT](LICENSE-MIT))
