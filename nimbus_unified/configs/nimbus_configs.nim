79aa0fb46e8221cc64a4c3a0b713bb5c924bcf99
Pedro Miranda <pedro.miranda@nimbus.team>
2024-10-23 12:10:35 +0100

    small repairs and additions

--- nimbus_unified/configs/nimbus_configs.nim
+++ nimbus_unified/configs/nimbus_configs.nim
@@ -12,7 +12,8 @@ type NimbusTasksError* = object of CatchableError
 
 ## Configuration
 ## TODO: implement a json (or other format like yaml) config reader for config reading (file config scenarios)
-## TODO: implement a command line reader to read arguments
+##  1) implement a command line reader to read arguments
+##  2) good option to adhere to other projects conventions and use the in place support to read and load
 type NimbusConfig* = object
   configTable: Table[string, string]
 
@@ -20,16 +21,19 @@ type NimbusConfig* = object
 type TaskParameters* = object
   name*: string
   configs*: string
-  beaconNodeConfigs*: BeaconNodeConf
-    # TODO: replace this with the extracted configs from NimbusConfig needed by the worker
+  beaconNodeConfigs*: BeaconNodeConf #
 
 ## Task shutdown flag
+##
 ## The behaviour required: this thread needs to atomically change the flag value when
 ##  a shutdown is required or when detects a stopped thread.
+##
 ## Given the behaviour wanted, atomic operations are sufficient without barriers or fences. Compilers
 ##  may reorder instructions, but given that the order is not important, this does not affect
 ##  the semantic wanted: If instructions are reordered, the worker will fail to read on the current iteration
 ##  but will read it correctly on the next iteration ( this thread is the only on which changes the flag behaviour,
 ##  and will always change it to true)
+##
+## With this we avoid the overhead of locks
 var isShutDownRequired*: Atomic[bool]
 isShutDownRequired.store(false)
