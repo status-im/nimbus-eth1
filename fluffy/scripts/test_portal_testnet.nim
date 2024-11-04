226d084e90207cbd349f763e1cea18fee23f5189
Kim De Mey <kim.demey@gmail.com>
2024-11-04 18:02:56 +0100

    Fix flaky test_portal_testnet CI test with retries (#2808)
    * Add two, not so great, quick fix attempts for flaky CI
    
    * Another attempt
    
    * Remove the increased sleep in test_history_network
    
    Need something better here.

--- fluffy/scripts/test_portal_testnet.nim
+++ fluffy/scripts/test_portal_testnet.nim
@@ -94,7 +94,7 @@ proc retryUntil[A](
     f: FutureCallback[A], c: CheckCallback[A], checkFailMessage: string, nodeIdx: int
 ): Future[A] =
   # some reasonable limits, which will cause waits as: 1, 2, 4, 8, 16, 32 seconds
-  return withRetries(f, c, 1, seconds(1), checkFailMessage, nodeIdx)
+  return withRetries(f, c, 2, seconds(1), checkFailMessage, nodeIdx)
 
 # Note:
 # When doing json-rpc requests following `RpcPostError` can occur:
