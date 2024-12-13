1d5a48e1539a732a672df742d3faebf4b791aacc
andri lim <jangko128@gmail.com>
2024-12-13 10:47:35 +0700

    Feature: configurable gas limit when building execution payload (#2931)
    * Feature: configurable gas limit when building execution payload
    
    * Raise default gas limit to 30M

--- nimbus/constants.nim
+++ nimbus/constants.nim
@@ -49,7 +49,7 @@ const
   GENESIS_EXTRA_DATA* =                     ""
   GAS_LIMIT_MINIMUM* =                      5000
   GAS_LIMIT_MAXIMUM* =                      int64.high.GasInt # Maximum the gas limit (2^63-1).
-  DEFAULT_GAS_LIMIT* =                      8_000_000
+  DEFAULT_GAS_LIMIT* =                      30_000_000
 
   EMPTY_SHA3* =                             hash32"c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"
 
