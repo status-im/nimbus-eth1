25e162f78170e430cc8bf3e2cff74b9d04c73ad9
Pedro Miranda <pedro.miranda@nimbus.team>
2025-01-06 17:39:13 +0000

    changed copyright year (2025)

--- nimbus_unified/run_nimbus_unified.sh
+++ nimbus_unified/run_nimbus_unified.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # nimbus_unified
-# Copyright (c) 2024 Status Research & Development GmbH
+# Copyright (c) 2025 Status Research & Development GmbH
 # Licensed and distributed under either of
 #   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
 #   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
@@ -18,6 +18,10 @@
 exec build/nimbus_unified \
 --network=holesky \
 --data-dir="build/data/shared_holesky_0" \
+# --network=mainnet \
+# --data-dir=build/data/shared_mainnet_0 \
 --tcp-port=9000 \
 --udp-port=9000 \
---log-level=TRACE
+--log-level=TRACE \
+--rest \
+--rest-port=5052
