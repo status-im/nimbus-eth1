785a51824179ec9d63edd5ed6e89320b6218d2eb
jangko <jangko128@gmail.com>
2025-01-13 14:03:20 +0700

    Fix copyright year

--- nimbus/graphql/ethapi.nim
+++ nimbus/graphql/ethapi.nim
@@ -1,5 +1,5 @@
 # nim-graphql
-# Copyright (c) 2021-2024 Status Research & Development GmbH
+# Copyright (c) 2021-2025 Status Research & Development GmbH
 # Licensed under either of
 #  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE))
 #  * MIT license ([LICENSE-MIT](LICENSE-MIT))
