62aeacde7b51bd1c7a40e3956f2d63addfad04b0
Jordan Hrycaj <jordan@status.im>
2025-01-15 13:40:26 +0000

    Re-name metrics variable to better fit into the current naming schemes
    also:
      Force metrics update when all peers vanish. There might be not much
      activity in that case so that the set of metrics might become stale.

--- nimbus/sync/beacon/worker/update/metrics.nim
+++ nimbus/sync/beacon/worker/update/metrics.nim
@@ -1,5 +1,5 @@
 # Nimbus
-# Copyright (c) 2023-2024 Status Research & Development GmbH
+# Copyright (c) 2023-2025 Status Research & Development GmbH
 # Licensed and distributed under either of
 #   * MIT license (license terms in the root directory or at
 #     https://opensource.org/licenses/MIT).
@@ -18,68 +18,68 @@ import
   ../headers_staged/staged_queue,
   ".."/[blocks_unproc, headers_unproc]
 
-declareGauge beacon_base, "" &
+declareGauge nec_base, "" &
   "Max block number of imported finalised blocks"
 
-declareGauge beacon_latest, "" &
+declareGauge nec_execution_head, "" &
   "Block number of latest imported blocks"
   
-declareGauge beacon_coupler, "" &
+declareGauge nec_sync_coupler, "" &
   "Max block number for header chain starting at genesis"
 
-declareGauge beacon_dangling, "" &
+declareGauge nec_sync_dangling, "" &
   "Starting/min block number for higher up headers chain"
 
-declareGauge beacon_head, "" &
+declareGauge nec_sync_head, "" &
   "Ending/max block number of higher up headers chain"
 
-declareGauge beacon_target, "" &
+declareGauge nec_consensus_head, "" &
   "Block number of sync target (would be consensus header)"
 
 
-declareGauge beacon_header_lists_staged, "" &
+declareGauge nec_sync_header_lists_staged, "" &
   "Number of header list records staged for serialised processing"
 
-declareGauge beacon_headers_unprocessed, "" &
+declareGauge nec_sync_headers_unprocessed, "" &
   "Number of block numbers ready to fetch and stage headers"
 
-declareGauge beacon_block_lists_staged, "" &
+declareGauge nec_sync_block_lists_staged, "" &
   "Number of block list records staged for importing"
 
-declareGauge beacon_blocks_unprocessed, "" &
+declareGauge nec_sync_blocks_unprocessed, "" &
   "Number of block numbers ready to fetch and stage block data"
 
 
-declareGauge beacon_buddies, "" &
+declareGauge nec_sync_peers, "" &
   "Number of currently active worker instances"
 
 
 template updateMetricsImpl(ctx: BeaconCtxRef) =
-  metrics.set(beacon_base, ctx.chain.baseNumber().int64)
-  metrics.set(beacon_latest, ctx.chain.latestNumber().int64)
-  metrics.set(beacon_coupler, ctx.layout.coupler.int64)
-  metrics.set(beacon_dangling, ctx.layout.dangling.int64)
-  metrics.set(beacon_head, ctx.layout.head.int64)
+  metrics.set(nec_base, ctx.chain.baseNumber().int64)
+  metrics.set(nec_execution_head, ctx.chain.latestNumber().int64)
+  metrics.set(nec_sync_coupler, ctx.layout.coupler.int64)
+  metrics.set(nec_sync_dangling, ctx.layout.dangling.int64)
+  metrics.set(nec_sync_head, ctx.layout.head.int64)
 
   # Show last valid state.
   if 0 < ctx.target.consHead.number:
-    metrics.set(beacon_target, ctx.target.consHead.number.int64)
+    metrics.set(nec_consensus_head, ctx.target.consHead.number.int64)
 
-  metrics.set(beacon_header_lists_staged, ctx.headersStagedQueueLen())
-  metrics.set(beacon_headers_unprocessed,
+  metrics.set(nec_sync_header_lists_staged, ctx.headersStagedQueueLen())
+  metrics.set(nec_sync_headers_unprocessed,
               (ctx.headersUnprocTotal() + ctx.headersUnprocBorrowed()).int64)
 
-  metrics.set(beacon_block_lists_staged, ctx.blocksStagedQueueLen())
-  metrics.set(beacon_blocks_unprocessed,
+  metrics.set(nec_sync_block_lists_staged, ctx.blocksStagedQueueLen())
+  metrics.set(nec_sync_blocks_unprocessed,
               (ctx.blocksUnprocTotal() + ctx.blocksUnprocBorrowed()).int64)
 
-  metrics.set(beacon_buddies, ctx.pool.nBuddies)
+  metrics.set(nec_sync_peers, ctx.pool.nBuddies)
 
 # ---------------
 
-proc updateMetrics*(ctx: BeaconCtxRef) =
+proc updateMetrics*(ctx: BeaconCtxRef; force = false) =
   let now = Moment.now()
-  if ctx.pool.nextUpdate < now:
+  if ctx.pool.nextUpdate < now or force:
     ctx.updateMetricsImpl()
     ctx.pool.nextUpdate = now + metricsUpdateInterval
 
