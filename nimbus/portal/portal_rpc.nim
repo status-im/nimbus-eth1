45ced0ca9e87385b1255ac95b462868a6a910690
Advaita Saha <adv11sphs@gmail.com>
2025-01-24 23:27:55 +0530

    initial structuring of portal integration

---
+++ nimbus/portal/portal_rpc.nim
@@ -0,0 +1,11 @@
+# Nimbus
+# Copyright (c) 2025 Status Research & Development GmbH
+# Licensed under either of
+#  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
+#    http://www.apache.org/licenses/LICENSE-2.0)
+#  * MIT license ([LICENSE-MIT](LICENSE-MIT) or
+#    http://opensource.org/licenses/MIT)
+# at your option. This file may not be copied, modified, or distributed except
+# according to those terms.
+
+# Creates the interface for any portal client with rpc endpoints to fetch interface
