fc4969d2ee93d620a29b1d186554523e45bfebef
kdeme <7857583+kdeme@users.noreply.github.com>
2025-02-14 22:00:16 +0100

    Bump nimbus-eth2 and required nim-kzg4884 (#3074)

--- nimbus_verified_proxy/nimbus_verified_proxy.nim
+++ nimbus_verified_proxy/nimbus_verified_proxy.nim
@@ -242,7 +242,8 @@ proc run*(
 
       targetGossipState = getTargetGossipState(
         slot.epoch, cfg.ALTAIR_FORK_EPOCH, cfg.BELLATRIX_FORK_EPOCH,
-        cfg.CAPELLA_FORK_EPOCH, cfg.DENEB_FORK_EPOCH, cfg.ELECTRA_FORK_EPOCH, isBehind,
+        cfg.CAPELLA_FORK_EPOCH, cfg.DENEB_FORK_EPOCH, cfg.ELECTRA_FORK_EPOCH,
+        cfg.FULU_FORK_EPOCH, isBehind,
       )
 
     template currentGossipState(): auto =
