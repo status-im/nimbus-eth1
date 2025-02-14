8f717aa2c34bde0e74c9e4750ac8c434e7b072d8
Jordan Hrycaj <jordan@status.im>
2025-02-14 11:58:03 +0000

    Update comment

--- execution_chain/nimbus_import.nim
+++ execution_chain/nimbus_import.nim
@@ -358,13 +358,8 @@ proc importBlocks*(conf: NimbusConf, com: CommonRef) =
         persistBlock()
         checkpoint()
 
-  # There is some fringe case where the `persister` is not in sync to the DB
-  # state. Checkpointing here would then produce a mismatch of the
-  # `getSavedStateBlockNumber()` value and the state which typically fails
-  # with a state root mismatch when the next program uses the DB.
-  #
-  # This all happens only if there were no `era` or `era1` files that could be
-  # imported.
+  # If there were no blocks written, we will not have loaded the block number
+  # and therefore should not call checkpoint().
   if 0 < persister.stats.blocks:
     checkpoint(true)
 
