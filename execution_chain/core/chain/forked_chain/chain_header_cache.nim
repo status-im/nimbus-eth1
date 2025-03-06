fcd539a4e13bf9e42397f7752ecfc4881bd223a2
Jordan Hrycaj <jordan@status.im>
2025-03-06 17:02:20 +0000

    Fix return code
    details
      For a registered hash the get-by-hash function must return the
      header if it exists, or an error code `true`.
    
      Querying the hash of an unregistered entry, the error code
      `false` is returned.

--- execution_chain/core/chain/forked_chain/chain_header_cache.nim
+++ execution_chain/core/chain/forked_chain/chain_header_cache.nim
@@ -274,12 +274,11 @@ proc fcHeaderGet*(fc: ForkedCacheRef; hash: Hash32): Result[Header,bool] =
   ## `hash` argument was not registered.
   ##
   fc.state.byHash.withValue(hash, val):
-    if val[] != 0:
-      if val[] < fc.state.ante.number:
-        return err(true) # no need to lookup database
+    if fc.state.ante.number < val[]: # no need to lookup database, otherwise
       var hdr = fc.chain.baseTxFrame.getHeader(val[]).valueOr:
         return err(true)
       return ok(move hdr)
+    return err(true)
   err(false)
 
 proc fcHeaderGet*(fc: ForkedCacheRef; bn: BlockNumber): Opt[Header] =
