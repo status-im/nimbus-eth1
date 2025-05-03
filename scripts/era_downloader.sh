8ac2cde867e701260790ad789ff50a88cd1b6100
Advaita Saha <adv11sphs@gmail.com>
2025-05-04 04:28:33 +0530

    fix: copyright

--- scripts/era_downloader.sh
+++ scripts/era_downloader.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Copyright (c) 2025 Status Research & Development GmbH. Licensed under
+# either of:
+# - Apache License, version 2.0
+# - MIT license
+# at your option. This file may not be copied, modified, or distributed except
+# according to those terms.
+
 # Usage: ./download_era.sh <download_url> <download_path>
 set -e
 
