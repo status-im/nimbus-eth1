c8784b0f2297a4184765d1b87b2188757c02a8c3
Jordan Hrycaj <jordan@status.im>
2025-05-14 10:32:18 +0100

    Remove `--debug-beacon-sync-blocks-queue-hwm` command line option
    why
      Not needed anymore as the block queue will run on a smaller memory
      footprint, anyway.

--- execution_chain/sync/beacon/worker/start_stop.nim
+++ execution_chain/sync/beacon/worker/start_stop.nim
@@ -49,21 +49,7 @@ proc setupServices*(ctx: BeaconCtxRef; info: static[string]) =
   # Start in suspended mode
   ctx.hibernate = true
 
-  # Take it easy and assume that queue records contain full block list (which
-  # is mostly the case anyway.) So the the staging queue is limited by the
-  # number of sub-list records rather than the number of accumulated block
-  # objects.
-  let hwm = if blocksStagedLwm <= ctx.pool.blkStagedHwm: ctx.pool.blkStagedHwm
-            else: blocksStagedHwmDefault
-  ctx.pool.blkStagedLenHwm = (hwm + nFetchBodiesBatch - 1) div nFetchBodiesBatch
-
-  # Set blocks batch import queue size
-  if ctx.pool.blkStagedHwm != 0:
-    debug info & ": import block lists queue", limit=ctx.pool.blkStagedLenHwm
-  ctx.pool.blkStagedHwm = hwm
-
-  # Set up header cache descriptor. This will evenually be integrated
-  # into `ForkedChainRef` (i.e. `ctx.pool.chain`.)
+  # Set up header cache descriptor
   ctx.pool.hdrCache = HeaderChainRef.init(ctx.pool.chain)
 
   # Set up the notifier informing when a new syncer session has started.
