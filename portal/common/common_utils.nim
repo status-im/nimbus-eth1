28f5674750683f655e7bb8ddeea9ec2d374478e8
bhartnett <benjamin@status.im>
2025-05-21 15:25:51 +0800

    Fix copyright.

--- portal/common/common_utils.nim
+++ portal/common/common_utils.nim
@@ -1,5 +1,5 @@
 # Nimbus
-# Copyright (c) 2021-2024 Status Research & Development GmbH
+# Copyright (c) 2021-2025 Status Research & Development GmbH
 # Licensed and distributed under either of
 #   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
 #   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
