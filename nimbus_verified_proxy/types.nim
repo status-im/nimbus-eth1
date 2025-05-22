dfc281d837de6a5592069ef07c17ec5131c4d6df
chirag-parmar <chiragparmar12209@gmail.com>
2025-05-22 12:05:09 +0530

    housekeeping

--- nimbus_verified_proxy/types.nim
+++ nimbus_verified_proxy/types.nim
@@ -5,12 +5,7 @@
 #   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
 # at your option. This file may not be copied, modified, or distributed except according to those terms.
 
-import
-  json_rpc/rpcproxy,
-  stint,
-  json_rpc/[rpcserver, rpcclient, rpcproxy],
-  web3/eth_api_types,
-  ./header_store
+import stint, json_rpc/[rpcclient, rpcproxy], web3/eth_api_types, ./header_store
 
 type
   VerifiedRpcProxy* = ref object
