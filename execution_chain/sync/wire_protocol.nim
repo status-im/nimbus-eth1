6ce4568e628ea33b9e3d55e035e5e68df7417a39
andri lim <jangko128@gmail.com>
2025-06-11 13:31:58 +0700

    Query and store blob transactions into txpool (#3375)
    * Query and store blob transactions into txpool
    
    Also a breakdown of transaction broadcast. But this time
    it's only handle incoming transactions, both regular and blob
    transactions. No rebroadcast whatsoever.
    
    It is a good change to let this incoming transactions handling
    become stable before we add the real broadcast things.
    
    * Additional validation: compare announced vs received stat

--- execution_chain/sync/wire_protocol.nim
+++ execution_chain/sync/wire_protocol.nim
@@ -10,11 +10,13 @@
 import
   ./wire_protocol/requester,
   ./wire_protocol/responder,
+  ./wire_protocol/broadcast,
   ./wire_protocol/types,
   ./wire_protocol/setup
 
 export
   requester,
   responder,
+  broadcast,
   types,
   setup
