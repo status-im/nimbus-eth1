d92cba5e2030b88723d2cd6ed3fc7cc3b4d9d675
jangko <jangko128@gmail.com>
2025-06-16 18:50:56 +0700

    Rlpx handshake using the latest version of a protocol capabilities

--- execution_chain/networking/rlpx.nim
+++ execution_chain/networking/rlpx.nim
@@ -998,10 +998,25 @@ proc removePeer(network: EthereumNode, peer: Peer) =
           if observer.protocols.len == 0 or peer.supports(observer.protocols):
             observer.onPeerDisconnected(peer)
 
+proc selectCapsByLatestVersion(peer: Peer): seq[ProtocolInfo] =
+  # Avoid using multiple capability handshake when connecting to a peer.
+  # Use only the latest capability version. e.g. choose eth/69 over eth/68.
+  # But other capabilities with different name is okay. e.g. snap/1
+  var map: Table[string, ProtocolInfo]
+  for proto in peer.dispatcher.activeProtocols:
+    map.withValue(proto.capability.name, val) do:
+      if proto.capability.version > val.capability.version:
+        val[] = proto
+    do:
+      map[proto.capability.name] = proto
+
+  for proto in map.values:
+    result.add proto
+
 proc callDisconnectHandlers(
     peer: Peer, reason: DisconnectionReason
 ): Future[void] {.async: (raises: []).} =
-  let futures = peer.dispatcher.activeProtocols
+  let futures = peer.selectCapsByLatestVersion()
     .filterIt(it.onPeerDisconnected != nil)
     .mapIt(it.onPeerDisconnected(peer, reason))
 
@@ -1102,7 +1117,7 @@ proc postHelloSteps(
   # the network and to yield on their `nextMsg` waits.
   #
 
-  let handshakes = peer.dispatcher.activeProtocols
+  let handshakes = peer.selectCapsByLatestVersion()
     .filterIt(it.onPeerConnected != nil)
     .mapIt(it.onPeerConnected(peer))
 
