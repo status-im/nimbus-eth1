d7bdac102103fb24564a9b06bdac0e4bbf8fe01b
Advaita Saha <adv11sphs@gmail.com>
2025-06-23 16:22:21 +0530

    some more unwanted code cleanup

--- execution_chain/evm/secp256r1verify.nim
+++ execution_chain/evm/secp256r1verify.nim
@@ -12,49 +12,6 @@ import
   libp2p/crypto/ecnist,
   bearssl/[ec, hash]
 
-proc `-`(x: uint32): uint32 {.inline.} =
-  result = (0xFFFF_FFFF'u32 - x) + 1'u32
-
-proc GT(x, y: uint32): uint32 {.inline.} =
-  var z = cast[uint32](y - x)
-  result = (z xor ((x xor y) and (x xor z))) shr 31
-
-proc CMP(x, y: uint32): int32 {.inline.} =
-  cast[int32](GT(x, y)) or -(cast[int32](GT(y, x)))
-
-proc EQ0(x: int32): uint32 {.inline.} =
-  var q = cast[uint32](x)
-  result = not (q or -q) shr 31
-
-proc NEQ(x, y: uint32): uint32 {.inline.} =
-  var q = cast[uint32](x xor y)
-  result = ((q or -q) shr 31)
-
-proc LT0(x: int32): uint32 {.inline.} =
-  result = cast[uint32](x) shr 31
-
-proc checkScalar*(scalar: openArray[byte], curve: cint): uint32 =
-  ## Return ``1`` if all of the following hold:
-  ##   - len(``scalar``) <= ``orderlen``
-  ##   - ``scalar`` != 0
-  ##   - ``scalar`` is lower than the curve ``order``.
-  ##
-  ## Otherwise, return ``0``.
-  var impl = ecGetDefault()
-  var orderlen: uint = 0
-  var order = cast[ptr UncheckedArray[byte]](impl.order(curve, orderlen))
-
-  var z = 0'u32
-  var c = 0'i32
-  for u in scalar:
-    z = z or u
-  if len(scalar) == int(orderlen):
-    for i in 0 ..< len(scalar):
-      c = c or (-(cast[int32](EQ0(c))) and CMP(scalar[i], order[i]))
-  else:
-    c = -1
-  result = NEQ(z, 0'u32) and LT0(c)
-
 proc isInfinityByte*(data: openArray[byte]): bool =
   ## Check if all values in ``data`` are zero.
   for b in data:
