e20495163246312a1a09cefd4f4ef3973ab65667
Jordan Hrycaj <jordan@status.im>
2025-07-23 10:47:29 +0100

    Provide tracer framework with intercepting syncer session handlers

---
+++ tools/syncer/trace/trace_desc.nim
@@ -0,0 +1,215 @@
+# Nimbus
+# Copyright (c) 2025 Status Research & Development GmbH
+# Licensed and distributed under either of
+#   * MIT license (license terms in the root directory or at
+#     https://opensource.org/licenses/MIT).
+#   * Apache v2 license (license terms in the root directory or at
+#     https://www.apache.org/licenses/LICENSE-2.0).
+# at your option. This file may not be copied, modified, or distributed
+# except according to those terms.
+
+## Trace environment descriptor and helpers
+##
+## TODO:
+## * n/a
+##
+
+{.push raises:[].}
+
+import
+  std/[net, streams],
+  pkg/[chronos, eth/common],
+  ../../../execution_chain/sync/wire_protocol,
+  ../../../execution_chain/sync/beacon/beacon_desc,
+  ../../../execution_chain/sync/beacon/worker/worker_desc
+
+export
+  beacon_desc,
+  worker_desc
+
+const
+  TraceVersionID* = 20250828
+
+  TraceSetupID* = 1                   ## Phase 1 layout ID, prepare
+  TraceRunnerID* = 10                 ## Phase 2 layout ID, full execution
+
+type
+  StopIfEosHdl* = proc(trc: TraceRef) {.gcsafe, raises: [].}
+    ## Terminate trace if the number of sessions is exhausted
+
+  TraceRef* = ref object of BeaconHandlersSyncRef
+    ## Overlay handlers extended by descriptor data
+    ctx*: BeaconCtxRef                ## Parent context
+    outStream*: Stream                ## Output file with capture records
+    backup*: BeaconHandlersRef        ## Can restore previous handlers
+    started*: Moment                  ## Start time
+    sessions*: int                    ## Initial number of sessions
+    remaining*: int                   ## Number of sessions left to run
+    stopIfEos*: StopIfEosHdl          ## Auto-disable trace when needed
+    serial: uint                      ## Unique record ID
+
+  # -------------
+
+  TraceRecType* = enum
+    TrtOops = 0
+    TrtVersionInfo = 1
+
+    TrtSyncActvFailed
+    TrtSyncActivated
+    TrtSyncHibernated
+
+    TrtSchedDaemonBegin
+    TrtSchedDaemonEnd
+    TrtSchedStart
+    TrtSchedStop
+    TrtSchedPool
+    TrtSchedPeerBegin
+    TrtSchedPeerEnd
+
+    TrtFetchHeaders
+    TrtSyncHeaders
+
+    TrtFetchBodies
+    TrtSyncBodies
+
+    TrtImportBlock
+    TrtSyncBlock
+
+  TraceRecBase* = object of RootObj
+    ## Trace context applicable with and without known peer
+    time*: Duration                   ## Relative to `TraceRef.started`
+    serial*: uint                     ## Increasing serial number
+    frameID*: uint                    ## Begin/end frame
+    nPeers*: uint
+    syncState*: SyncState
+    chainMode*: HeaderChainMode
+    poolMode*: bool
+
+    baseNum*: BlockNumber             ## Max finalised number from `FC` module
+    latestNum*: BlockNumber           ## Number of latest branch head
+    antecedent*: BlockNumber          ## Lower end of header chain cache
+
+    hdrUnprLen*: uint64               ## # unprocessed header entries
+    hdrUnprChunks*: uint              ## # unprocessed header iv segments
+    hdrUnprLast*: BlockNumber         ## last avail block number
+    hdrUnprLastLen*: uint64           ## size of last block number interval
+
+    blkUnprLen*: uint64               ## # unprocessed block entries
+    blkUnprChunks*: uint              ## # unprocessed block iv segments
+    blkUnprLeast*: BlockNumber        ## least avail block number
+    blkUnprLeastLen*: uint64          ## size of first interval
+
+    stateAvail*: int                  ## Bitmask: 1=peerCtrl, 2=peerID, etc.
+    peerCtrl*: BuddyRunState          ##  1) Rlp encoded `Opt[seq[xxx]]` would
+    peerID*: Hash                     ##  2) .. need manual decoder/reader
+    nHdrErrors*: uint8                ##  4) # header comm. errors
+    nBlkErrors*: uint8                ##  8) # body comm. errors
+    slowPeer*: Hash                   ## 16) Registered slow peer
+
+
+  TraceVersionInfo* = object of TraceRecBase
+    version*: uint
+    networkId*: NetworkId
+
+  # -------------
+
+  TraceSyncActvFailed* = object of TraceRecBase
+
+  TraceSyncActivated* = object of TraceRecBase
+    head*: Header                     ## Part of environment
+    finHash*: Hash32                  ## Part of environment
+
+  TraceSyncHibernated* = object of TraceRecBase
+
+  # -------------
+
+  TraceSchedDaemonBegin* = object of TraceRecBase
+    ## Environment is captured before the daemon handler body is executed.
+
+  TraceSchedDaemonEnd* = object of TraceRecBase
+    ## Environment is captured when leaving the daemon handler.
+    idleTime*: Duration               ## Suggested idle time
+
+  TraceSchedStart* = object of TraceRecBase
+    ## Environment is captured when leaving sched the start handler.
+    peerIP*: IpAddress                ## Descriptor argument
+    peerPort*: Port                   ## Descriptor argument
+    accept*: bool                     ## Result/return code
+
+  TraceSchedStop* = object of TraceRecBase
+    ## Environment is captured when leaving the sched stop handler.
+    peerIP*: IpAddress                ## Descriptor argument
+    peerPort*: Port                   ## Descriptor argument
+
+  TraceSchedPool* = object of TraceRecBase
+    ## Environment is captured leaving the pool handler.
+    peerIP*: IpAddress                ## Descriptor argument
+    peerPort*: Port                   ## Descriptor argument
+    last*: bool                       ## Request argument
+    laps*: uint                       ## Request argument
+    stop*: bool                       ## Result/return code
+
+  TraceSchedPeerBegin* = object of TraceRecBase
+    ## Environment is captured before the peer handler body is executed.
+    peerIP*: IpAddress                ## Descriptor argument
+    peerPort*: Port                   ## Descriptor argument
+
+  TraceSchedPeerEnd* = object of TraceRecBase
+    ## Environment is captured when leaving peer handler.
+    idleTime*: Duration               ## Suggested idle time
+
+  # -------------
+
+  TraceFetchHeaders* = object of TraceRecBase
+    ## Environment is captured after the `getBlockHeaders()` handler is run.
+    req*: BlockHeadersRequest         ## Fetch request
+    ivReq*: BnRange                   ## Request as interval of block numbers
+    fieldAvail*: uint                 ## Bitmask: 1=fetched, 2=error
+    fetched*: FetchHeadersData        ## If dowloaded successfully
+    error*: BeaconError
+
+  TraceSyncHeaders* = object of TraceRecBase
+    ## Environment is captured when the `syncBlockHeaders()` handler is run.
+
+
+  TraceFetchBodies* = object of TraceRecBase
+    ## Environment is captured after the `getBlockBodies()` handler is run.
+    req*: BlockBodiesRequest          ## Fetch request
+    ivReq*: BnRange                   ## Request as interval of block numbers
+    fieldAvail*: uint                 ## Bitmask: 1=fetchd, 2=error
+    fetched*: FetchBodiesData         ## If dowloaded successfully
+    error*: BeaconError
+
+  TraceSyncBodies* = object of TraceRecBase
+    ## Environment is captured when the `syncBlockBodies()` handler is run.
+
+
+  TraceImportBlock* = object of TraceRecBase
+    ## Environment is captured after the `importBlock()` handler is run.
+    ethBlock*: EthBlock               ## Request argument
+    effPeerID*: Hash                  ## Request argument
+    fieldAvail*: uint                 ## Bitmask: 1=elapsed, 2=error
+    elapsed*: Duration                ## Processing time on success
+    error*: BeaconError
+
+  TraceSyncBlock* = object of TraceRecBase
+    ## Environment is captured after the `syncImportBlock()` handler is run.
+
+# ------------------------------------------------------------------------------
+# Public helpers
+# ------------------------------------------------------------------------------
+
+func trace*(ctx: BeaconCtxRef): TraceRef =
+  ## Getter, get trace descriptor (if any)
+  if ctx.handler.version == TraceRunnerID:
+    return ctx.handler.TraceRef
+
+func newSerial*(trc: TraceRef): uint64 =
+  trc.serial.inc
+  if trc.serial == 0:
+    trc.serial.inc
+  trc.serial
+
+# ------------------------------------------------------------------------------
+# End
+# ------------------------------------------------------------------------------
---
+++ tools/syncer/trace/trace_setup/setup_sched.nim
@@ -0,0 +1,177 @@
+# Nimbus
+# Copyright (c) 2025 Status Research & Development GmbH
+# Licensed and distributed under either of
+#   * MIT license (license terms in the root directory or at
+#     https://opensource.org/licenses/MIT).
+#   * Apache v2 license (license terms in the root directory or at
+#     https://www.apache.org/licenses/LICENSE-2.0).
+# at your option. This file may not be copied, modified, or distributed
+# except according to those terms.
+
+## Overlay handler for trace environment
+
+{.push raises:[].}
+
+import
+  pkg/[chronicles, chronos],
+  ../../../../execution_chain/networking/p2p,
+  ../trace_desc,
+  ./[setup_helpers, setup_write]
+
+logScope:
+  topics = "beacon trace"
+
+# ------------------------------------------------------------------------------
+# Private helpers
+# ------------------------------------------------------------------------------
+
+proc getIP(buddy: BeaconBuddyRef): IpAddress =
+  buddy.peer.remote.node.address.ip
+
+proc getPort(buddy: BeaconBuddyRef): Port =
+  let peer = buddy.peer
+  if peer.remote.node.address.tcpPort != Port(0):
+    peer.remote.node.address.tcpPort
+  else:
+    peer.remote.node.address.udpPort
+
+# ------------------------------------------------------------------------------
+# Public functions
+# ------------------------------------------------------------------------------
+
+proc schedDaemonTrace*(
+    ctx: BeaconCtxRef;
+      ): Future[Duration]
+      {.async: (raises: []).} =
+  ## Replacement for `schedDaemon()` handler which in addition
+  ## write data to the output stream for tracing.
+  ##
+  var tBeg: TraceSchedDaemonBegin
+  tBeg.init ctx
+  tBeg.frameID = tBeg.serial
+  ctx.traceWrite tBeg
+
+  trace "+Daemon", serial=tBeg.serial, frameID=tBeg.frameID.idStr,
+    syncState=tBeg.syncState
+
+  let idleTime = await ctx.trace.backup.schedDaemon ctx
+
+  var tEnd: TraceSchedDaemonEnd
+  tEnd.init ctx
+  tEnd.frameID = tBeg.serial # refers back to `tBeg` capture
+  tEnd.idleTime = idleTime
+  ctx.traceWrite tEnd
+
+  if 0 < tEnd.serial:
+    trace "-Daemon", serial=tEnd.serial, frameID=tEnd.frameID.idStr,
+      syncState=tBeg.syncState, idleTime=idleTime.toStr
+  else:
+    trace "-Daemon (blind)", serial="n/a", frameID=tEnd.frameID.idStr,
+      syncState=tBeg.syncState, idleTime=idleTime.toStr
+
+  return idleTime
+
+
+proc schedStartTrace*(buddy: BeaconBuddyRef): bool =
+  ## Similar to `schedDaemonTrace()`
+  ##
+  let
+    ctx = buddy.ctx
+    acceptOk = ctx.trace.backup.schedStart(buddy)
+
+  if not ctx.hibernate:
+    var tRec: TraceSchedStart
+    tRec.init buddy
+    tRec.frameID = tRec.serial
+    tRec.peerIP = buddy.getIP()
+    tRec.peerPort = buddy.getPort()
+    tRec.accept = acceptOk
+    buddy.traceWrite tRec
+
+    trace "=StartPeer", peer=($buddy.peer), peerID=buddy.peerID.short,
+      serial=tRec.serial, frameID=tRec.frameID.idStr,
+      syncState=tRec.syncState
+
+  acceptOk
+
+
+proc schedStopTrace*(buddy: BeaconBuddyRef) =
+  ## Similar to `schedDaemonTrace()`
+  ##
+  let ctx = buddy.ctx
+
+  ctx.trace.backup.schedStop(buddy)
+
+  if not ctx.hibernate:
+    var tRec: TraceSchedStop
+    tRec.init buddy
+    tRec.frameID = tRec.serial
+    tRec.peerIP = buddy.getIP()
+    tRec.peerPort = buddy.getPort()
+    buddy.traceWrite tRec
+
+    trace "=StopPeer", peer=($buddy.peer), peerID=buddy.peerID.short,
+      serial=tRec.serial, frameID=tRec.frameID.idStr,
+      syncState=tRec.syncState
+
+
+proc schedPoolTrace*(buddy: BeaconBuddyRef; last: bool; laps: int): bool =
+  ## Similar to `schedDaemonTrace()`
+  ##
+  let stopOk = buddy.ctx.trace.backup.schedPool(buddy, last, laps)
+
+  var tRec: TraceSchedPool
+  tRec.init buddy
+  tRec.frameID = tRec.serial
+  tRec.peerIP = buddy.getIP()
+  tRec.peerPort = buddy.getPort()
+  tRec.last = last
+  tRec.laps = laps.uint
+  tRec.stop = stopOk
+  buddy.traceWrite tRec
+
+  trace "=Pool", peer=($buddy.peer), peerID=buddy.peerID.short,
+    serial=tRec.serial, frameID=tRec.frameID.idStr
+
+  stopOk
+
+
+proc schedPeerTrace*(
+    buddy: BeaconBuddyRef;
+      ): Future[Duration]
+      {.async: (raises: []).} =
+  ## Similar to `schedDaemonTrace()`
+  ##
+  let
+    ctx = buddy.ctx
+    noisy = not ctx.hibernate
+
+  var tBeg: TraceSchedPeerBegin
+  if noisy:
+    tBeg.init buddy
+    tBeg.frameID = tBeg.serial
+    tBeg.peerIP = buddy.getIP()
+    tBeg.peerPort = buddy.getPort()
+    buddy.traceWrite tBeg
+
+    trace "+Peer", peer=($buddy.peer), peerID=buddy.peerID.short,
+      serial=tBeg.serial, frameID=tBeg.frameID.idStr, syncState=tBeg.syncState
+
+  let idleTime = await ctx.trace.backup.schedPeer(buddy)
+
+  if noisy:
+    var tEnd: TraceSchedPeerEnd
+    tEnd.init buddy
+    tEnd.frameID = tBeg.serial # refers back to `tBeg` capture
+    tEnd.idleTime = idleTime
+    buddy.traceWrite tEnd
+
+    trace "-Peer", peer=($buddy.peer), peerID=buddy.peerID.short,
+      serial=tEnd.serial, frameID=tEnd.frameID.idStr, syncState=tBeg.syncState,
+      idleTime=idleTime.toStr
+
+  return idleTime
+
+# ------------------------------------------------------------------------------
+# End
+# ------------------------------------------------------------------------------
