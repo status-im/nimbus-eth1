d6a3dbc0eb50248f011749b0833e38175d949540
bhartnett <benjamin@status.im>
2025-07-31 19:46:32 +0800

    Fix state root mismatch.

--- execution_chain/db/aristo/aristo_tx_frame.nim
+++ execution_chain/db/aristo/aristo_tx_frame.nim
@@ -37,6 +37,15 @@ proc buildSnapshot(txFrame: AristoTxRef, minLevel: int) =
         # `frame` has a snapshot only in the first iteration of the for loop
         txFrame.snapshot = move(frame.snapshot)
 
+        # Copy cached values that are not present in the newer txFrame.
+        # These are needed to update the main caches in the CoreDbRef instance.
+        for k, v in frame.accLeaves:
+          if k notin txFrame.accLeaves:
+            txFrame.accLeaves[k] = v
+        for k, v in frame.stoLeaves:
+          if k notin txFrame.stoLeaves:
+            txFrame.stoLeaves[k] = v
+
         # Verify that https://github.com/nim-lang/Nim/issues/23759 is not present
         assert frame.snapshot.vtx.len == 0 and frame.snapshot.level.isNone()
 
