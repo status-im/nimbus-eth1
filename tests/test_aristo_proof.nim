acf848f2b38a06cd66325cfaf7826e070480d4ee
bhartnett <benjamin@status.im>
2025-08-19 14:00:30 +0800

    Implement aristo verifyProof function which supports passing in a table of nodes.

--- tests/test_aristo_proof.nim
+++ tests/test_aristo_proof.nim
@@ -10,131 +10,176 @@
 {.push raises: [].}
 
 import
-  std/sequtils,
+  std/[tables, sequtils],
   unittest2,
   stint,
   results,
   stew/byteutils,
+  eth/common/hashes,
   eth/trie/[hexary, db, trie_defs],
   ../execution_chain/db/aristo/aristo_proof
 
-proc getKeyBytes(i: int): seq[byte] =
+template getBytes(i: int): seq[byte] =
   @(u256(i).toBytesBE())
 
+func branchToNodesTable(branch: openArray[seq[byte]]): Table[Hash32, seq[byte]] =
+  var nodes: Table[Hash32, seq[byte]]
+  for n in branch:
+    nodes[keccak256(n)] = n
+  nodes
+
 suite "Aristo proof verification":
+  const numValues = 1000
 
-  test "Validate proof for existing value":
-    let numValues = 1000
-
-    var db = newMemoryDB()
+  setup:
+    let db = newMemoryDB()
     var trie = initHexaryTrie(db)
 
+  test "Validate proof for existing value":
     for i in 1..numValues:
       let
-        key = getKeyBytes(i).keccak256()
-        value = getKeyBytes(i)
+        indexBytes = getBytes(i)
+        key = keccak256(indexBytes)
+        value = indexBytes
       trie.put(key.data, value)
 
     for i in 1..numValues:
       let
-        key = getKeyBytes(i).keccak256()
-        value = getKeyBytes(i)
-        proof = trie.getBranch(key.data)
+        indexBytes = getBytes(i)
+        key = keccak256(indexBytes)
+        value = indexBytes
         root = trie.rootHash()
+        proof = trie.getBranch(key.data)
 
-      let proofRes = verifyProof(proof, root, key).expect("valid proof")
-      check:
-        proofRes.isSome()
-        proofRes.get() == value
+      block:
+        let leafValue = verifyProof(proof, root, key).expect("valid proof")
+        check:
+          leafValue.isSome()
+          leafValue.get() == value
+
+      block:
+        let leafValue = verifyProof(branchToNodesTable(proof), root, key).expect("valid proof")
+        check:
+          leafValue.isSome()
+          leafValue.get() == value
 
   test "Validate proof for non-existing value":
-    let numValues = 1000
-    var db = newMemoryDB()
-    var trie = initHexaryTrie(db)
-
     for i in 1..numValues:
       let
-        key = getKeyBytes(i).keccak256()
-        value = getKeyBytes(i)
+        indexBytes = getBytes(i)
+        key = keccak256(indexBytes)
+        value = indexBytes
       trie.put(key.data, value)
 
     let
       nonExistingKey = toSeq(toBytesBE(u256(numValues + 1))).keccak256()
-      proof = trie.getBranch(nonExistingKey.data)
       root = trie.rootHash()
+      proof = trie.getBranch(nonExistingKey.data)
 
-    let proofRes = verifyProof(proof, root, nonExistingKey).expect("valid proof")
-    check:
-      proofRes.isNone()
+    block:
+      let leafValue = verifyProof(proof, root, nonExistingKey).expect("valid proof")
+      check:
+        leafValue.isNone()
+
+    block:
+      let leafValue = verifyProof(branchToNodesTable(proof), root, nonExistingKey).expect("valid proof")
+      check:
+        leafValue.isNone()
 
   # The following test cases were copied from the Rust hexary trie implementation.
   # See here: https://github.com/citahub/cita_trie/blob/master/src/tests/mod.rs#L554
   test "Validate proof for empty trie":
-    let db = newMemoryDB()
-    var trie = initHexaryTrie(db)
-
     let
-      proof = trie.getBranch("not-exist".toBytes.keccak256().data)
-      res = verifyProof(proof, trie.rootHash, "not-exist".toBytes.keccak256())
-
+      key = keccak256("not-exist".toBytes)
+      root = trie.rootHash()
+      proof = trie.getBranch(key.data)
     check:
-      trie.rootHash == keccak256(emptyRlp)
-      proof.len() == 1 # Note that the Rust implementation returns an empty list for this scenario
+      root == keccak256(emptyRlp)
       proof == @[emptyRlp]
-      res.isErr()
+
+    block:
+      let verifyRes = verifyProof(proof, root, key)
+      check verifyRes.isErr()
+
+    block:
+      let verifyRes = verifyProof(branchToNodesTable(proof), root, key)
+      check verifyRes.isErr()
 
   test "Validate proof for one element trie":
-    let db = newMemoryDB()
-    var trie = initHexaryTrie(db)
-
-    trie.put("k".toBytes.keccak256().data, "v".toBytes)
+    let
+      key = keccak256("k".toBytes)
+      value = "v".toBytes
+    trie.put(key.data, value)
 
     let
-      rootHash = trie.rootHash
-      proof = trie.getBranch("k".toBytes.keccak256().data)
-      res = verifyProof(proof, rootHash, "k".toBytes.keccak256()).expect("valid proof")
+      root = trie.rootHash
+      proof = trie.getBranch(key.data)
+    check proof.len() == 1
 
-    check:
-      proof.len() == 1
-      res.isSome()
-      res.get() == "v".toBytes
+    block:
+      let leafValue = verifyProof(proof, root, key).expect("valid proof")
+      check:
+        leafValue.isSome()
+        leafValue.get() == value
+
+    block:
+      let leafValue = verifyProof(branchToNodesTable(proof), root, key).expect("valid proof")
+      check:
+        leafValue.isSome()
+        leafValue.get() == value
 
   test "Validate proof bytes":
-    let db = newMemoryDB()
-    var trie = initHexaryTrie(db)
+    let
+      key1 = keccak256("doe".toBytes)
+      key2 = keccak256("dog".toBytes)
+      key3 = keccak256("dogglesworth".toBytes)
+      value1 = "reindeer".toBytes
+      value2 = "puppy".toBytes
+      value3 = "cat".toBytes
 
-    trie.put("doe".toBytes.keccak256().data, "reindeer".toBytes)
-    trie.put("dog".toBytes.keccak256().data, "puppy".toBytes)
-    trie.put("dogglesworth".toBytes.keccak256().data, "cat".toBytes)
+    trie.put(key1.data, value1)
+    trie.put(key2.data, value2)
+    trie.put(key3.data, value3)
+
+    let
+      root = trie.rootHash
 
     block:
       let
-        rootHash = trie.rootHash
-        proof = trie.getBranch("doe".toBytes.keccak256().data)
-        res = verifyProof(proof, rootHash, "doe".toBytes.keccak256()).expect("valid proof")
-
+        proof = trie.getBranch(key1.data)
+        leafValue1 = verifyProof(proof, root, key1).expect("valid proof")
+        leafValue2 = verifyProof(branchToNodesTable(proof), root, key1).expect("valid proof")
       check:
-        res.isSome()
-        res.get() == "reindeer".toBytes
+        leafValue1.isSome()
+        leafValue1.get() == value1
+        leafValue2.isSome()
+        leafValue2.get() == value1
 
     block:
       let
-        rootHash = trie.rootHash
-        proof = trie.getBranch("dogg".toBytes.keccak256().data)
-        res = verifyProof(proof, rootHash, "dogg".toBytes.keccak256()).expect("valid proof")
-
-      check res.isNone()
+        key = keccak256("dogg".toBytes)
+        proof = trie.getBranch(key.data)
+        leafValue1 = verifyProof(proof, root, key).expect("valid proof")
+        leafValue2 = verifyProof(branchToNodesTable(proof), root, key).expect("valid proof")
+      check leafValue1.isNone()
+      check leafValue2.isNone()
 
     block:
       let
         proof = newSeq[seq[byte]]()
-        res = verifyProof(proof, trie.rootHash, "doe".toBytes.keccak256())
-
-      check res.isErr()
+        verifyRes1 = verifyProof(proof, root, key1)
+        verifyRes2 = verifyProof(branchToNodesTable(proof), root, key1)
+      check:
+        verifyRes1.isErr()
+        verifyRes2.isErr()
 
     block:
       let
         proof = @["aaa".toBytes, "ccc".toBytes]
-        res = verifyProof(proof, trie.rootHash, "doe".toBytes.keccak256())
-
-      check res.isErr()
+        verifyRes1 = verifyProof(proof, root, key1)
+        verifyRes2 = verifyProof(branchToNodesTable(proof), root, key1)
+      echo verifyRes1
+      echo verifyRes2
+      check:
+        verifyRes1.isErr()
+        verifyRes2.isErr()
