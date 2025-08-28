0f6abbaee599ecfbc66e396c9e12f2b85ee27566
Jordan Hrycaj <jordan@status.im>
2025-08-28 17:12:42 +0100

    Provide command line capture replay tool
    details
      This tool wraps and runs the `nimbus_execution_client` with the
      sync scheduler partially superseded by a capturing state data
      replay facility.

--- tools/syncer/replay/README.md
+++ tools/syncer/replay/README.md
@@ -5,6 +5,36 @@ Inspection
 ----------
 
 Given a (probably gzipped) capture file **(capture)** as a result of
- tracing, its content can be visualised via
+tracing, its content can be visualised via
 
        ./build/syncer_test_client_inspect (capture)
+
+Replay
+------
+
+Copy and secure the current database directory **(database)** as **(dbcopy)**,
+say. Then start a capture run on the original data base as
+
+       ./build/syncer_test_client_replay \
+          --datadir=(database) ...  -- (capture)
+
+where **(capture)** will contain all the data for the replay. This file can
+bebome quite big (e.g. 30GiB for the last 120k blocks synchronised on
+*mainnet*) but can be gzipped after the capture run was stopped.
+
+Monitor the capture run so it can be stopped at an an appropriate state using
+metrics or logs. With the above command line argumants, only the next sync
+session is logged ranging from the activation message (when *Activating syncer*
+is logged) up intil the suspend message (when *Suspending syncer* is logged.)
+
+Now, the captured run can be replayed on the secured database copy
+**(dbcopy)** with the (probably gzipped) **(capture)** file via
+
+       ./build/syncer_test_client_replay \
+          --datadir=(dbcopy) ... -- (capture)
+
+where ihe additional arguments **...** of either command above need not be
+the same.
+
+Note that you need another copy of **(database)** if you need to re-exec the
+latter command line statement.
---
+++ tools/syncer/syncer_test_client_replay.nim
@@ -0,0 +1,141 @@
+# This file may not be copied, modified, or distributed except according to
+# those terms.
+
+import
+  std/[cmdline, os, strutils],
+  pkg/[chronicles, results],
+  ../../execution_chain/config,
+  ../../execution_chain/sync/beacon,
+  ./helpers/[nimbus_el_wrapper, sync_ticker],
+  ./replay/replay_setup
+
+type
+  ArgsDigest = tuple
+    elArgs: seq[string] # split command line: left to "--" marker
+    fileName: string    # capture file name
+    noStopQuit: bool    # capture modifier argument
+    fakeImport: bool    # ditto
+    syncTicker: bool    # ..
+
+let
+  cmdName = getAppFilename().extractFilename()
+
+# ------------------------------------------------------------------------------
+# Private helpers, command line parsing tools
+# ------------------------------------------------------------------------------
+
+proc argsCheck(q: seq[string]): seq[string] =
+  if q.len == 0 or
+     q[0] == "-h" or
+     q[0] == "--help":
+    echo "",
+      "Usage: ", cmdName,
+      " [<execution-layer-args>.. --] <capture-file> [<attributes>..]\n",
+      "       Capture file:\n",
+      "           Read from trace capture file <capture-file> and replay the\n",
+      "           syncer session.",
+      "           <capture-file> argument.\n",
+      "       Attributes:\n",
+      "           noStopQuit    Continue as normal after the captured replay\n",
+      "                         states are exhausted. Otherwise the program\n",
+      "                         will terminate.\n",
+      "           fakeImport    Will not import blocks while replaying\n.",
+      "           syncTicker    Log sync state regularly.\n"
+    quit(QuitFailure)
+  return q
+
+proc argsError(s: string) =
+  echo "*** ", cmdName, ": ", s, "\n"
+  discard argsCheck(@["-h"]) # usage & quit
+
+# -------------
+
+proc parseCmdLine(): ArgsDigest =
+  ## Parse command line:
+  ## ::
+  ##    [<el-args>.. --] <filename> [noStopQuit] ..
+  ##
+  var exArgs: seq[string]
+
+  # Split command line by "--" into `exArgs[]` and `elArgs[]`
+  let args = commandLineParams().argsCheck()
+  for n in 0 ..< args.len:
+    if args[n] == "--":
+      if 0 < n:
+        result.elArgs = args[0 .. n-1]
+      if n < args.len:
+        exArgs = args[n+1 .. ^1].argsCheck()
+      break
+
+  # Case: no <el-options> delimiter "--" given
+  if exArgs.len == 0 and result.elArgs.len == 0:
+    exArgs = args
+
+  result.fileName = exArgs[0]
+  for n in 1 ..< exArgs.len:
+    let w = exArgs[n].split('=',2)
+
+    block:
+      # noStopQuit
+      const token = "noStopQuit"
+      if toLowerAscii(w[0]) == toLowerAscii(token):
+        if 1 < w.len:
+          argsError("Sub-argument has no value: " & token)
+        result.noStopQuit = true
+        continue
+
+    block:
+      # fakeImport
+      const token = "fakeImport"
+      if toLowerAscii(w[0]) == toLowerAscii(token):
+        if 1 < w.len:
+          argsError("Sub-argument has no value: " & token)
+        result.fakeImport = true
+        continue
+
+    block:
+      # syncTicker
+      const token = "syncTicker"
+      if toLowerAscii(w[0]) == toLowerAscii(token):
+        if 1 < w.len:
+          argsError("Sub-argument has no value: " & token)
+        result.syncTicker = true
+        continue
+
+    argsError("Sub-argument unknown: " & exArgs[n])
+
+# ------------------------------------------------------------------------------
+# Private functions
+# ------------------------------------------------------------------------------
+
+proc beaconSyncConfig(args: ArgsDigest): BeaconSyncConfigHook =
+  return proc(desc: BeaconSyncRef) =
+    if args.syncTicker:
+      desc.ctx.pool.ticker = syncTicker()
+    desc.ctx.replaySetup(
+               fileName = args.fileName,
+               noStopQuit = args.noStopQuit,
+               fakeImport = args.fakeImport).isOkOr:
+      fatal "Cannot set up replay handlers", error
+      quit(QuitFailure)
+
+# ------------------------------------------------------------------------------
+# Main
+# ------------------------------------------------------------------------------
+
+# Pre-parse command line
+let argsDigest = parseCmdLine()
+
+# Early plausibility check
+if not argsDigest.fileName.fileExists:
+  argsError("No such capture file: \"" & argsDigest.fileName & "\"")
+
+# Processing left part command line arguments
+let conf = makeConfig(cmdLine = argsDigest.elArgs)
+
+# Run execution client
+conf.runNimbusExeClient(argsDigest.beaconSyncConfig)
+
+# ------------------------------------------------------------------------------
+# End
+# ------------------------------------------------------------------------------
