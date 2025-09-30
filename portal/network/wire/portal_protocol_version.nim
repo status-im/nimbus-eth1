4c201015520aa1efcfe50c5acd3fb2e9a3461b67
jangko <jangko128@gmail.com>
2025-09-30 14:04:36 +0700

    bump nim-eth

--- portal/network/wire/portal_protocol_version.nim
+++ portal/network/wire/portal_protocol_version.nim
@@ -7,8 +7,7 @@
 
 {.push raises: [].}
 
-import
-  eth/rlp, eth/p2p/discoveryv5/node, eth/common/base_rlp, ../../common/common_types
+import eth/rlp, eth/p2p/discoveryv5/node, eth/common/base_rlp, ../../common/common_types
 
 export base_rlp
 
