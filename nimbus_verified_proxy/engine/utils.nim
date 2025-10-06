32db1944cc90c7562d891893611887a28be99046
chirag-parmar <chiragparmar12209@gmail.com>
2025-10-06 10:32:48 +0530

    add comments

--- nimbus_verified_proxy/engine/utils.nim
+++ nimbus_verified_proxy/engine/utils.nim
@@ -10,13 +10,13 @@
 import results, stint
 
 func chainIdToNetworkId*(chainId: UInt256): Result[UInt256, string] =
-  if chainId == 1.u256:
+  if chainId == 1.u256: # mainnet
     ok(1.u256)
-  elif chainId == 11155111.u256:
+  elif chainId == 11155111.u256: # sepolia
     ok(11155111.u256)
-  elif chainId == 17000.u256:
+  elif chainId == 17000.u256: # holesky
     ok(17000.u256)
-  elif chainId == 560048.u256:
+  elif chainId == 560048.u256: # hoodi
     ok(560048.u256)
   else:
     err("Unknown chainId")
