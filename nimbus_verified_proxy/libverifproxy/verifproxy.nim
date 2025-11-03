68847053f4f76f6a8f63a3cfcd3c0d9c92e5a5d6
chirag-parmar <chiragparmar12209@gmail.com>
2025-11-03 19:04:32 +0530

    init

--- nimbus_verified_proxy/libverifproxy/verifproxy.nim
+++ nimbus_verified_proxy/libverifproxy/verifproxy.nim
@@ -6,14 +6,38 @@
 # at your option. This file may not be copied, modified, or distributed except according to those terms.
 
 import
-  std/[atomics, json, net],
+  algorithm,
+  json_serialization,
+  chronos,
+  eth/net/nat,
+  std/[atomics, locks, json, net],
   beacon_chain/spec/[digest, network],
   beacon_chain/nimbus_binary_common,
+  ../engine/types,
+  ../engine/engine,
+  ../lc/lc,
+  ../lc_backend,
   ../nimbus_verified_proxy,
-  ../nimbus_verified_proxy_conf
+  ../nimbus_verified_proxy_conf,
+  ../json_rpc_backend
 
-proc quit*() {.exportc, dynlib.} =
-  echo "Quitting"
+{.pragma: exported, cdecl, exportc, dynlib, raises: [].}
+{.pragma: exportedConst, exportc, dynlib.}
+
+type
+  Task = ref object
+    status: int
+    response: string
+    finished: bool
+    cb: CallBackProc
+
+  Context = object
+    tasks: seq[Task]
+    stop: bool
+    frontend: EthApiFrontend
+
+  CallBackProc =
+    proc(ctx: ptr Context, status: int, res: cstring) {.cdecl, gcsafe, raises: [].}
 
 proc NimMain() {.importc, exportc, dynlib.}
 
@@ -29,56 +53,237 @@ proc initLib() =
     locals = addr(locals)
     nimGC_setStackBottom(locals)
 
-proc runContext(ctx: ptr Context) {.thread.} =
-  const defaultListenAddress = (static parseIpAddress("0.0.0.0"))
-  let str = $ctx.configJson
-  try:
-    let jsonNode = parseJson(str)
+proc toUnmanagedPtr[T](x: ref T): ptr T =
+  GC_ref(x)
+  addr x[]
 
-    let myConfig = VerifiedProxyConf(
-      eth2Network: some(jsonNode["eth2Network"].getStr()),
-      trustedBlockRoot: Eth2Digest.fromHex(jsonNode["trustedBlockRoot"].getStr()),
-      backendUrl: parseCmdArg(Web3Url, jsonNode["backendUrl"].getStr()),
-      frontendUrl: parseCmdArg(Web3Url, jsonNode["frontendUrl"].getStr()),
-      beaconApiUrls: parseCmdArg(UrlList, jsonNode["beaconApiUrls"].getStr()),
-      logLevel: jsonNode["logLevel"].getStr(),
-      logStdout: StdoutLogKind.Auto,
-      dataDirFlag: none(OutDir),
+func asRef[T](x: ptr T): ref T =
+  cast[ref T](x)
+
+proc destroy[T](x: ptr T) =
+  x[].reset()
+  GC_unref(asRef(x))
+
+proc createAsyncTaskContext(): ptr Context {.exported.} =
+  let ctx = Context.new()
+  ctx.toUnmanagedPtr()
+
+proc createTask(cb: CallBackProc): Task =
+  let task = Task()
+  task.finished = false
+  task.cb = cb
+  task
+
+proc freeResponse(res: cstring) {.exported.} =
+  deallocShared(res)
+
+proc freeContext(ctx: ptr Context) {.exported.} =
+  ctx.destroy()
+
+proc alloc(str: string): cstring =
+  var ret = cast[cstring](allocShared(str.len + 1))
+  let s = cast[seq[char]](str)
+  for i in 0 ..< str.len:
+    ret[i] = s[i]
+  ret[str.len] = '\0'
+  return ret
+
+proc eth_blockNumber(ctx: ptr Context, cb: CallBackProc) {.exported.} =
+  let task = createTask(cb)
+
+  ctx.tasks.add(task)
+
+  let fut = ctx.frontend.eth_blockNumber()
+
+  fut.addCallback proc(_: pointer) {.gcsafe.} =
+    if fut.cancelled():
+      task.response = Json.encode(fut.error())
+      task.finished = true
+      task.status = -2
+    elif fut.failed():
+      task.response = Json.encode(fut.error())
+      task.finished = true
+      task.status = -1
+    else:
+      task.response = Json.encode(fut.value())
+      task.status = 0
+      task.finished = true
+
+proc pollAsyncTaskEngine(ctx: ptr Context) {.exported.} =
+  var delList: seq[int] = @[]
+
+  let taskLen = ctx.tasks.len
+  for idx in 0 ..< taskLen:
+    let task = ctx.tasks[idx]
+    if task.finished:
+      task.cb(ctx, task.status, alloc(task.response))
+      delList.add(idx)
+
+  # sequence changes as we delete so delting in descending order
+  for i in delList.sorted(SortOrder.Descending):
+    ctx.tasks.delete(i)
+
+  if ctx.tasks.len > 0:
+    poll()
+
+proc load(
+    T: type VerifiedProxyConf, configJson: string
+): T {.raises: [CatchableError, ValueError].} =
+  let jsonNode = parseJson($configJson)
+
+  let
+    eth2Network = some(jsonNode.getOrDefault("eth2Network").getStr("mainnet"))
+    trustedBlockRoot =
+      if jsonNode.contains("trustedBlockRoot"):
+        Eth2Digest.fromHex(jsonNode["trustedBlockRoot"].getStr())
+      else:
+        raise
+          newException(ValueError, "`trustedBlockRoot` not specified in JSON config")
+    backendUrl =
+      if jsonNode.contains("backendUrl"):
+        parseCmdArg(Web3Url, jsonNode["backendUrl"].getStr())
+      else:
+        raise newException(ValueError, "`backendUrl` not specified in JSON config")
+    beaconApiUrls =
+      if jsonNode.contains("beaconApiUrls"):
+        parseCmdArg(UrlList, jsonNode["beaconApiUrls"].getStr())
+      else:
+        raise newException(ValueError, "`beaconApiUrls` not specified in JSON config")
+    logLevel = jsonNode.getOrDefault("logLevel").getStr("INFO")
+    logStdout =
+      case jsonNode.getOrDefault("logStdout").getStr("None")
+      of "Colors": StdoutLogKind.Colors
+      of "NoColors": StdoutLogKind.NoColors
+      of "Json": StdoutLogKind.Json
+      of "Auto": StdoutLogKind.Auto
+      else: StdoutLogKind.None
+    maxBlockWalk = jsonNode.getOrDefault("maxBlockWalk").getInt(1000)
+    headerStoreLen = jsonNode.getOrDefault("headerStoreLen").getInt(256)
+    storageCacheLen = jsonNode.getOrDefault("storageCacheLen").getInt(256)
+    codeCacheLen = jsonNode.getOrDefault("codeCacheLen").getInt(64)
+    accountCacheLen = jsonNode.getOrDefault("accountCacheLen").getInt(128)
+
+  return VerifiedProxyConf(
+    eth2Network: eth2Network,
+    trustedBlockRoot: trustedBlockRoot,
+    backendUrl: backendUrl,
+    beaconApiUrls: beaconApiUrls,
+    logLevel: logLevel,
+    logStdout: logStdout,
+    dataDirFlag: none(OutDir),
+    maxBlockWalk: uint64(maxBlockWalk),
+    headerStoreLen: headerStoreLen,
+    storageCacheLen: storageCacheLen,
+    codeCacheLen: codeCacheLen,
+    accountCacheLen: accountCacheLen,
+  )
+
+proc run(
+    ctx: ptr Context, configJson: string
+) {.async: (raises: [ValueError, CancelledError, CatchableError]).} =
+  try:
+    initLib()
+  except Exception as err:
+    raise newException(CancelledError, err.msg)
+
+  let config = VerifiedProxyConf.load($configJson)
+
+  setupLogging(config.logLevel, config.logStdout)
+
+  let
+    engineConf = RpcVerificationEngineConf(
+      chainId: getConfiguredChainId(config.eth2Network),
+      maxBlockWalk: config.maxBlockWalk,
+      headerStoreLen: config.headerStoreLen,
+      accountCacheLen: config.accountCacheLen,
+      codeCacheLen: config.codeCacheLen,
+      storageCacheLen: config.storageCacheLen,
     )
+    engine = RpcVerificationEngine.init(engineConf)
+    lc = LightClient.new(config.eth2Network, some config.trustedBlockRoot)
 
-    run(myConfig, ctx)
-  except Exception as err:
-    echo "Exception when running ", getCurrentExceptionMsg(), err.getStackTrace()
-    ctx.onHeader(getCurrentExceptionMsg(), 3)
-    ctx.cleanup()
+    # initialize backend for JSON-RPC
+    jsonRpcClient = JsonRpcClient.init(config.backendUrl)
 
-  #[let node = parseConfigAndRun(ctx.configJson)
+    # initialize backend for light client updates
+    lcRestClientPool = LCRestClientPool.new(lc.cfg, lc.forkDigests)
 
-  while not ctx[].stop: # and node.running:
-    let timeout = sleepAsync(100.millis)
-    waitFor timeout
+  # connect light client to LC by registering on header methods 
+  # to use engine header store
+  connectLCToEngine(lc, engine)
 
-  # do cleanup
-  node.stop()]#
+  # add light client backend
+  lc.setBackend(lcRestClientPool.getEthLCBackend())
 
-proc startVerifProxy*(
-    configJson: cstring, onHeader: OnHeaderCallback
-): ptr Context {.exportc, dynlib.} =
-  initLib()
+  # the backend only needs the url to connect to
+  engine.backend = jsonRpcClient.getEthApiBackend()
 
-  let ctx = createShared(Context, 1)
-  ctx.configJson = cast[cstring](allocShared0(len(configJson) + 1))
-  ctx.onHeader = onHeader
-  copyMem(ctx.configJson, configJson, len(configJson))
+  # inject the frontend into c context
+  ctx.frontend = engine.frontend
 
-  try:
-    createThread(ctx.thread, runContext, ctx)
-  except Exception as err:
-    echo "Exception when attempting to invoke createThread ",
-      getCurrentExceptionMsg(), err.getStackTrace()
-    ctx.onHeader(getCurrentExceptionMsg(), 3)
-    ctx.cleanup()
-  return ctx
+  # start backend
+  var status = await jsonRpcClient.start()
+  if status.isErr():
+    raise newException(ValueError, status.error)
 
-proc stopVerifProxy*(ctx: ptr Context) {.exportc, dynlib.} =
+  # adding endpoints will also start the backend
+  lcRestClientPool.addEndpoints(config.beaconApiUrls)
+
+  # this starts the light client manager which is
+  # an endless loop
+  await lc.start()
+
+# TODO: if frontend is accessed if this fails then it throws a sefault
+# TODO: there is log leakage(at WARN level) even when logging is set to FATAL and stdout is set to None
+proc startVerifProxy(
+    ctx: ptr Context, configJson: cstring, cb: CallBackProc
+) {.exported.} =
+  let task = createTask(cb)
+
+  ctx.tasks.add(task)
+
+  let fut = run(ctx, $configJson)
+
+  fut.addCallback proc(udata: pointer) {.gcsafe.} =
+    if fut.cancelled():
+      task.response = Json.encode(fut.error())
+      task.finished = true
+      task.status = -2
+    elif fut.failed():
+      task.response = Json.encode(fut.error())
+      task.finished = true
+      task.status = -1
+    else:
+      task.response = "success"
+      task.status = 0
+      task.finished = true
+
+proc stopVerifProxy(ctx: ptr Context) {.exported.} =
   ctx.stop = true
+
+# C-callable: downloads a page and returns a heap-allocated C string.
+proc nonBusySleep(ctx: ptr Context, secs: cint, cb: CallBackProc) {.exported.} =
+  let task = createTask(cb)
+
+  ctx.tasks.add(task)
+
+  let fut = sleepAsync((secs).seconds)
+
+  fut.addCallback proc(_: pointer) {.gcsafe.} =
+    if fut.cancelled:
+      task.response = "cancelled"
+      task.finished = true
+      task.status = -2
+    elif fut.failed():
+      task.response = "failed"
+      task.finished = true
+      task.status = -1
+    else:
+      try:
+        task.response = "slept"
+        task.status = 0
+      except CatchableError as e:
+        task.response = e.msg
+        task.status = -1
+      finally:
+        task.finished = true
