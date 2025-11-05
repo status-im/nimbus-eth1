6702c05b9eea60d823df133088720d346ed85bfb
Jordan Hrycaj <jordan@status.im>
2025-11-05 17:04:34 +0000

    Rollup of PR #3790, 67b7415..d2e3378

---
+++ tools/syncer/replay/replay_runner/runner_dispatch/dispatch_sched.nim
@@ -0,0 +1,212 @@
+# Nimbus
+# Copyright (c) 2025 Status Research & Development GmbH
+# Licensed and distributed under either of
+#   * MIT license (license terms in the root directory or at
+#     https://opensource.org/licenses/MIT).
+#   * Apache v2 license (license terms in the root directory or at
+#     https://www.apache.org/licenses/LICENSE-2.0).
+# at your option. This file may not be copied, modified, or distributed
+# except according to those terms.
+
+## Replay runner
+
+{.push raises:[].}
+
+import
+  std/tables,
+  pkg/[chronicles, chronos, eth/common],
+  ../runner_desc,
+  ./dispatch_helpers
+
+logScope:
+  topics = "replay runner"
+
+# ------------------------------------------------------------------------------
+# Private helper
+# ------------------------------------------------------------------------------
+
+proc schedDaemonProcessImpl(
+    daemon: ReplayDaemonRef;
+    instr: ReplaySchedDaemonBegin;
+    info: static[string];
+      ) {.async: (raises: []).} =
+  ## Run the task `schedDaemon()`. This function has to be run background
+  ## process (using `asyncSpawn`.)
+  ##
+  let run = daemon.run
+  trace info & ": begin", n=run.iNum, serial=instr.bag.serial,
+    frameID=instr.frameIdStr, syncState=instr.bag.syncState
+
+  discard await run.backup.schedDaemon(run.ctx)
+  daemon.processFinishedClearFrame(instr, info)
+
+  trace info & ": end", n=run.iNum, serial=instr.bag.serial,
+    frameID=instr.frameIdStr, syncState=instr.bag.syncState
+
+
+proc schedPeerProcessImpl(
+    buddy: ReplayBuddyRef;
+    instr: ReplaySchedPeerBegin;
+    info: static[string];
+      ) {.async: (raises: []).} =
+  ## Run the task `schedPeer()`. This function has to be run background
+  ## process (using `asyncSpawn`.)
+  ##
+  let run = buddy.run
+  trace info & ": begin", n=run.iNum, serial=instr.bag.serial,
+    frameID=instr.frameIdStr, peer=($buddy.peer), peerID=buddy.peerID.short,
+    syncState=instr.bag.syncState
+
+  # Activate peer
+  buddy.run.nPeers.inc
+
+  discard await run.backup.schedPeer(buddy, instr.bag.rank)
+  buddy.processFinishedClearFrame(instr, info)
+
+  trace info & ": end", n=run.iNum, serial=instr.bag.serial,
+    frameID=instr.frameIdStr, peer=($buddy.peer), peerID=buddy.peerID.short,
+    syncState=instr.bag.syncState
+
+# ------------------------------------------------------------------------------
+# Public dispatcher handlers
+# ------------------------------------------------------------------------------
+
+proc schedDaemonBegin*(
+    run: ReplayRunnerRef;
+    instr: ReplaySchedDaemonBegin;
+      ) {.async: (raises: []).} =
+  ## Run the `schedDaemon()` task.
+  ##
+  # Synchronise against captured environment and start process
+  const info = instr.replayLabel()
+  let daemon = run.newDaemonFrame(instr, info).valueOr: return
+  discard await daemon.waitForSyncedEnv(instr, info)
+  asyncSpawn daemon.schedDaemonProcessImpl(instr, info)
+
+
+proc schedDaemonEnd*(
+    run: ReplayRunnerRef;
+    instr: ReplaySchedDaemonEnd;
+      ) {.async: (raises: []).} =
+  ## Clean up (in foreground) after `schedDaemon()` process has terminated.
+  ##
+  const info = instr.replayLabel()
+  let daemon = run.getDaemon(info).valueOr: return
+  daemon.whenProcessFinished(instr, info).isErrOr:
+    daemon.delDaemon(info) # Clean up
+
+
+proc schedStartWorker*(
+    run: ReplayRunnerRef;
+    instr: ReplaySchedStart;
+      ) =
+  ## Runs `schedStart()` in the foreground.
+  ##
+  const
+    info = instr.replayLabel()
+  let
+    buddy = run.newPeer(instr, info).valueOr: return
+    accept = run.backup.schedStart(buddy)
+
+  trace info & ": begin", n=run.iNum, serial=instr.bag.serial,
+    peer=($buddy.peer), peerID=buddy.peerID.short
+
+  if accept != instr.bag.accept:
+    warn info & ": result argument differs", n=run.iNum,
+      serial=instr.bag.serial, peer=buddy.peer, expected=instr.bag.accept,
+      result=accept
+
+  # Syncer state was captured when leaving the `schedStart()` handler.
+  buddy.checkSyncerState(instr, ignLatestNum=true, info) # relaxed check
+
+  if not accept:
+    buddy.delPeer(info) # Clean up
+
+  trace info & ": end", n=run.iNum, serial=instr.bag.serial,
+    peer=($buddy.peer), peerID=buddy.peerID.short
+
+
+proc schedStopWorker*(
+    run: ReplayRunnerRef;
+    instr: ReplaySchedStop;
+      ) =
+  ## Runs `schedStop()` in the foreground.
+  ##
+  const info = instr.replayLabel()
+  let buddy = run.getOrNewPeerFrame(instr, info).valueOr: return
+  run.backup.schedStop(buddy)
+
+  # As the `schedStop()` function environment was captured only after the
+  # syncer was activated, there might still be some unregistered peers hanging
+  # around. So it is perfectly OK to see the peer for the first time, here
+  # which has its desciptor sort of unintialised (relative to `instr`.)
+  if not buddy.isNew:
+    # Syncer state was captured when leaving the `schedStop()` handler.
+    if instr.bag.peerCtx.isNone():
+      warn info & ": peer ctx missing", n=run.iNum, serial=instr.bag.serial
+      return
+    if instr.bag.peerCtx.value.peerCtrl == Stopped and not buddy.ctrl.stopped:
+      buddy.ctrl.stopped = true
+    buddy.checkSyncerState(instr, info)
+
+  # Clean up
+  buddy.delPeer(info)
+
+  trace info & ": done", n=run.iNum, serial=instr.bag.serial,
+    peer=($buddy.peer), peerID=buddy.peerID.short
+
+
+proc schedPoolWorker*(
+    run: ReplayRunnerRef;
+    instr: ReplaySchedPool;
+      ) =
+  ## Runs `schedPool()` in the foreground.
+  ##
+  const info = instr.replayLabel()
+  let buddy = run.getOrNewPeerFrame(instr, info).valueOr: return
+
+  if 0 < run.nPeers:
+    warn info & ": no active peers allowed", n=run.iNum,
+      serial=instr.bag.serial, peer=buddy.peer, nPeers=run.nPeers, expected=0
+
+  # The scheduler will reset the `poolMode` flag before starting the
+  # `schedPool()` function.
+  run.ctx.poolMode = false
+
+  discard run.backup.schedPool(buddy, instr.bag.last, instr.bag.laps.int)
+
+  # Syncer state was captured when leaving the `schedPool()` handler.
+  buddy.checkSyncerState(instr, info)
+  buddy.processFinishedClearFrame(instr, info)
+
+  info info & ": done", n=run.iNum, serial=instr.bag.serial,
+    peer=($buddy.peer), peerID=buddy.peerID.short
+
+
+proc schedPeerBegin*(
+    run: ReplayRunnerRef;
+    instr: ReplaySchedPeerBegin;
+      ) {.async: (raises: []).} =
+  ## Run the `schedPeer()` task.
+  ##
+  # Synchronise against captured environment and start process
+  const info = instr.replayLabel()
+  let buddy = run.getOrNewPeerFrame(instr, info).valueOr: return
+  discard await buddy.waitForSyncedEnv(instr, info)
+  asyncSpawn buddy.schedPeerProcessImpl(instr, info)
+
+
+proc schedPeerEnd*(
+    run: ReplayRunnerRef;
+    instr: ReplaySchedPeerEnd;
+      ) {.async: (raises: []).} =
+  ## Clean up (in foreground) after `schedPeer()` process has terminated.
+  ##
+  const info = instr.replayLabel()
+  let buddy = run.getPeer(instr, info).valueOr: return
+  buddy.whenProcessFinished(instr, info).isErrOr:
+    buddy.run.nPeers.dec # peer is not active, anymore
+
+# ------------------------------------------------------------------------------
+# End
+# ------------------------------------------------------------------------------
---
+++ tools/syncer/syncer_test_client_trace.nim
@@ -0,0 +1,112 @@
+# Nimbus
+# Copyright (c) 2025 Status Research & Development GmbH
+# Licensed under either of
+#  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE))
+#  * MIT license ([LICENSE-MIT](LICENSE-MIT))
+# at your option.
+# This file may not be copied, modified, or distributed except according to
+# those terms.
+
+import
+  std/[cmdline, os, strutils, terminal],
+  pkg/[chronicles, confutils, results],
+  ../../execution_chain/[conf, nimbus_desc, nimbus_execution_client],
+  ../../execution_chain/sync/beacon,
+  ./helpers/sync_ticker,
+  ./trace/trace_setup
+
+const
+  fgSection = fgYellow
+  fgOption = fgBlue
+
+type
+  ToolConfig* = object of RootObj
+    captureFile {.
+      separator: "TRACE TOOL OPTIONS:"
+      desc: "Store captured states in the <capture-file> argument. If this " &
+            "option is missing, no capture file is written"
+      name: "capture-file" .}: Option[OutFile]
+
+    nSessions {.
+      desc: "Run a trace for this many sessions (i.e. from activation to " &
+            "suspension)"
+      defaultValue: 1
+      name: "num-trace-sessions" .}: uint
+
+    nPeersMin {.
+      desc: "Minimal number of peers needed for activating the first syncer " &
+             "session"
+      defaultValue: 0
+      name: "num-peers-min" .}: uint
+
+    noSyncTicker {.
+      desc: "Disable logging sync status regularly"
+      defaultValue: false
+      name: "disable-sync-ticker" .}: bool
+
+  SplitCmdLine = tuple
+    leftArgs: seq[string]  # split command line: left to "--" marker (nimbus)
+    rightArgs: seq[string] # split command line: right to "--" marker (tool)
+
+# ------------------------------------------------------------------------------
+# Private functions
+# ------------------------------------------------------------------------------
+
+proc splitCmdLine(): SplitCmdLine =
+  ## Split commans line options
+  ## ::
+  ##   [<nimbus-options> --] [<tool-options]
+  ##
+  let args = commandLineParams()
+  for n in 0 ..< args.len:
+    if args[n] == "--":
+      if 0 < n:
+        result.leftArgs = args[0 .. n-1]
+      if n < args.len:
+        result.rightArgs = args[n+1 .. ^1]
+      return
+  result.rightArgs = args
+
+
+proc beaconSyncConfig(conf: ToolConfig): BeaconSyncConfigHook =
+  return proc(desc: BeaconSyncRef) =
+    if not conf.noSyncTicker:
+      desc.ctx.pool.ticker = syncTicker()
+    if 1 < conf.nPeersMin:
+      desc.ctx.pool.minInitBuddies = conf.nPeersMin.int
+    if conf.nSessions == 0 or
+       conf.captureFile.isNone:
+      return
+    desc.ctx.traceSetup(
+               fileName = conf.captureFile.unsafeGet.string,
+               nSessions = conf.nSessions.int).isOkOr:
+      fatal "Cannot set up trace handlers", error
+      quit(QuitFailure)
+
+# ------------------------------------------------------------------------------
+# Main
+# ------------------------------------------------------------------------------
+
+let
+  (leftOpts, rightOpts) = splitCmdLine()
+
+  rightConf = ToolConfig.load(
+    cmdLine = rightOpts,
+    copyrightBanner = ansiForegroundColorCode(fgSection) &
+      "\pNimbus execution layer with trace extension.\p" &
+      "Extended command line options:\p" &
+      ansiForegroundColorCode(fgOption) &
+      "  [<nimbus-options> --] [<tool-options>]")
+
+  leftConf = makeConfig(cmdLine = leftOpts)
+
+  # Update node config for lazy beacon sync update
+  nodeConf = NimbusNode(
+    beaconSyncRef: BeaconSyncRef.init rightConf.beaconSyncConfig)
+
+# Run execution client
+leftConf.main(nodeConf)
+
+# ------------------------------------------------------------------------------
+# End
+# ------------------------------------------------------------------------------
