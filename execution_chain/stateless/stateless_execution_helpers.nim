67dacff07160e641e7fe2cf99a4f9a336d500cbe
bhartnett <benjamin@status.im>
2025-11-14 16:06:14 +0800

    Add json and jsonFile stateless execution helpers.

--- execution_chain/stateless/stateless_execution_helpers.nim
+++ execution_chain/stateless/stateless_execution_helpers.nim
@@ -9,47 +9,56 @@
 
 {.push raises: [], gcsafe.}
 
-import eth/common/blocks_rlp, ./stateless_execution
+import
+  eth/common/blocks_rlp,
+  web3/[eth_api_types, conversions],
+  stew/io2,
+  ./stateless_execution
+
+from ../../hive_integration/engine_client import toBlockHeader, toTransactions
 
 export stateless_execution
 
-proc statelessProcessBlock*(
-    witnessBytes: openArray[byte], com: CommonRef, blkBytes: openArray[byte]
+ExecutionWitness.useDefaultSerializationIn JrpcConv
+
+proc statelessProcessBlockRlp*(
+    witnessRlpBytes: openArray[byte], com: CommonRef, blkRlpBytes: openArray[byte]
 ): Result[void, string] =
   let
-    witness = ?ExecutionWitness.decode(witnessBytes)
+    witness = ?ExecutionWitness.decode(witnessRlpBytes)
     blk =
       try:
-        rlp.decode(blkBytes, Block)
+        rlp.decode(blkRlpBytes, Block)
       except RlpError as e:
         return err(e.msg)
   statelessProcessBlock(witness, com, blk)
 
-proc statelessProcessBlock*(
-    witnessBytes: openArray[byte],
-    id: NetworkId,
-    config: ChainConfig,
-    blkBytes: openArray[byte],
+func toBlock(blockObject: BlockObject): Block =
+  Block.init(
+    blockObject.toBlockHeader(),
+    BlockBody(
+      transactions: blockObject.transactions.toTransactions(),
+      withdrawals: blockObject.withdrawals,
+    ),
+  )
+
+proc statelessProcessBlockJson*(
+    witnessJson: string, com: CommonRef, blkJson: string
+): Result[void, string] =
+  try:
+    let
+      witness = JrpcConv.decode(witnessJson, ExecutionWitness)
+      blk = JrpcConv.decode(blkJson, BlockObject).toBlock()
+    statelessProcessBlock(witness, com, blk)
+  except SerializationError as e:
+    return err(e.msg)
+
+proc statelessProcessBlockJsonFiles*(
+    witnessJsonFilePath: string, com: CommonRef, blockJsonFilePath: string
 ): Result[void, string] =
   let
-    witness = ?ExecutionWitness.decode(witnessBytes)
-    blk =
-      try:
-        rlp.decode(blkBytes, Block)
-      except RlpError as e:
-        return err(e.msg)
-  statelessProcessBlock(witness, id, config, blk)
-
-proc statelessProcessBlock*(
-    witnessBytes: openArray[byte], id: NetworkId, blkBytes: openArray[byte]
-): Result[void, string] =
-  let
-    witness = ?ExecutionWitness.decode(witnessBytes)
-    blk =
-      try:
-        rlp.decode(blkBytes, Block)
-      except RlpError as e:
-        return err(e.msg)
-  statelessProcessBlock(witness, id, chainConfigForNetwork(id), blk)
-
-## create function that takes json types
+    witnessFileStr = io2.readAllChars(witnessJsonFilePath).valueOr:
+      return err("Reading witness json file failed")
+    blkFileStr = io2.readAllChars(blockJsonFilePath).valueOr:
+      return err("Reading block json file failed")
+  statelessProcessBlockJson(witnessFileStr, com, blkFileStr)
