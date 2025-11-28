2798547486070db880f19ec1010bef3e9b4cdf16
bhartnett <benjamin@status.im>
2025-11-28 10:56:56 +0800

    Fix new payload validation.

--- execution_chain/beacon/api_handler/api_newpayload.nim
+++ execution_chain/beacon/api_handler/api_newpayload.nim
@@ -44,10 +44,25 @@ template validateVersion(com, timestamp, payloadVersion, apiVersion) =
   if apiVersion == Version.V5:
     if not com.isAmsterdamOrLater(timestamp):
       raise unsupportedFork("newPayloadV5 expect payload timestamp fall within Amsterdam")
-
     if payloadVersion != Version.V4:
       raise invalidParams("newPayload" & $apiVersion &
       " expect ExecutionPayloadV4" &
+      " but got ExecutionPayload" & $payloadVersion)
+
+  elif apiVersion == Version.V4:
+    if not com.isPragueOrLater(timestamp):
+      raise unsupportedFork("newPayloadV4 expect payload timestamp fall within Prague")
+    if payloadVersion != Version.V3:
+      raise invalidParams("newPayload" & $apiVersion &
+      " expect ExecutionPayloadV3" &
+      " but got ExecutionPayload" & $payloadVersion)
+
+  elif apiVersion == Version.V3:
+    if not com.isCancunOrLater(timestamp):
+      raise unsupportedFork("newPayloadV3 expect payload timestamp fall within Cancun")
+    if payloadVersion != Version.V3:
+      raise invalidParams("newPayload" & $apiVersion &
+      " expect ExecutionPayloadV3" &
       " but got ExecutionPayload" & $payloadVersion)
 
   if com.isAmsterdamOrLater(timestamp):
@@ -55,20 +70,12 @@ template validateVersion(com, timestamp, payloadVersion, apiVersion) =
       raise invalidParams("if timestamp is Amsterdam or later, " &
         "payload must be ExecutionPayloadV4, got ExecutionPayload" & $payloadVersion)
 
-  if apiVersion == Version.V4:
-    if not com.isPragueOrLater(timestamp):
-      raise unsupportedFork("newPayloadV4 expect payload timestamp fall within Prague")
-
-  if com.isPragueOrLater(timestamp):
+  elif com.isPragueOrLater(timestamp):
     if payloadVersion != Version.V3:
       raise invalidParams("if timestamp is Prague or later, " &
         "payload must be ExecutionPayloadV3, got ExecutionPayload" & $payloadVersion)
 
-  if apiVersion == Version.V3:
-    if not com.isCancunOrLater(timestamp):
-      raise unsupportedFork("newPayloadV3 expect payload timestamp fall within Cancun")
-
-  if com.isCancunOrLater(timestamp):
+  elif com.isCancunOrLater(timestamp):
     if payloadVersion != Version.V3:
       raise invalidParams("if timestamp is Cancun or later, " &
         "payload must be ExecutionPayloadV3, got ExecutionPayload" & $payloadVersion)
@@ -81,13 +88,6 @@ template validateVersion(com, timestamp, payloadVersion, apiVersion) =
   elif payloadVersion != Version.V1:
     raise invalidParams("if timestamp is earlier than Shanghai, " &
       "payload must be ExecutionPayloadV1, got ExecutionPayload" & $payloadVersion)
-
-  if apiVersion == Version.V3 or apiVersion == Version.V4:
-    # both newPayloadV3 and newPayloadV4 expect ExecutionPayloadV3
-    if payloadVersion != Version.V3:
-      raise invalidParams("newPayload" & $apiVersion &
-      " expect ExecutionPayloadV3" &
-      " but got ExecutionPayload" & $payloadVersion)
 
 template validatePayload(apiVersion, payloadVersion, payload) =
   if payloadVersion >= Version.V2:
