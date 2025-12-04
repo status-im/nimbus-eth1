1071c5671e5c9cfa30adee24ff656264b51085a0
Lewis Revill <lewis1revill@gmail.com>
2025-12-04 09:51:16 +0000

    Reduce down stateless_execution_helpers to compile for baremetal

--- execution_chain/stateless/stateless_execution_helpers.nim
+++ execution_chain/stateless/stateless_execution_helpers.nim
@@ -9,49 +9,21 @@
 
 {.push raises: [], gcsafe.}
 
-import
-  eth/common/blocks_rlp,
-  web3/[eth_api_types, conversions],
-  stew/[io2, byteutils],
-  ./stateless_execution
-
-from ../../hive_integration/engine_client import toBlockHeader, toTransactions
+from ./witness_types import ExecutionWitness
+from eth/rlp import read, RlpError
+from eth/common/blocks import Block
+from stew/byteutils import hexToSeqByte
+from ./stateless_execution import statelessProcessBlock
+from stint import u256
+import results
 
 export stateless_execution
-
-ExecutionWitness.useDefaultSerializationIn JrpcConv
-
-proc readFileToStr*(filePath: string): Result[string, string] =
-  let fileStr = io2.readAllChars(filePath).valueOr:
-    return err("Error reading file: " & filePath)
-  ok(fileStr)
 
 func toBytes*(hexStr: string): Result[seq[byte], string] =
   try:
     ok(hexToSeqByte(hexStr))
   except ValueError as e:
     err("Error converting hex string to bytes: " & e.msg)
-
-func decodeJson*(T: type ExecutionWitness, jsonStr: string): Result[T, string] =
-  try:
-    ok(JrpcConv.decode(jsonStr, T))
-  except SerializationError as e:
-    err("Error decoding json string: " & e.msg)
-
-func decodeJson*(T: type BlockObject, jsonStr: string): Result[T, string] =
-  try:
-    ok(JrpcConv.decode(jsonStr, T))
-  except SerializationError as e:
-    err("Error decoding json string: " & e.msg)
-
-func toBlock*(blockObject: BlockObject): Block =
-  Block.init(
-    blockObject.toBlockHeader(),
-    BlockBody(
-      transactions: blockObject.transactions.toTransactions(),
-      withdrawals: blockObject.withdrawals,
-    ),
-  )
 
 func decodeRlp*(
     T: type ExecutionWitness, rlpBytes: openArray[byte]
@@ -68,33 +40,48 @@ func decodeRlp*(T: type Block, rlpBytes: openArray[byte]): Result[T, string] =
     err("Error decoding rlp bytes: " & e.msg)
 
 proc statelessProcessBlockRlp*(
-    witnessRlpBytes: openArray[byte], com: CommonRef, blkRlpBytes: openArray[byte]
+    witnessRlpBytes: openArray[byte], blkRlpBytes: openArray[byte]
 ): Result[void, string] =
   let
     witness = ?ExecutionWitness.decodeRlp(witnessRlpBytes)
     blk = ?Block.decodeRlp(blkRlpBytes)
-  statelessProcessBlock(witness, com, blk)
+  statelessProcessBlock(witness, 1.u256, blk)
 
 proc statelessProcessBlockRlp*(
-    witnessRlpStr: string, com: CommonRef, blkRlpStr: string
+    witnessRlpStr: string, blkRlpStr: string
 ): Result[void, string] =
   let
     witnessRlpBytes = ?witnessRlpStr.toBytes()
     blkRlpBytes = ?blkRlpStr.toBytes()
-  statelessProcessBlockRlp(witnessRlpBytes, com, blkRlpBytes)
+  statelessProcessBlockRlp(witnessRlpBytes, blkRlpBytes)
 
-proc statelessProcessBlockJson*(
-    witnessJson: string, com: CommonRef, blkJson: string
-): Result[void, string] =
-  let
-    witness = ?ExecutionWitness.decodeJson(witnessJson)
-    blkObject = ?BlockObject.decodeJson(blkJson)
-  statelessProcessBlock(witness, com, blkObject.toBlock())
+proc main() = 
+  echo "Input block as RLP"
+  var blockIn = ""
+  while true:
+    try:
+      let ch = stdin.readChar();
+      blockIn.add(ch)
+      if ch == '}':
+        break
+    except IOError:
+      echo "Failed"
+      break
 
-proc statelessProcessBlockJsonFiles*(
-    witnessJsonFilePath: string, com: CommonRef, blockJsonFilePath: string
-): Result[void, string] =
-  let
-    witnessJson = ?readFileToStr(witnessJsonFilePath)
-    blkJson = ?readFileToStr(blockJsonFilePath)
-  statelessProcessBlockJson(witnessJson, com, blkJson)
+  echo "Input witness as RLP"
+  var witnessIn = ""
+  while true:
+    try:
+      let ch = stdin.readChar();
+      witnessIn.add(ch)
+      if ch == '}':
+        break
+    except IOError:
+      echo "Failed"
+      break
+
+  statelessProcessBlockRlp(witnessIn, blockIn).isOkOr:
+    echo error
+
+when isMainModule:
+  main()
