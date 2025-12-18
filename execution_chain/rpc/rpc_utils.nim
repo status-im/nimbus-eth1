b85c69907c21d0277577cc6041dcef76859ab24f
kdeme <7857583+kdeme@users.noreply.github.com>
2025-12-18 16:52:24 +0100

    Adjust for optional totalDifficulty in JSON-RPC BlockObject

--- execution_chain/rpc/rpc_utils.nim
+++ execution_chain/rpc/rpc_utils.nim
@@ -128,7 +128,7 @@ proc populateTransactionObject*(tx: Transaction,
 
 proc populateBlockObject*(blockHash: Hash32,
                           blk: Block,
-                          totalDifficulty: UInt256,
+                          totalDifficulty: Opt[UInt256],
                           fullTx: bool,
                           withUncles: bool = false): BlockObject =
   template header: auto = blk.header
@@ -389,3 +389,10 @@ proc getEthConfigObject*(com: CommonRef,
     res.last = Opt.none(ConfigObject)
 
   return res
+
+proc getTotalDifficulty*(chain: ForkedChainRef, blockHash: Hash32): Opt[UInt256] =
+  let txFrame = chain.txFrame(blockHash).txFrameBegin()
+  defer:
+    txFrame.dispose()
+
+  txFrame.getScore(blockHash)
