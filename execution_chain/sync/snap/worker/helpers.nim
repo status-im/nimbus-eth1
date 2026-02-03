97d66e9661bdb38f9ba2755fd0d2dafc224aada7
Jordan Hrycaj <jordan@status.im>
2026-02-03 11:16:53 +0000

    Add `totalRatio()` function for unprocessed interval logging
    why
      Shows a factor of what is to be done,
      e.g. `0.0` for all done, `1.0` for nothing done.

--- execution_chain/sync/snap/worker/helpers.nim
+++ execution_chain/sync/snap/worker/helpers.nim
@@ -13,7 +13,7 @@
 ## Extracted helpers from `worker_desc` (avoids circular import)
 
 import
-  std/strformat,
+  std/[fenv, math, strformat],
   pkg/[chronos, stew/interval_set],
   ../../../core/chain,
   ../../../networking/p2p,
@@ -26,18 +26,65 @@ export
 
 
 func toStr*(h: Hash32): string =
-  if h == emptyRoot: "n/a"
-  elif h == zeroHash32: "n/a"
+  if h == emptyRoot: "empty"
+  elif h == zeroHash32: "zero"
   else: h.short
 
-func toStr*(w: float): string =
-  &"{w:.7g}" # => 1.234567e+x
+# --------------
 
-func toStr*(w: (float,float)): string =
-  if w[0] < w[1]: w[0].toStr & ".." & w[1].toStr
-  elif w[0] == w[1]: w[0].toStr
+func per256*(w: UInt256): float =
+  ## Represents the quotiont `w / 2^256` as `float` value. Note that the
+  ## result is non-negaive and always smaller than `1f`.
+  ##
+  when sizeof(float) != sizeof(uint):
+    {.error: "Expected float having the same size as uint".}
+  if w == 0:
+    return 0f
+  let mantissa = 256 - w.leadingZeros
+  if mantissa <= mantissaDigits(float):             # `<= 53` on a 64 bit system
+    return w.truncate(uint).float / 2f.pow(256.float)
+  # Calculate `total / 2^exp / 2^(256-exp)` = `total / 2^256`
+  let exp = mantissa - mantissaDigits(float)        # is positive
+  (w shr exp).truncate(uint).float / 2f.pow((256 - exp).float)
+
+func per256*(w: Opt[UInt256]): float =
+  ## Variant of `per256()` where the argument `w` covers the full scalar
+  ## range with `Opt.none()` repesenting `0` and `Opt.some(0)` representing
+  ## `2^255` (where the latter is not in the scalar range for `UInt256`,
+  ## anymore.)
+  ##
+  if w.isNone: 0f
+  elif w.value == 0: 1f
+  else: w.value.per256()
+
+
+func toStr*(w: float, precision: static[int] = 7): string =
+  if w == 0f:
+    "0.0"
+  elif w == 1f:
+    "1.0"
+  else:
+    when precision == 2:
+      &"{w:.2e}"
+    elif precision == 3:
+      &"{w:.3e}"
+    elif precision == 4:
+      &"{w:.4e}"
+    elif precision == 7:
+      &"{w:.7e}"
+    elif precision == 11:
+      &"{w:.11e}"
+    elif precision == 15:
+      &"{w:.15e}"
+    else:
+      {.error: "Unsupported precision".}
+
+func toStr*(w: (float,float), precision: static[int] = 4): string =
+  if w[0] < w[1]: w[0].toStr(precision) & ".." & w[1].toStr(precision)
+  elif w[0] == w[1]: w[0].toStr(precision)
   else: "n/a"
 
+# --------------
 
 func toStr*(a: chronos.Duration): string =
   if twoHundredYears <= a:
