fdc5d38158fe1049524450ea103d540ac9c025e1
bhartnett <benjamin@status.im>
2026-02-23 04:25:29 +0100

    Try fixing windows CI failure.

--- execution_chain/conf.nim
+++ execution_chain/conf.nim
@@ -281,7 +281,7 @@ type
 
     numThreads* {.
       separator: "\pPERFORMANCE OPTIONS",
-      defaultValue: 0,
+      defaultValue: 1,
       desc: "Number of worker threads (\"0\" = use as many threads as there are CPU cores available)"
       name: "num-threads" .}: int
 
