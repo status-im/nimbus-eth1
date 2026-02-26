ca6b6cfcc1e80b606f6fd7d076458b77f27c0a70
bhartnett <benjamin@status.im>
2026-02-27 00:13:06 +0800

    Add test and fix copyright.

--- tests/test_kvt.nim
+++ tests/test_kvt.nim
@@ -1,5 +1,5 @@
 # Nimbus
-# Copyright (c) 2025 Status Research & Development GmbH
+# Copyright (c) 2025-2026 Status Research & Development GmbH
 # Licensed under either of
 #  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
 #    http://www.apache.org/licenses/LICENSE-2.0)
@@ -44,3 +44,29 @@ suite "Kvt TxFrame":
       let tx = db.baseTxFrame()
       check:
         tx.get([byte 0, 1, 2]).expect("entry") == @[byte 0, 1, 3]
+
+  test "Delete range":
+    let
+      tx0 = db.txFrameBegin(db.baseTxFrame())
+
+    check:
+      tx0.put([byte 0, 1, 1], [byte 0, 1, 4]).isOk()
+      tx0.put([byte 0, 1, 2], [byte 0, 1, 5]).isOk()
+      tx0.put([byte 0, 1, 3], [byte 0, 1, 6]).isOk()
+
+    let batch = db.putBegFn().expect("working batch")
+    db.persist(batch, tx0)
+    check:
+      db.putEndFn(batch).isOk()
+
+
+    check db.delRangeBe([byte 0, 1, 2], [byte 0, 1, 3], compactRange = false).isOk()
+
+    block:
+      # using the same backend but new txRef and cache
+      let tx = db.baseTxFrame()
+      check:
+        tx.get([byte 0, 1, 1]).expect("entry") == @[byte 0, 1, 4]
+        tx.get([byte 0, 1, 3]).expect("entry") == @[byte 0, 1, 6]
+
+    db.finish()
