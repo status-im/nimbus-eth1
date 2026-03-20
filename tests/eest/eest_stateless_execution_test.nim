9826c51439aea0c157af1d14e929a035c7093644
kdeme <7857583+kdeme@users.noreply.github.com>
2026-03-20 17:24:15 +0100

    Add zkevm eest tests with witness comparison

--- tests/eest/eest_stateless_execution_test.nim
+++ tests/eest/eest_stateless_execution_test.nim
@@ -1,5 +1,5 @@
 # Nimbus
-# Copyright (c) 2025 Status Research & Development GmbH
+# Copyright (c) 2025-2026 Status Research & Development GmbH
 # Licensed under either of
 #  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE))
 #  * MIT license ([LICENSE-MIT](LICENSE-MIT))
@@ -17,7 +17,8 @@ const
   baseFolder = "tests/fixtures"
   eestType = "blockchain_tests"
   eestReleases = [
-    "eest_develop"
+    "eest_develop",
+    "eest_zkevm", # contains witness and test will compare them against the witness generated
   ]
 
 const skipFiles = [
