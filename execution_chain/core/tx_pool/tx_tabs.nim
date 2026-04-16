827eac77fb574604c8bc08f4ae6067860928d689
Advaita Saha <adv11sphs@gmail.com>
2026-04-16 15:52:38 +0530

    remove time based FIFO

--- execution_chain/core/tx_pool/tx_tabs.nim
+++ execution_chain/core/tx_pool/tx_tabs.nim
@@ -11,7 +11,7 @@
 {.push raises: [].}
 
 import
-  std/[tables, heapqueue, times],
+  std/[tables, heapqueue],
   eth/common/base,
   eth/common/addresses,
   eth/common/hashes,
@@ -60,11 +60,7 @@ func removeLookup*(blobTab: var BlobLookupTab, item: TxItemRef) =
 # HeapQueue needs `<` to be overloaded for custom object
 # and in this case, we want to pop highest price first.
 # That's why we use '>' instead of '<' in the implementation.
-# Tiebreaker: earliest arrival first (FIFO), matching geth/reth.
-func `<`(a, b: TxItemRef): bool =
-  if a.price != b.price:
-    return a.price > b.price
-  a.time < b.time
+func `<`(a, b: TxItemRef): bool = a.price > b.price
 
 proc validBlobItem(item: TxItemRef;
                    fork: EVMFork;
