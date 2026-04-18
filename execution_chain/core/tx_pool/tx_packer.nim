a40f7789e374cb71fc06fa422f6053a35c9a43ef
jangko <jangko128@gmail.com>
2026-04-18 17:13:35 +0700

    Unify processTransaction and tx_packer

--- execution_chain/core/tx_pool/tx_packer.nim
+++ execution_chain/core/tx_pool/tx_packer.nim
@@ -62,32 +62,6 @@ const
 # Private helpers
 # ------------------------------------------------------------------------------
 
-proc classifyValidatePacked(vmState: BaseVMState; item: TxItemRef): bool =
-  ## Verify the argument `item` against the accounts database. This function
-  ## is a wrapper around the `verifyTransaction()` call to be used in a similar
-  ## fashion as in `asyncProcessTransactionImpl()`.
-  let
-    roDB = vmState.readOnlyLedger
-    baseFee = vmState.blockCtx.baseFeePerGas.get(0.u256)
-    fork = vmState.fork
-    gasLimit = vmState.blockCtx.gasLimit
-    excessBlobGas = calcExcessBlobGas(vmState.com, vmState.parent, fork)
-
-  roDB.validateTransaction(
-    item.tx, item.sender, gasLimit, baseFee, excessBlobGas, vmState.com, fork).isOk
-
-func classifyPacked(vmState: BaseVMState; callResult: LogResult): bool =
-  ## Classifier for *packing* (i.e. adding up `gasUsed` values after executing
-  ## in the VM.) This function checks whether the sum of the arguments
-  ## `gasBurned` and `moreGasBurned` is within acceptable constraints.
-  let totalGasUsed = if vmState.fork >= FkAmsterdam:
-                       max(vmState.blockRegularGasUsed + callResult.blockRegularGasUsed,
-                         vmState.blockStateGasUsed + callResult.blockStateGasUsed)
-                     else:
-                       vmState.cumulativeGasUsed + callResult.gasUsed
-
-  totalGasUsed < vmState.blockCtx.gasLimit
-
 func classifyPackedNext(vmState: BaseVMState): bool =
   ## Classifier for *packing* (i.e. adding up `gasUsed` values after executing
   ## in the VM.) This function returns `true` if the packing level is still
@@ -100,74 +74,39 @@ func classifyPackedNext(vmState: BaseVMState): bool =
   else:
     vmState.cumulativeGasUsed < vmState.blockCtx.gasLimit
 
-func baseFee(pst: TxPacker): GasInt =
-  ## Getter, baseFee for the next bock header. This value is auto-generated
-  ## when a new insertion point is set via `head=`.
-  if pst.vmState.blockCtx.baseFeePerGas.isSome:
-    pst.vmState.blockCtx.baseFeePerGas.get.truncate(GasInt)
-  else:
-    0.GasInt
-
-# ------------------------------------------------------------------------------
-# Private functions
-# ------------------------------------------------------------------------------
-
-proc runTxCommit(pst: var TxPacker; item: TxItemRef; callResult: LogResult, xp: TxPoolRef) =
-  ## Book keeping after executing argument `item` transaction in the VM. The
-  ## function returns the next number of items `nItems+1`.
-  let
-    vmState = pst.vmState
-    inx     = pst.packedTxs.len
-    gasTip  = item.tx.tip(pst.baseFee)
-
-  let reward = callResult.gasUsed.u256 * gasTip.u256
-  if vmState.balTrackerEnabled:
-    vmState.balTracker.trackAddBalanceChange(xp.feeRecipient, reward)
-  vmState.ledger.addBalance(xp.feeRecipient, reward)
-  pst.blockValue += reward
-
-  # Update receipts sequence
-  if vmState.receipts.len <= inx:
-    vmState.receipts.setLen(inx + receiptsExtensionSize)
-
-  # gasUsed accounting
-  vmState.cumulativeGasUsed += callResult.gasUsed
-  vmState.blockRegularGasUsed += callResult.blockRegularGasUsed
-  vmState.blockStateGasUsed += callResult.blockStateGasUsed
-  vmState.receipts[inx] = vmState.makeReceipt(item.tx.txType, callResult)
-  pst.packedTxs.add item
-
 # ------------------------------------------------------------------------------
 # Private functions: packer packerVmExec() helpers
 # ------------------------------------------------------------------------------
 
 proc vmExecInit(xp: TxPoolRef): Result[TxPacker, string] =
-  let packer = TxPacker(
-    vmState: xp.vmState,
-    numBlobPerBlock: 0,
-    blockValue: 0.u256,
-    stateRoot: xp.vmState.parent.stateRoot,
-  )
+  let
+    vmState = xp.vmState
+    packer = TxPacker(
+      vmState: vmState,
+      numBlobPerBlock: 0,
+      blockValue: vmState.ledger.getBalance(xp.feeRecipient),
+      stateRoot: vmState.parent.stateRoot,
+    )
 
   # Setup block access list tracker for pre‑execution system calls
-  if xp.vmState.balTrackerEnabled:
-    xp.vmState.balTracker.setBlockAccessIndex(0)
-    xp.vmState.balTracker.beginCallFrame()
+  if vmState.balTrackerEnabled:
+    vmState.balTracker.setBlockAccessIndex(0)
+    vmState.balTracker.beginCallFrame()
 
   # EIP-4788
   if xp.nextFork >= FkCancun:
     let beaconRoot = xp.parentBeaconBlockRoot
-    xp.vmState.processBeaconBlockRoot(beaconRoot).isOkOr:
+    vmState.processBeaconBlockRoot(beaconRoot).isOkOr:
       return err(error)
 
   # EIP-2935
   if xp.nextFork >= FkPrague:
-    xp.vmState.processParentBlockHash(xp.vmState.blockCtx.parentHash).isOkOr:
+    vmState.processParentBlockHash(vmState.blockCtx.parentHash).isOkOr:
       return err(error)
 
   # Commit block access list tracker changes for pre‑execution system calls
-  if xp.vmState.balTrackerEnabled:
-    xp.vmState.balTracker.commitCallFrame()
+  if vmState.balTrackerEnabled:
+    vmState.balTracker.commitCallFrame()
 
   ok(packer)
 
@@ -196,54 +135,29 @@ proc vmExecGrabItem(pst: var TxPacker; item: TxItemRef, xp: TxPoolRef): bool =
     if (pst.numBlobPerBlock + item.tx.versionedHashes.len).uint64 > maxBlobsPerBlock:
       return ContinueWithNextAccount
 
-  let
-    blobGasUsed = item.tx.getTotalBlobGas
-    maxBlobGasPerBlock = getMaxBlobGasPerBlock(vmState.com, vmState.fork)
-  if vmState.blobGasUsed + blobGasUsed > maxBlobGasPerBlock:
-    return ContinueWithNextAccount
-
-  let
-    regularGasAvailable = vmState.blockCtx.gasLimit - vmState.blockRegularGasUsed
-  if min(TX_GAS_LIMIT.GasInt, item.tx.gasLimit) > regularGasAvailable:
-    return ContinueWithNextAccount
-
-  # Validate transaction relative to the current vmState
-  if not vmState.classifyValidatePacked(item):
-    return ContinueWithNextAccount
-
   if vmState.balTrackerEnabled:
     vmState.balTracker.setBlockAccessIndex(pst.packedTxs.len() + 1)
     vmState.balTracker.beginCallFrame()
 
-  # Execute EVM for this transaction
-  let
-    savePoint = vmState.ledger.beginSavePoint()
-    callResult = item.tx.txCallEvm(item.sender, pst.vmState, pst.baseFee)
-
-  doAssert 0 <= callResult.gasUsed
-
   # Find out what to do next: accepting this tx or trying the next account
-  if not vmState.classifyPacked(callResult):
-    if vmState.balTrackerEnabled:
-      vmState.balTracker.rollbackCallFrame(rollbackReads = true)
-    vmState.ledger.rollback(savePoint)
+  let rc = processTransaction(vmState, item.tx, item.sender, rollbackReads = true)
+  if rc.isErr:
     if vmState.classifyPackedNext():
       return ContinueWithNextAccount
     return StopCollecting
 
-  # Commit ledger changes
-  vmState.ledger.commit(savePoint)
-
-  vmState.ledger.persist(clearEmptyAccount = vmState.fork >= FkSpurious)
-
   # Finish book-keeping
-  pst.runTxCommit(item, callResult, xp)
+  let inx = pst.packedTxs.len
 
+  # Update receipts sequence
+  if vmState.receipts.len <= inx:
+    vmState.receipts.setLen(inx + receiptsExtensionSize)
+
+  vmState.receipts[inx] = vmState.makeReceipt(item.tx.txType, rc.value)
+  vmState.allLogs.add rc.value.logEntries
+
+  pst.packedTxs.add item
   pst.numBlobPerBlock += item.tx.versionedHashes.len
-  vmState.blobGasUsed += blobGasUsed
-
-  if vmState.balTrackerEnabled:
-    vmState.balTracker.commitCallFrame()
 
   ContinueWithNextAccount
 
@@ -279,6 +193,7 @@ proc vmExecCommit(pst: var TxPacker, xp: TxPoolRef): Result[void, string] =
   # Update flexi-array, set proper length
   vmState.receipts.setLen(pst.packedTxs.len)
 
+  pst.blockValue = vmState.ledger.getBalance(xp.feeRecipient) - pst.blockValue
   pst.receiptsRoot = vmState.receipts.calcReceiptsRoot
   pst.logsBloom = vmState.receipts.createBloom
   pst.stateRoot = vmState.ledger.getStateRoot()
