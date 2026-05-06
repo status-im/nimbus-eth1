0053f39eb67dda3ebc86714fbd9e1e8ec191cdc7
jangko <jangko128@gmail.com>
2026-05-06 11:29:41 +0700

    Reduce repeated call to intrinsicGas
    `intrinsicGas` is called twice when calling `processTransaction`.
    Once by `validateTxBasic`, and the other one called by `setupHost`.
    
    And in txpool, the same call is 3x because validateTxBasic is called twice.
    This commit reduce `intrinsicGas` call to one in `processTransaction`.
    
    And in txpool become 2x, but will soon refactored too.

--- execution_chain/core/tx_pool/tx_desc.nim
+++ execution_chain/core/tx_pool/tx_desc.nim
@@ -24,6 +24,7 @@ import
   ../../constants,
   ../../transaction,
   ../../core/eip8037,
+  ../../transaction/call_types,
   ../chain/forked_chain,
   ../pow/header,
   ../eip4844,
@@ -365,10 +366,13 @@ proc addTx*(xp: TxPoolRef, ptx: PooledTransaction): Result[void, TxError] =
     debug "Transaction already known", txHash = id
     return err(txErrorAlreadyKnown)
 
+  let
+    intrinsic = ptx.tx.intrinsicGas(xp.nextFork, xp.gasLimit)
+
   validateTxBasic(
     xp.com,
     ptx.tx,
-    xp.gasLimit,
+    intrinsic,
     xp.nextFork,
     validateFork = true).isOkOr:
     debug "Invalid transaction: Basic validation failed",
