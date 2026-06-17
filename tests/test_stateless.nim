0c4dc5698adcd4a3eadd1520c4f72e21347bd141
bhartnett <benjamin@status.im>
2026-06-17 10:43:43 +0200

    Fix copyright.

--- tests/test_stateless.nim
+++ tests/test_stateless.nim
@@ -1,5 +1,5 @@
 # Nimbus
-# Copyright (c) 2025 Status Research & Development GmbH
+# Copyright (c) 2026 Status Research & Development GmbH
 # Licensed under either of
 #  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
 #    http://www.apache.org/licenses/LICENSE-2.0)
@@ -7,6 +7,8 @@
 #    http://opensource.org/licenses/MIT)
 # at your option. This file may not be copied, modified, or
 # distributed except according to those terms.
+
+{.warning[UnusedImport]: off.}
 
 import
   ./stateless/[
