ce58afe95c97039e3a2a38916c54c7211c9de0b3
jordan <jordan@dry.pudding>
2026-06-26 18:29:58 +0100

    Remove cruft (due to fsa reorg)

--- execution_chain/sync/snap/worker/session/session_helpers.nim
+++ execution_chain/sync/snap/worker/session/session_helpers.nim
@@ -105,36 +105,6 @@ proc setPivotTag*(
 
 # ----------------
 
-proc countKvtNodes*(
-    ctx: SnapCtxRef;
-      ): tuple[nAccNodes, nStoNodes, nCodes: uint64, ela: Duration] =
-  ## Simple stored nodes counter
-  let
-    db = ctx.pool.mptAsm
-    start = Moment.now()
-  for _ in db.walkAccKvt():
-    result.nAccNodes.inc
-  for _ in db.walkStoKvt():
-    result.nStoNodes.inc
-  for _ in db.walkCodeKvt():
-    result.nCodes.inc
-  result.ela = Moment.now() - start
-
-proc countDnglLinks*(
-    ctx: SnapCtxRef;
-      ): tuple[nAccDngl, nStoDngl, nCodeMiss: uint64, ela: Duration] =
-  ## Simple stored nodes counter
-  let
-    db = ctx.pool.mptAsm
-    start = Moment.now()
-  for _ in db.walkAccDnglKvt():
-    result.nAccDngl.inc
-  for _ in db.walkStoDnglKvt():
-    result.nStoDngl.inc
-  for _ in db.walkCodeMissKvt():
-    result.nCodeMiss.inc
-  result.ela = Moment.now() - start
-
 func decodeAccount*(pyl: openArray[byte]): Opt[Account] =
   ## Decode RLP encoded `Account`
   try:
