1c8036a44daf0e0f0fce8717832ac088b7abf0dc
Advaita Saha <adv11sphs@gmail.com>
2026-07-01 19:27:06 +0530

    introduce space in error message

--- execution_chain/beacon/api_handler/api_newpayload.nim
+++ execution_chain/beacon/api_handler/api_newpayload.nim
@@ -179,14 +179,14 @@ proc newPayload*(ben: BeaconEngineRef,
       except RlpError as e:
         warn "Failed to decode payload",
           error = e.msg
-        raise parseError("Failed to decode payload" & e.msg)
+        raise parseError("Failed to decode payload: " & e.msg)
     blockAccessList =
       try:
         blockAccessList(payload)
       except RlpError as e:
         warn "Failed to decode payload",
           error = e.msg
-        raise parseError("Failed to decode payload" & e.msg)
+        raise parseError("Failed to decode payload: " & e.msg)
 
   template header: Header = blk.header
 
