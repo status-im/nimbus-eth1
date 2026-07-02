d62ed0f574423957c45cefce57a7691dc3b1dadb
jangko <jangko128@gmail.com>
2026-07-02 09:24:19 +0700

    Reenable zkevm test and upgrade test fixtures to v0.5.0

--- tests/eest/eest_stateless_execution_test.nim
+++ tests/eest/eest_stateless_execution_test.nim
@@ -18,9 +18,7 @@ const
   eestType = "blockchain_tests"
   eestReleases = [
     "eest_develop",
-    # zkevm v0.4.1 is not compatible with glamsterdam devnet v6.1.0
-    # reenable after we have compatible zkevm release.
-    # "eest_zkevm"
+    "eest_zkevm"
   ]
 
 const skipFiles = [
@@ -33,6 +31,24 @@ const skipFiles = [
   # Witness codes mismatch -> codes optimisation: implemented in
   # https://github.com/status-im/nimbus-eth1/pull/4099
   "witness_codes_create_same_hash_then_read.json",
+
+  # related to for_bpo2toamsterdamattime15k
+  "precompile_warming.json",
+  "call_value_cost_at_transition.json",
+  "cold_account_access_at_transition.json",
+  "create_base_cost_at_transition.json",
+  "ext_code_surcharge_at_transition.json",
+  "selfdestruct_account_write_at_transition.json",
+  "sstore_write_cost_at_transition.json",
+  "reservoir_available_after_transition.json",
+  "sstore_state_gas_at_transition.json",
+  "tx_gas_above_cap_at_transition.json",
+  "max_code_size_via_create_fork_transition.json",
+  "max_initcode_size_via_create_fork_transition.json",
+  "bal_fork_transition_happy_path.json",
+  "fork_transition_bal_size_constraint.json",
+  "slotnum_at_fork_transition.json",
+  "transfer_log_fork_transition.json",
 ]
 
 runEESTSuite(
