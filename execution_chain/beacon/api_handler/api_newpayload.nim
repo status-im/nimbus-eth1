c7494f07228534ff522ef1dca4aeabbc01a3855f
kdeme <7857583+kdeme@users.noreply.github.com>
2026-07-31 11:57:24 +0200

    Add engine_newPayloadWithWitnessV4 and V5
    Implement the newPayloadWithWitness methods, returning the execution
    witness (geth/ethrex ExtWitness RLP) on PayloadStatusV1. Not yet part of
    the Engine API spec.
    
    Requires --stateless-provider to produce a witness.
    Might have to adjust this to be turned on just for the call in the future.

--- execution_chain/beacon/api_handler/api_newpayload.nim
+++ execution_chain/beacon/api_handler/api_newpayload.nim
@@ -18,7 +18,8 @@ import
   ../web3_eth_conv,
   ../beacon_engine,
   ../payload_conv,
-  ./api_utils
+  ./api_utils,
+  ./api_witness
 
 {.push gcsafe, raises:[].}
 
@@ -152,7 +153,8 @@ proc newPayload*(ben: BeaconEngineRef,
                  payload: ExecutionPayload,
                  versionedHashes = Opt.none(seq[Hash32]),
                  beaconRoot = Opt.none(Hash32),
-                 executionRequests = Opt.none(seq[seq[byte]])):
+                 executionRequests = Opt.none(seq[seq[byte]]),
+                 withWitness = false):
                    Future[PayloadStatusV1] {.async: (raises: [CancelledError, ApplicationError, RlpError]).} =
 
   trace "Engine API request received",
@@ -219,7 +221,11 @@ proc newPayload*(ben: BeaconEngineRef,
   if chain.haveBlockAndState(blockHash):
     debug "Ignoring already known beacon payload",
       number = header.number, hash = blockHash.short
-    return validStatus(blockHash)
+    return
+      if withWitness:
+        validStatus(blockHash, ben.collectWitness(blockHash))
+      else:
+        validStatus(blockHash)
 
   # If this block was rejected previously, keep rejecting it
   let res = ben.checkInvalidAncestor(blockHash, blockHash)
@@ -295,4 +301,8 @@ proc newPayload*(ben: BeaconEngineRef,
     gasUsed = header.gasUsed,
     blobGas = header.blobGasUsed.get(0'u64)
 
-  return validStatus(blockHash)
+  return
+    if withWitness:
+      validStatus(blockHash, ben.collectWitness(blockHash))
+    else:
+      validStatus(blockHash)
