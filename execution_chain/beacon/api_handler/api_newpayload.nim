a14e1c607ab9c736ade7cfd98e4c30a274657393
kdeme <7857583+kdeme@users.noreply.github.com>
2026-08-01 16:45:55 +0200

    Generate newPayloadWithWitness witness on demand
    When --stateless-provider is disabled, build the execution witness by
    re-executing the block against its parent state instead of reading a
    stored one, so the engine_newPayloadWithWitness* methods work also
    without the flag.

--- execution_chain/beacon/api_handler/api_newpayload.nim
+++ execution_chain/beacon/api_handler/api_newpayload.nim
@@ -223,7 +223,7 @@ proc newPayload*(ben: BeaconEngineRef,
       number = header.number, hash = blockHash.short
     return
       if withWitness:
-        validStatus(blockHash, ben.collectWitness(blockHash))
+        validStatus(blockHash, ben.collectWitness(blk))
       else:
         validStatus(blockHash)
 
@@ -303,6 +303,6 @@ proc newPayload*(ben: BeaconEngineRef,
 
   return
     if withWitness:
-      validStatus(blockHash, ben.collectWitness(blockHash))
+      validStatus(blockHash, ben.collectWitness(blk))
     else:
       validStatus(blockHash)
