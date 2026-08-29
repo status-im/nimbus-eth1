fe2b4532a40f5818f34f2664255e0d3bd34abb1f
nitely <ecastroborsani@gmail.com>
2026-08-29 02:37:37 -0300

    oops

--- portal/rpc/rpc_portal_history_api.nim
+++ portal/rpc/rpc_portal_history_api.nim
@@ -156,7 +156,7 @@ proc installPortalHistoryApiHandlers*(rpcServer: RpcServer, n: HistoryNetwork) =
         headerBytes: string
     ): string {.async: (raises: [RpcResponseError, ValueError, CancelledError]).} =
       let header = decodeRlp(hexToSeqByte(headerBytes), Header).valueOr:
-        raise RpcResponseError((code: -39010, msg: "Failed to decode header: " & error))
+        raise applicationError((code: -39010, msg: "Failed to decode header: " & error))
 
       let blockBody = (await n.getBlockBody(header)).valueOr:
         raise contentNotFoundErr()
