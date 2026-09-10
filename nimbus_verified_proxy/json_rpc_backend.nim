b6f8116bbf51c9b6f19336d1baf50ced62cf5e5c
Chirag Parmar <chiragparmar12209@gmail.com>
2026-09-10 11:28:51 +0530

    fee history fixes (#4771)

--- nimbus_verified_proxy/json_rpc_backend.nim
+++ nimbus_verified_proxy/json_rpc_backend.nim
@@ -15,13 +15,10 @@ import
   ./engine/types,
   ./nimbus_verified_proxy_conf
 
-# for eth_feeHistory
-EthJson.automaticSerialization(int, true)
-
 # created a new sig for the feeHistory method on the RpcClient type
 createRpcSigsFromNim(RpcClient, EthJson):
   proc eth_feeHistory(
-    blockCount: Quantity, newestBlock: BlockIdentifier, rewardPercentiles: seq[int]
+    blockCount: Quantity, newestBlock: BlockIdentifier, rewardPercentiles: seq[float64]
   ): FeeHistoryResult
 
 type JsonRpcClient* = ref object
@@ -174,7 +171,7 @@ proc getExecutionApiBackend*(client: JsonRpcClient): ExecutionApiBackend =
         ok(await client.resolveClient().eth_getLogs(filterOptions))
 
     feeHistoryProc = proc(
-        blockCount: Quantity, newestBlock: BlockTag, rewardPercentiles: seq[int]
+        blockCount: Quantity, newestBlock: BlockTag, rewardPercentiles: seq[float64]
     ): Future[EngineResult[FeeHistoryResult]] {.async: (raises: [CancelledError]).} =
       rpcCall:
         ok(
